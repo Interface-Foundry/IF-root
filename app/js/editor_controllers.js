356f24d9bf82ebe37009266b5c8511ef0eae3eae
Ross Leonardy <rossleonardy@gmail.com>
2014-06-23 11:16:03 -0400

    Added editor forms

--- app/js/editor_controllers.js
+++ app/js/editor_controllers.js
@@ -1,4 +1,16 @@
 //parent
-function WorldMakerCtrl($location, $scope, $routeParams, db) {
-
+function WorldMakerCtrl($location, $scope, $routeParams, db, $rootScope) {
+	$scope.fields = [
+		{placeholder: 'World Name', inputType: 'text', isRequired: true},
+		{placeholder: 'Picture', inputType: 'picture', isRequired: true},
+		{placeholder: 'Summary', inputType: 'text', isRequired: false},
+		{placeholder: 'Description', inputType: 'text', isRequired: false},
+		{placeholder: 'Hashtag', inputType: 'text', isRequired: false},
+		{placeholder: 'Category', inputType: 'text', isRequired: false}
+	]; 
+	
+	$scope.submitForm = function() {
+		alert("it works");
+		
+	};
 }
