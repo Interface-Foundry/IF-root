ec992da5d9ed409405c48a42bf51cb45c75d87c6
jrbaldwin <jrbaldwin@gmail.com>
2014-06-27 16:03:05 -0400

    added server query framework

---
+++ server_bubblequery/constants/helper.js
@@ -0,0 +1,104 @@
+var integers = require('./integers');
+var Bubble = require('../../landmark_schema.js');
+
+function distanceInRadians(distance) {
+  re
+}
+
+exports.log = function(methodName, log) {
+  console.log(new Date().toUTCString() + " - [" + methodName + "] " + log);
+}
+
+exports.isInBubble = function(bubbleDistance) {
+  return (bubbleDistance < integers.RADIUS_DATA_BUBBLE);
+}
+
+exports.normalizeDateTime = function(date) {
+  return date.substring(1, date.length - 1);
+}
+
+exports.normalizeCoordinate = function(coordinate) {
+  var coordinateObject = JSON.parse("[" + coordinate + "]");
+  var normalizedCoordinate = coordinateObject.map(function(coordinate) {
+    return parseFloat(coordinate);
+  })
+  return {type:'Point', coordinates: normalizedCoordinate};
+}
+
+exports.checkParameters = function(keys, dictionary) {
+  for(var k in keys) {
+    if (!keys[k] in dictionary)
+      return {'result':false, 'missingKey':keys[k]};
+  }
+  return {'result': true};
+}
+
+exports.bubbleLiveStatus = function(currentDateTime, bubble) {
+
+  // Returns -1, if the bubble is already expired (ended)
+  // Returns 0, if the bubble is live
+  // Returns second left for the bubble to be live, else
+
+  //no time bubble
+  if (bubble['startTime'] == undefined){
+    return 0;
+  }
+
+  else {
+
+    var bubbleStartTime = new Date(bubble['startTime']).getTime();
+    var bubbleEndTime = new Date(bubble['endTime']).getTime();
+    var currentDateTime = new Date(currentDateTime).getTime();
+
+    var timeDifferenceStart = currentDateTime - bubbleStartTime;
+    var timeDifferenceEnd = bubbleEndTime - currentDateTime;
+
+    if (timeDifferenceStart < 0) // Bubble upcoming to be live
+      return Math.abs(timeDifferenceStart); // Abs, to get the difference
+    else if (timeDifferenceStart > 0 && timeDifferenceEnd > 0) // Bubble live
+      return 0;
+    return -1;
+  
+  }
+
+}
+
+exports.returnCoordinateObject = function(coordinate) {
+  return {type:'Point', coordinates: coordinate};
+}
+
+exports.returnBubbleObject = function(bubble) {
+  return {
+    'Lat': bubble['loc']['coordinates'][1],
+    'Lon': bubble['loc']['coordinates'][0],
+    'startTime': bubble['time']['start'],
+    'endTime': bubble['time']['end']
+  } 
+}
+
+exports.saveBubble = function(location, startTime, endTime, callback) {
+  var bubbleObject = Bubble({ loc: {type:'Point', coordinates:location },
+    startTime: startTime,
+    endTime: endTime });
+  return bubbleObject.save(callback);
+}
+
+exports.listBubblesNearCoordinate = function(coordinate, callback) {
+
+  var geoNearOptions = { spherical:true,
+    distanceMultiplier: integers.DISTANCE_MULTIPLIER_METERS,
+    maxDistance: integers.RADIUS_DATA_BUBBLE_WITH_PROXIMITY
+  };
+
+  Bubble.geoNear(coordinate, geoNearOptions, function (err, data) {
+    return callback(err, data);
+  });
+}
+
+exports.compareUpcomingBubbles = function(bubbleA, bubbleB) {
+  if(bubbleA['upcoming'] < bubbleB['upcoming'])
+    return -1;
+  if(bubbleA['upcoming'] > bubbleB['upcoming'])
+    return 1;
+  return 0;
+}
---
+++ server_bubblequery/handlers/bubble.js
@@ -0,0 +1,77 @@
+var helper = require('../constants/helper');
+var integers = require('../constants/integers');
+var strings = require('../constants/strings');
+var lists = require('../constants/lists');
+var bubble = require('../controllers/bubble');
+var Response = require('../response');
+
+exports.listBubbles = function(req, res) {
+  helper.log('listBubbles', 'From handler listing bubbles');
+
+  var parameterList = lists.PARAMETERS_LIST_BUBBLES;
+  var listBubblesResponse;
+
+  try {
+
+    console.log(req.query);
+    var parameterCheck = helper.checkParameters(parameterList, req.query);
+
+    if (!parameterCheck['result']) {
+      listBubblesResponse = new Response(integers.CODE_ERROR_MISSING_PARAMETER,
+        parameterCheck['missingKey']);
+      return res.send(listBubblesResponse);
+    }
+
+    var listBubblesRequest = req.query;
+
+    var localTime = listBubblesRequest.localTime;
+    var userCoordinate = listBubblesRequest['userCoordinate'];
+
+    // Normalize date from string to date
+    localTime = helper.normalizeDateTime(localTime);
+
+    // Normalize coordinates from string to coordinate array
+    userCoordinate = helper.normalizeCoordinate(userCoordinate);
+
+    var bubbleList = bubble.listBubbles(localTime, userCoordinate,
+      function onListBubbles(err, data) {
+        if(err) {
+          listBubblesResponse = new Response(integers.CODE_ERROR,
+            err.message,
+            null);
+          console.log(listBubblesResponse);
+          return res.send(listBubblesResponse);
+        }
+        listBubblesResponse = new Response(integers.CODE_SUCCESS,
+          strings.MESSAGE_SUCCESS,
+          data);
+        console.log(listBubblesResponse);
+        return res.send(listBubblesResponse);
+      });
+  }
+  catch (err) {
+    listBubblesResponse = new Response(integers.CODE_ERROR,
+      err.message,
+      null);
+      console.log(listBubblesResponse);
+      return res.send(listBubblesResponse);
+  }
+}
+
+exports.createBubble = function(req, res) {
+  helper.log('createBubbles', 'From handler listing bubbles');
+  var createBubbleResponse = {};
+
+  try {
+    var createBubbleResponse = bubble.createBubble(req, res);
+    createBubbleResponse = new Response(integers.CODE_SUCCESS,
+      strings.MESSAGE_SUCCESS, createBubbleResponse);
+    return res.send(createBubbleResponse);
+  }
+  catch (err) {
+    createBubbleResponse = new Response(integers.CODE_ERROR,
+      err.message,
+      null);
+    return res.send(createBubbleResponse);
+  }
+}
