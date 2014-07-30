c23030baea7da64e33b1a0c152532f8acbfe7035
jrbaldwin <jrbaldwin@gmail.com>
2014-07-30 01:39:14 -0400

    revert

--- app/js/init_controllers.js
+++ app/js/init_controllers.js
@@ -80,27 +80,27 @@ function WorldRouteCtrl($location, $scope, $routeParams, db, $rootScope, apertur
                 if (data[0].liveAndInside[0].id){
 
                     //DISABLE AFTER DEMO
-                    $location.path('/w/AlleyNYC_Startup_Showcase');
+                    //$location.path('/w/AlleyNYC_Startup_Showcase');
 
                     //ENABLE AFTER DEMO
-                    //$location.path('w/'+data[0].liveAndInside[0].id); 
+                    $location.path('w/'+data[0].liveAndInside[0].id); 
                 }
                 else {
                     //DISABLE AFTER DEMO
-                    $location.path('/w/AlleyNYC_Startup_Showcase');
+                    //$location.path('/w/AlleyNYC_Startup_Showcase');
 
                     //ENABLE AFTER DEMO
                     // console.log('world has no id');
-                    // noWorlds();
+                    noWorlds();
                 }
             }
             else {
-          
+
                 //DISABLE AFTER DEMO
-                $location.path('/w/AlleyNYC_Startup_Showcase');
+                //$location.path('/w/AlleyNYC_Startup_Showcase');
 
                 //ENABLE AFTER DEMO
-                //noWorlds(); //not inside any worlds
+                noWorlds(); //not inside any worlds
 
             }
         });
