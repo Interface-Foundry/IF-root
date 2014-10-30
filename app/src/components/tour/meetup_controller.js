72cf259e10202b11e2bf05e8cf3ea307066cfb44
jrbaldwin <jrbaldwin@gmail.com>
2014-10-30 04:58:31 -0400

    change meetup navbar color

--- app/src/components/tour/meetup_controller.js
+++ app/src/components/tour/meetup_controller.js
@@ -4,7 +4,7 @@ function MeetupController($scope, $window, $location, styleManager, $rootScope) 
 
 	var style = styleManager;
 
-	style.navBG_color = "rgba(244, 81, 30, 0.8)";
+	style.navBG_color = "rgba(173, 212, 224, 0.8)";
 
 	angular.element('#view').bind("scroll", function () {
 		console.log(this.scrollTop);
