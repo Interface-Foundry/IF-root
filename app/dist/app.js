c5b9f75fc10354a9e67910874c8ce2bba75d7741
Ross Leonardy <rossleonardy@gmail.com>
2014-11-12 11:56:55 -0500

    billionth new testflight address

--- app/dist/app.js
+++ app/dist/app.js
@@ -11402,7 +11402,7 @@ app.controller('WalkLocationController', ['$scope', '$rootScope', '$timeout', 'l
 app.controller('HomeController', ['$scope', 'worldTree', function ($scope, worldTree) {
 	worldTree.getNearby().then(function(data) {
 		console.log(data);
-	$scope.homeBubble = data.liveAndInside[0];
+	$scope.homeBubbles = data.liveAndInside;
 	$scope.nearbyBubbles = data.live;	
 	});
 }]);
--- app/phonegap/platforms/ios/www/app.js
+++ app/phonegap/platforms/ios/www/app.js
@@ -11489,7 +11489,7 @@ app.controller('WalkLocationController', ['$scope', '$rootScope', '$timeout', 'l
 app.controller('HomeController', ['$scope', 'worldTree', function ($scope, worldTree) {
 	worldTree.getNearby().then(function(data) {
 		console.log(data);
-	$scope.homeBubble = data.liveAndInside[0];
+	$scope.homeBubbles = data.liveAndInside;
 	$scope.nearbyBubbles = data.live;	
 	});
 }]);
