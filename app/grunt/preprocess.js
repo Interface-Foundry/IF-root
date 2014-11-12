c9e8ea22e24faa70e56c8054d9533a76896c18dd
Ross Leonardy <rossleonardy@gmail.com>
2014-11-12 10:23:46 -0500

    more fixes committed

--- app/grunt/preprocess.js
+++ app/grunt/preprocess.js
@@ -4,9 +4,11 @@ module.exports = {
 			'phonegap/www/app.css': 'dist/app.css',
 			'phonegap/www/app.js': 'dist/app.js',
 			'phonegap/www/lib.js': 'dist/lib.js',
-			'phonegap/www/index.html': 'src/index.html'
+			'phonegap/www/index.html': 'src/index.html',
+			'phonegap/www/templates/authDialog.html': 'phonegap/www/templates/authDialog.html'
 		},
 		options: {
+			inline: true,
 			context: {
 				PHONEGAP: true
 			}
@@ -17,7 +19,8 @@ module.exports = {
 		'dist/app.css': 'dist/app.css',
 		'dist/app.js': 'dist/app.js',
 		'dist/lib.js': 'dist/lib.js',
-		'dist/index.html': 'src/index.html'
+		'dist/index.html': 'src/index.html',
+		'dist/templates/authDialog.html': 'dist/templates/authDialog.html'
 		},
 		options: {
 			inline: true,
