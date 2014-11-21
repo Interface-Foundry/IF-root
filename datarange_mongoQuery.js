2a4133b648c00fca2cab5c8830738f8192c406cd
jrbaldwin <jrbaldwin@gmail.com>
2014-11-21 15:30:30 -0500

    update query

--- datarange_mongoQuery.js
+++ datarange_mongoQuery.js
@@ -36,8 +36,8 @@ function objectIdWithTimestamp(timestamp)
 
 var qw = {
     '_id': {
-        $gte: objectIdWithTimestamp('2014/11/3'),
-        $lt: objectIdWithTimestamp('2014/11/10')
+        $gte: objectIdWithTimestamp('2014/11/11'),
+        $lt: objectIdWithTimestamp('2014/11/21')
     }
 };   
 
