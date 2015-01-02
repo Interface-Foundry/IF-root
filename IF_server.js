afcb17e1806abc61b775918b9ebc86d5c673b9c7
jrbaldwin <jrbaldwin@gmail.com>
2015-01-02 10:45:13 -0500

    re-added flash on server

--- IF_server.js
+++ IF_server.js
@@ -116,7 +116,7 @@ var express = require('express'),
 
     app.use(passport.initialize());
     app.use(passport.session()); // persistent login sessions
-    //app.use(flash()); // use connect-flash for flash messages stored in session
+    app.use(flash()); // use connect-flash for flash messages stored in session
 
 
 //===================//
