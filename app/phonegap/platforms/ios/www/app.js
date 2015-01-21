6458a13abcef1dbfa489c296c56ac2ff748223d8
Ross Leonardy <rossleonardy@gmail.com>
2015-01-21 17:10:08 -0500

    Another app.js fix

--- app/phonegap/platforms/ios/www/app.js
+++ app/phonegap/platforms/ios/www/app.js
@@ -22438,7 +22438,7 @@ function loadWidgets() {
 			$scope.messages = true;
 
 			//angular while loop the query every 2 seconds
-			db.messages.query({limit:1, worldID:$routeParams.worldURL}, function(data){ 
+			db.messages.query({limit:1, roomID:$scope.world._id}, function(data){ 
 				console.log('db.messages', data);
 				if (data.length>0) {
 					$scope.msg = data[0];
