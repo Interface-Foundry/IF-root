4ce1d8f6347c9304b32c3928bb3c98966584e111
Jeehyun Kim <inneekim@gmail.com>
2015-02-02 16:42:27 -0500

    IF_mail uses SendGrid now

--- components/IF_mail/IF_mail.js
+++ components/IF_mail/IF_mail.js
@@ -1,12 +1,14 @@
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 var nodemailer = require('nodemailer');
-var smtpTransport = require('nodemailer-smtp-transport');
+var sgTransport = require('nodemailer-sendgrid-transport');
 
-var transport = nodemailer.createTransport(smtpTransport({
-    host: 'localhost',
-    port: 25,
-    strictSSL: false,
-    rejectUnauthorized: false
-}));
+var options = {
+	auth: {
+		api_user:'IF_mail',
+		api_key: 'yLh6_foodistasty_q!WfT]7a',
+	}
+}
 
-module.exports = transport;
+var client = nodemailer.createTransport(sgTransport(options));
+
+module.exports = client;
