d4b5196f5adda6801668220544de0576ea114b37
Jeehyun Kim <inneekim@gmail.com>
2015-02-04 14:20:01 -0500

    cleans up indentation levels

--- components/IF_bubbleroutes/worlds_query.js
+++ components/IF_bubbleroutes/worlds_query.js
@@ -21,30 +21,32 @@ var route = function(userCoord0, userCoord1, userTime, res){
   function(err,data) {
     var four_groups = _.groupBy(data, function(world){
 
-      if (world.distance <= 150) {
+    if (world.distance <= 150) {
         if ( (!world.time.end && !world.time.start)  //If there's no start or end time bubble is always live.
           || ( (new Date(world.time.start) + 604800000) > new Date(userTime)) //If there's a start but no end time, default end is start time plus a week. 604800000 milliseconds is a week
           || (new Date(world.time.end) > new Date(userTime)) ) {
           return '150m';
-      }
-      else { 
-        return '150mPast'; 
-      }
+        }
+        else { 
+            return '150mPast'; 
+        }
     }
-    else if ( (!world.time.end && !world.time.start)  
+    else {
+        if ( (!world.time.end && !world.time.start)  
       || (new Date(world.time.start) + 604800000 > new Date(userTime)) 
       || (new Date(world.time.end) > new Date(userTime)) ) {
       return '2.5km';
   }
-  else { 
-    return '2.5kmPast';
-  }
+    else { 
+        return '2.5kmPast';
+  }}
 });
     for(var key in four_groups) {
-      four_groups[key] = _.sortBy(four_groups[key], function(world) {
-
-                return -(world.distance + Math.pow( (new Date(world.time.start) - new Date(userTime))/3600000), 2); //time interval is in milliseconds
-              });
+        four_groups[key] = _.sortBy(four_groups[key], function(world) {
+        return -(world.distance + Math.pow( (new Date(world.time.start) - new Date(userTime))/3600000), 2); //time interval is in milliseconds
+ });
+    for (var i = 0; i < (four_groups[key]).length; i++){
+        console.log(four_groups[key][i]['distance'], four_groups[key][i]['name'])}
     };
 
     res.send([four_groups]);
