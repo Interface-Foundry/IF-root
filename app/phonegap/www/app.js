6e37e7f55093c23efe40d2e14d994ae9331cdd2e
Steve Rubinstein <slrubinstein@gmail.com>
2015-02-20 18:21:19 -0500

    adding animation styles

--- app/phonegap/www/app.js
+++ app/phonegap/www/app.js
@@ -21243,12 +21243,14 @@ app.directive('floorSelector', floorSelector);
 function floorSelector() {
 	return {
 		restrict: 'E',
+		scope: true,
 		// template: '<div>123</div>',
 		templateUrl: 'components/floor_selector/floor.selector.html',
 		link: function(scope, elem, attrs) {
 			elem.bind('click', function() {
-				console.log('click!')
+				scope.showFloors = !scope.showFloors;
 			});
+			scope.showFloors = false;
 		}
 	};
 }
