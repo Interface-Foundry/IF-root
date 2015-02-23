ef804e4a72322399a3983d52f796d39dde370600
Steve Rubinstein <slrubinstein@gmail.com>
2015-02-23 13:14:20 -0500

    user can select floors in the floor selector to change their current floor

--- app/dist/app.js
+++ app/dist/app.js
@@ -21141,15 +21141,51 @@ app.directive('floorSelector', floorSelector);
 function floorSelector() {
 	return {
 		restrict: 'E',
-		scope: true,
-		// template: '<div>123</div>',
+		scope: {
+			world: '@world'
+		},
 		templateUrl: 'components/floor_selector/floor.selector.html',
-		link: function(scope, elem, attrs) {
-			elem.bind('click', function() {
+		link: function(scope, elem, attr) {
+
+			scope.currentFloor = {};			
+
+			scope.selectFloor = function(index) {
+				scope.currentFloor = scope.floors[index];
 				scope.showFloors = !scope.showFloors;
+			}
+
+			scope.openFloorMenu = function() {
+				scope.showFloors = !scope.showFloors;
+			}
+			
+			// when world changes in world controller, assign local vars in directive scope
+			attr.$observe('world', function(world) {
+				var world = JSON.parse(attr.world);
+				if (!world.style || !world.style.maps || !world.style.maps.localMapArray) {
+					return;
+				}
+				scope.showFloors = false;
+				scope.floors = _.chain(world.style.maps.localMapArray)
+					.filter(function(f) {
+						return f.floor_num;
+					})
+					.groupBy(function(f) {
+						return f.floor_num;
+					})
+					.sortBy(function(f) {
+						return -f.floor_num;
+					})
+					.flatten()
+					.value();
+
+				scope.currentFloor = scope.floors[0];
+				console.log(scope.currentFloor)
 			});
-			scope.showFloors = false;
+
+
 		}
+
+
 	};
 }
 app.controller('HomeController', ['$scope', '$rootScope', '$location', 'worldTree', 'styleManager', 'mapManager', 'geoService', 'ifGlobals', function ($scope, $rootScope, $location, worldTree, styleManager, mapManager, geoService, ifGlobals) {
