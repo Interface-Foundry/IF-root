25eea9c81e55a1e4c51c9c88c245c8290d356b4c
Peter Brandt <peter.m.brandt@gmail.com>
2015-03-20 12:12:14 -0400

    adding lots of files for sequence generation and testing

---
+++ IF_services/IF_analytics/test/visualization/heatmap.js
@@ -0,0 +1,10 @@
+var map = L.map('map').setView([40.7384012, -73.9878516], 13);
+
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18
+}).addTo(map);
+
+// using window.latlngs
+
+var heat = L.heatLayer(latlngs, { radius: 25}).addTo(map);
