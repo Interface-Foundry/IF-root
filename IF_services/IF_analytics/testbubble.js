ae3112d87f64355237abfa0fb6041ca0b4492d7c
Peter Brandt <peter.m.brandt@gmail.com>
2015-03-23 19:52:16 -0400

    adding positive test for bubble presence

--- IF_services/IF_analytics/testbubble.js
+++ IF_services/IF_analytics/testbubble.js
@@ -2,12 +2,34 @@ var llb = require('./lonLatToBubble.js');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/if');
 
-llb([0,0], function(err, bubble) {
-  if (err) {
-    console.log('error :<');
-    return console.log(err);
-  }
 
-  console.log(bubble);
-    process.exit(0);
+var bubblePoint = [-73.9936362, 40.71499120000001];
+var emptyPoint = [0, 0];
+
+
+
+llb(emptyPoint, function(err, bubble) {
+    if (err) {
+        console.log('test failed');
+        console.log(err);
+    }
+
+    if (bubble) {
+        console.error('test failed bubble found');
+        console.log(bubble);
+    }
+});
+
+
+llb(bubblePoint, function(err, bubble) {
+    if (err) {
+        console.log('test failed');
+        console.log(err);
+    }
+
+    if (!bubble) {
+        console.error("test failed no bubble foud");
+    }
+
+    console.log(bubble);
 });
