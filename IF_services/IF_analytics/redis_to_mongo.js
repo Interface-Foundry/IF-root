f9429e2153fdf78ce5df252c11eee578103c53e3
Peter Brandt <peter.m.brandt@gmail.com>
2015-03-25 11:59:31 -0400

    finishing redis cache dump

--- IF_services/IF_analytics/redis_to_mongo.js
+++ IF_services/IF_analytics/redis_to_mongo.js
@@ -8,6 +8,7 @@ var mongoose = require('mongoose');
 var analyticsdb = process.env.ANALYTICS_DB || 'mongodb://localhost:27017/if';
 var Analytics = require('_if_/components/IF_schemas/analytics_schema.js');
 var redis = require('redis');
+var client = redis.createClient();
 
 mongoose.connect(analyticsdb, function(err) {
   if (err) {
@@ -17,12 +18,26 @@ mongoose.connect(analyticsdb, function(err) {
 
 // periodically dump the redis cache into the analytics db
 setTimeout(function() {
-  redis.lrange('analytics', function (err, docs) {
-    docs.map(function(doc) {
+
+  // get all the documents
+  client.lrange('analytics', 0, -1, function (err, docs) {
+    docs.map(function(doc_str) {
+      var doc;
+      try {
+        doc = JSON.parse(doc_str);
+      }
+      catch (e){
+        console.error('Error JSON.parsing analytics doc' + doc);
+      }
+
+      // save to db
       new Analytics(doc).save(function (err) {
         if (err) {
           console.error(err);
         }
+
+        // remove from processing queue
+        client.lrem('analytics', 1, doc_str, redis.print);
       });
     });
   });
