0127ee492e52ef46754a7244c44951ef8744a1c2
muchimoto <muchimoto@gmail.com>
2015-03-25 11:38:42 -0400

    added template

---
+++ components/IF_schemas/announcements_schema.js
@@ -0,0 +1,37 @@
+// load the things we need
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema,
+    ObjectID = Schema.ObjectID;
+
+// ANNOUCEMENTS | Add announcement schema (timestamp, region: 'global') 
+// with ability for super user to add announcements in object array 
+// (headline, html body, URL, priority #,. maybe img url) @mitsuakiuchimoto 
+
+var announcementsSchema = mongoose.Schema({
+    headline: {
+        type: String,
+        required: true
+    }, 
+    body: {
+        type: String,
+        required: true
+    }, 
+    URL: {
+        type: String,
+        required: true
+    }, 
+    priority: {type: Number},
+    live: {type: Boolean},
+    imgURL: {
+        type: String,
+        required: true
+    },
+    region: {
+        type: String,
+        default: 'global'
+    },
+    timestamp: { type: Date, default: Date.now }
+});
+
+// create the model for users and expose it to our app
+module.exports = mongoose.model('announcements', announcementsSchema);
