803c728d7586fcb23eda0f4b42a5bf8ff007cbe1
muchimoto <muchimoto@gmail.com>
2015-03-27 12:34:24 -0400

    modularized announcement and contest routes to separate files

--- IF_server.js
+++ IF_server.js
@@ -60,6 +60,8 @@ var sanitize = require('mongo-sanitize');
 var worlds_query = require('./components/IF_bubbleroutes/worlds_query');
 var random_bubble = require('./components/IF_bubbleroutes/random_bubble');
 
+
+
 //---- SEARCH -------//
 var text_search = require('./components/IF_search/text_search');
 var bubble_search = require('./components/IF_search/bubble_search');
@@ -76,8 +78,6 @@ var mongoose = require('mongoose'),
     visitSchema = require('./components/IF_schemas/visit_schema.js'),
     anonUserSchema = require('./components/IF_schemas/anon_user_schema.js'),
     analyticsSchema = require('./components/IF_schemas/analytics_schema.js'),
-    announcementSchema = require('./components/IF_schemas/announcements_schema.js'),
-    contestSchema = require('./components/IF_schemas/contest_schema.js'),
     monguurl = require('monguurl');
 
 mongoose.connect(configDB.url);
@@ -206,7 +206,6 @@ app.post('/feedback', function(req, res) {
 // var client = redis.createClient(); //creates a new client 
 
 var redisClient = require('./redis.js');
-
 
 //====================================//
 //======= RESET PASSWORD MAILER ======//
@@ -390,6 +389,12 @@ app.get('/api/user/loggedin', function(req, res) {
 });
 
 
+//--- SUPER USER ROUTER ----//
+app.use('/api/announcements', require('./components/IF_superuser/announcement_routes'));
+app.use('/api/contests', require('./components/IF_superuser/contest_routes'));
+
+
+
 // PROFILE SECTION =========================
 app.get('/api/user/profile', isLoggedIn, function(req, res) {
 
@@ -417,274 +422,7 @@ app.get('/api/user/profile', isLoggedIn, function(req, res) {
     });
 });
 
-// SUPERUSER SECTION =========================
 
-//load all announcements for that region
-app.get('/api/announcements/:id', function(req, res) {
-    if (req.user.admin) {
-        //find all announcements for given region, then sort by priority
-        announcementSchema.aggregate({
-            $match: {
-                region: req.params.id.toString().toLowerCase()
-            }
-        }, {
-            $sort: {
-                priority: 1
-            }
-        }, function(err, announcements) {
-            if (err) {
-                return handleError(res, err);
-            }
-            return res.send(announcements);
-        });
-
-    } else {
-        console.log('you are not authorized...stand down..')
-    }
-})
-
-//Create new announcement for that region and shift priorities for all others
-app.post('/api/announcements', function(req, res) {
-    if (req.user.admin) {
-        //Increment priority for all other announcements other than one being created
-        announcementSchema.update({}, {
-                $inc: {
-                    priority: 1
-                }
-            }, {
-                multi: true
-            }, function(err, result) {
-                if (err) {
-                    console.log(err)
-                }
-                console.log('documents incremented', result)
-            })
-            //Then create the new announcement with priority one
-        var newannouncement = new announcementSchema();
-        //Merge new announcement with whatever is sent from frontend
-        var announcement = _.extend(newannouncement, req.body);
-        //Save announcement
-        announcement.save(
-            function(err, announcement) {
-                if (err) {
-                    console.log(err)
-                }
-                console.log('saved!', announcement)
-                    //Re-sort all announcements, then send to front-end
-                announcementSchema.find().sort({
-                    priority: 1
-                }).exec(function(err, announcements) {
-                    console.log('announcements is..', announcements)
-                    if (err) {
-                        console.log(err)
-                    }
-                    return res.send(announcements)
-                })
-            });
-    }
-})
-
-//When superuser changes priority of announcement
-app.post('/api/announcements/:id/sort', function(req, res) {
-
-    //-------If priority is moving up-----//
-    if (req.body.dir === 'up' && req.body.priority !== 1) {
-        //find the announcement immediately above the current announcement
-        //and increment it's priority
-        announcementSchema.update({
-            priority: req.body.priority - 1
-        }, {
-            $inc: {
-                priority: 1
-            }
-        }).exec(function() {
-            //find the current announcement and decrement it's priority
-            announcementSchema.update({
-                _id: req.params.id
-            }, {
-                $inc: {
-                    priority: -1
-                }
-            }).exec(function(err, result) {
-
-                console.log('prioritiezed announcement is..', result)
-                //Re-sort all announcements, then send to front-end
-                announcementSchema.find().sort({
-                    priority: 1
-                }).exec(function(err, announcements) {
-              
-                    if (err) {
-                        console.log(err)
-                    }
-                    return res.send(announcements)
-                })
-            })
-        })
-    }
-
-    //-------If priority is moving down-----//
-    if (req.body.dir === 'down') {
-        //find the announcement immediately below the current announcement
-        //and decrement it's priority (aka move priority up)
-        announcementSchema.update({
-            priority: req.body.priority + 1
-        }, {
-            $inc: {
-                priority: -1
-            }
-        }).exec(function() {
-            //find the current announcement and increment it's priority
-            announcementSchema.update({
-                _id: req.params.id
-            }, {
-                $inc: {
-                    priority: 1
-                }
-            }).exec(function(err, result) {
-
-                 console.log('prioritiezed announcement is..', result)
-                //Re-sort all announcements, then send to front-end
-                announcementSchema.find().sort({
-                    priority: 1
-                }).exec(function(err, announcements) {
-                  
-                    if (err) {
-                        console.log(err)
-                    }
-                    return res.send(announcements)
-                })
-            })
-        })
-    }
-
-})
-
-//When superuser edits announcement content or toggles 'Live' button
-app.put('/api/announcements/:id', function(req, res) {
-    announcementSchema.findOne({
-        _id: req.params.id
-    }, function(err, result) {
-        if (err) {
-            return handleError(res, err);
-        }
-        if (!result) {
-            return res.send(404);
-        }
-        //Merge existing announcement with updated object from frontend
-        var announcement = _.extend(result, req.body);
-
-        //Save announcement
-        announcement.save(
-            function(err, announcement) {
-                if (err) {
-                    console.log(err)
-                }
-                console.log('updated announcement is..', announcement)
-                    //Re-sort all announcements, then send to front-end
-                announcementSchema.find().sort({
-                    priority: 1
-                }).exec(function(err, announcements) {
-
-                    if (err) {
-                        console.log(err)
-                    }
-                    return res.send(announcement)
-                })
-            })
-    })
-})
-
-//delete announcement for that region
-app.delete('/api/announcements/:id', function(req, res) {
-    announcementSchema.findById(req.params.id, function(err, announcement) {
-        if (err) {
-            return handleError(res, err);
-        }
-        if (!announcement) {
-            return res.send(404);
-        }
-        var prior = announcement.priority;
-
-        announcementSchema.update({
-                priority: {
-                    $gt: prior
-                }
-            }, {
-                $inc: {
-                    priority: -1
-                }
-            }, {
-                multi: true
-            },
-            function(err, numberAffected, rawResponse) {
-                if (err) {
-                    console.log(err)
-                }
-                console.log('updated ', numberAffected, 'records')
-                console.log(rawResponse)
-                announcement.remove(function(err) {
-                    if (err) {
-                        return handleError(res, err);
-                    }
-                    console.log('deleted successfully!')
-                        //Re-sort all announcements, then send to front-end
-                    announcementSchema.find().sort({
-                        priority: 1
-                    }).exec(function(err, announcements) {
-                        console.log('announcements is..', announcements)
-                        if (err) {
-                            console.log(err)
-                        }
-                        return res.send(announcements)
-                    })
-                })
-            })
-    });
-})
-
-
-//load current contest for that region
-app.get('/api/contests/:id', function(req, res) {
-    if (req.user.admin) {
-
-        console.log('hitting get contest')
-        //find current contest
-        announcementSchema.aggregate({
-            $match: {
-                region: req.params.id.toString().toLowerCase()
-            }
-        }, {
-            current: true
-        }, function(err, contest) {
-            if (err) {
-                console.log(err);
-            }
-            return res.send(contest);
-        });
-
-    } else {
-        console.log('you are not authorized...stand down..')
-    }
-})
-
-
-//create new contest for that region
-app.post('/api/contests', function(req, res) {
-    if (req.user.admin) {
-        var newcontest = new contestSchema();
-        var contest = _.extend(newcontest, req.body);
-
-        contest.save(
-            function(err, contest) {
-                if (err) {
-                    console.log(err)
-                }
-                return res.send(contest);
-            });
-    }
-})
-
-// END OF SUPERUSER SECTION =========================
 
 
 // route middleware to ensure user is logged in
---
+++ components/IF_superuser/contest_routes.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var express = require('express'),
+router = express.Router(),
+contestSchema = require('../IF_schemas/contest_schema.js')
+
+
+//load current contest for that region
+router.get('/:id', function(req, res) {
+    if (req.user.admin) {
+
+        console.log('hitting get contest')
+        //find current contest
+        announcementSchema.aggregate({
+            $match: {
+                region: req.params.id.toString().toLowerCase()
+            }
+        }, {
+            current: true
+        }, function(err, contest) {
+            if (err) {
+                console.log(err);
+            }
+            return res.send(contest);
+        });
+
+    } else {
+        console.log('you are not authorized...stand down..')
+    }
+})
+
+
+//create new contest for that region
+router.post('/', function(req, res) {
+    if (req.user.admin) {
+        var newcontest = new contestSchema();
+        var contest = _.extend(newcontest, req.body);
+
+        contest.save(
+            function(err, contest) {
+                if (err) {
+                    console.log(err)
+                }
+                return res.send(contest);
+            });
+    }
+})
+
+
+module.exports = router;
+
+
