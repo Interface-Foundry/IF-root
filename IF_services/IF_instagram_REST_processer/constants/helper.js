ac62bd78e64dcca34242c3102302c6c11140c618
muchimoto <muchimoto@gmail.com>
2015-04-03 14:46:19 -0400

    fixed instagram crawler bug

--- IF_services/IF_instagram_REST_processer/constants/helper.js
+++ IF_services/IF_instagram_REST_processer/constants/helper.js
@@ -74,7 +74,8 @@ var downloadImage = function(imageURL) {
                                     Key: fileName
                                 };
                                 s3.headObject(params, function(err, metadata) {
-                                    if (err && err.code === 'Not Found') {
+                                    if (err && err.code == 'NotFound') {
+                                       console.log('Not found! Saving!')
                                         s3.putObject({
                                             Bucket: awsBucket,
                                             Key: fileName,
