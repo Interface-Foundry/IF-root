9deec8a35a6281a2099132818721c242817ad140
jrbaldwin <jrbaldwin@gmail.com>
2015-04-13 10:21:45 -0400

    adding new custom phonegap Facebook strategy

---
+++ components/IF_auth/strategies/client_facebook.js
@@ -0,0 +1,58 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var passport = require('passport'),
+	CustomStrategy = require('./../custom_strategy/custom_strategy').Strategy,
+	User = require('mongoose').model('User'),
+	users = require('../../app/controllers/users.server.controller');
+
+module.exports = function() {
+	console.log('here we are');
+	// Use local strategy
+	passport.use(new CustomStrategy({
+			userId: 'userId',
+			accessToken: 'accessToken'
+		},
+		function(userId, accessToken, response, done) {
+			console.log('in the authentication function');
+			if (response.error){
+				return done(null, false, {
+					message: response.error.message
+				});
+			}
+
+
+			console.log(userId);
+
+			var userObject = JSON.parse(response);
+			console.log(userObject.id);
+
+			if (userId !== userObject.id){
+				return done(null, false, {
+					message: 'Incorrect Access Token'
+				});
+			}
+
+			var providerData = {};
+			providerData.json = userObject;
+			providerData.accessToken = accessToken;
+
+			var providerUserProfile = {
+				firstName: userObject.first_name,
+				lastName: userObject.last_name,
+				displayName: userObject.name,
+				email: userObject.email || '',
+				username: userObject.username,
+				provider: 'facebook_client',
+				providerIdentifierField: 'id',
+				providerData: providerData
+			};
+
+			console.log(providerUserProfile);
+
+			users.saveFacebookUserProfile(providerUserProfile, done);
+		}
+	));
+};
