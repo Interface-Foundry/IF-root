7b4bebc7caaa9b0ba154007584d21afe390dc082
muchimoto <muchimoto@gmail.com>
2015-04-14 19:08:22 -0400

    changed global.config.geoipURL

--- config/production.js
+++ config/production.js
@@ -4,8 +4,8 @@
 // =================================
 module.exports = {
 
-  //geoipURL: 'http://127.0.0.1:8080/json/',
-  geoipURL: 'http://www.freegeoip.net/json/',
+  geoipURL: 'http://127.0.0.1:8080/json/',
+  // geoipURL: 'http://www.freegeoip.net/json/',
   mongodb: {
 	  url: 'mongodb://ifappuser:password@mongodb-r1.kipapp.co,mongodb-r2.kipapp.co/if'.replace('password', process.env.MONGOPW)
   },
