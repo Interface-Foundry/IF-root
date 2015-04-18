994cc0dd46231bdd5b226b24a7be05912f5c8455
jrbaldwin <jrbaldwin@gmail.com>
2015-04-18 09:42:29 -0400

    some tweaks

--- app/grunt/preprocess.js
+++ app/grunt/preprocess.js
@@ -5,6 +5,7 @@ module.exports = {
 			'phonegap/www/app.js': 'dist/app.js',
 			'phonegap/www/lib.js': 'dist/lib.js',
 			'phonegap/www/index.html': 'src/index.html',
+			'phonegap/www/components/user/templates/me.html': 'src/components/user/templates/me.html',
 			'phonegap/www/templates/authDialog.html': 'phonegap/www/templates/authDialog.html',
 			'phonegap/www/templates/createAccountDialog.html': 'phonegap/www/templates/createAccountDialog.html'
 		},
@@ -22,6 +23,7 @@ module.exports = {
 		'dist/app.js': 'dist/app.js',
 		'dist/lib.js': 'dist/lib.js',
 		'dist/index.html': 'src/index.html',
+		'dist/components/user/templates/me.html': 'src/components/user/templates/me.html',
 		'dist/templates/authDialog.html': 'dist/templates/authDialog.html',
 		'dist/templates/createAccountDialog.html': 'dist/templates/createAccountDialog.html'
 		},
