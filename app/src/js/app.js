b0a70581ab9296f6bcb9cafa0aefeff330743cb5
muchimoto <muchimoto@gmail.com>
2015-05-01 11:58:05 -0400

    Update app.js
    changed http back to https

--- app/src/js/app.js
+++ app/src/js/app.js
@@ -48,7 +48,7 @@ var updateTitle = function($rootScope) {
 
               // TODO use a environment-specific config
               // http://stackoverflow.com/a/18343298
-		    			request.url = 'http://kipapp.co' + request.url;
+		    			request.url = 'https://kipapp.co' + request.url;
 
 
 
