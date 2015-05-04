3b7047df4940a7fd77c7a9198ecbc8c4bc9245c9
Toks Fifo <toksfifo@gmail.com>
2015-05-04 17:51:02 -0400

    grunt and cordova build
    minor edit to foundry.less

--- app/phonegap/platforms/ios/www/app.js
+++ app/phonegap/platforms/ios/www/app.js
@@ -5971,44 +5971,6 @@ function lazyLoad() {
 	}
 }
 
-app.directive('progressCircle', function() {
-	return {
-		restrict: 'EA',
-		scope: {
-			top: '=',
-			left: '=',
-			fullWidth: '=',
-			spinLeft: '=',
-			spinLeftLong: '='
-		},
-		templateUrl: 'templates/progressCircle.html',
-		controller: function($scope) {
-			$scope.style = function() {
-				return {
-					'top': $scope.top + 'px',
-					'left': $scope.left + 'px',
-					'width': $scope.fullWidth + 'px',
-					'height': $scope.fullWidth + 'px',
-					'clip': getClip($scope.spinLeft, $scope.spinLeftLong, $scope.fullWidth)
-				};
-			};
-
-			$scope.styleClip = {
-				'clip': 'rect(0px,' + $scope.fullWidth/2 + 'px,' + $scope.fullWidth + 'px,' + '0px)'
-			};
-
-			var getClip = function(spinLeft, spinLeftLong, fullWidth) {
-				var clipNone = 'rect(auto, auto, auto, auto)';
-				var clipAll = 'rect(0px, 0px, 0px, 0px)';
-				var clipLeft = 'rect(0px,' + fullWidth + 'px,' + fullWidth + 'px,' + (fullWidth/2) + 'px)';
-
-				if (spinLeft && spinLeftLong) return clipNone;
-				else if (!spinLeft && !spinLeftLong) return clipAll;
-				else return clipLeft; // default
-			}
-		}
-	};
-});
 //angular.module('IF-directives', [])
 app.directive('ryFocus', function($rootScope, $timeout) {
 	return {
