8504405f047302d63d4b77a96e07d8866ef68b10
Peter Brandt <peter.m.brandt@gmail.com>
2015-05-05 19:20:55 -0400

    adding test configuration file

---
+++ config/test.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// find the local ip address
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // geoipURL: 'http://web-server-charmander.kipapp.co:8080/',
+  facebookAuth: {
+    callbackURL: 'http://pikachu.kipapp.co/auth/facebook/callback'
+  }
+
+  // FACEBOOK_ID:      'app-id',
+  // FACEBOOK_SECRET:  'secret',
+
+  // // MongoDB connection options
+  // mongo: {
+  //   uri: 'mongodb://localhost/app'
+  // },
+
+// seedDB: true
+};
