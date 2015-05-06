5d9535ca83227586ecbe065ce96e90c36de3042d
Peter Brandt <peter.m.brandt@gmail.com>
2015-05-06 09:43:34 -0400

    file for exporting data from production for testing

--- conf/mongo/make_testdb.sh
+++ conf/mongo/make_testdb.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-rm -rf testdb
+rm -rf dump
+rm -f dump.tar.gz
 
 # landmarks in NYC
-mongodump -d if -c landmarks -u ifappuser -p $MP --query '{loc:{$near:{$geometry:{type:"Point",coordinates:[-73.9878042,40.7479261]},$maxDistance:10000}},"loc.coordinates.0":{$ne:-74.0059}}'
+mongodump -d if -c landmarks -u ifappuser -p $MP --query '{loc:{$near:{$geometry:{type:"Point",coordinates:[-73.9878042,40.7479261]},$maxDistance:20000}},"loc.coordinates.0":{$ne:-74.0059}}'
 
 # users
 mongodump -d if -c users -u ifappuser -p $MP --query '{"local.email":/interfacefoundry/}'
@@ -12,13 +13,13 @@ mongodump -d if -c users -u ifappuser -p $MP --query '{"local.email":/interfacef
 mongodump -u ifappuser -p $MP -d if -c announcements
 mongodump -u ifappuser -p $MP -d if -c contestentries
 mongodump -u ifappuser -p $MP -d if -c contests
-mongodump -u ifappuser -p $MP -d if -c instagrams
+#mongodump -u ifappuser -p $MP -d if -c instagrams
 mongodump -u ifappuser -p $MP -d if -c stickers
 mongodump -u ifappuser -p $MP -d if -c styles
-mongodump -u ifappuser -p $MP -d if -c tweets
+#mongodump -u ifappuser -p $MP -d if -c tweets
 mongodump -u ifappuser -p $MP -d if -c visits
 mongodump -u ifappuser -p $MP -d if -c worldchats
 
-# targz and
-tar czvf
+# targz dat shit
+tar czvf dump.tar.gz
 
