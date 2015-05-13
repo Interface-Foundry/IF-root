2274e2941435155370ab35dd976daa9fc582a1b5
Steve Rubinstein <slrubinstein@gmail.com>
2015-05-13 17:29:59 -0400

    fix world builder service bug

--- app/dist/app.js
+++ app/dist/app.js
@@ -19737,7 +19737,7 @@ function worldBuilderService(mapManager, userManager, localStore, apertureServic
 
 			// if bubble has local maps then do not show world marker
 			if (!mapManager.localMapArrayExists(world)) {
-				addWorldMarker();
+				addWorldMarker(world);
 			}
 
 		} else {
@@ -19753,6 +19753,9 @@ function worldBuilderService(mapManager, userManager, localStore, apertureServic
 
 		if (tilesDict.hasOwnProperty(worldStyle.maps.cloudMapName)) {
 			mapManager.setBaseLayer(tilesDict[worldStyle.maps.cloudMapName]['url']);
+		} else if (worldStyle.maps.cloudMapName === 'none') {
+			mapManager.layers.baselayers = {};
+			angular.element('#leafletmap')[0].style['background-color'] = 'black';
 		} else if (worldStyle.maps.hasOwnProperty('cloudMapID')) {
 			mapManager.setBaseLayer('https://{s}.tiles.mapbox.com/v3/'+worldStyle.maps.cloudMapID+'/{z}/{x}/{y}.png');
 		} else {
@@ -19764,7 +19767,7 @@ function worldBuilderService(mapManager, userManager, localStore, apertureServic
 		mapManager.toggleOverlay(mapLayer);
 
 	}
-	function addWorldMarker() {
+	function addWorldMarker(world) {
 		mapManager.addMarker('c', {
 			lat: world.loc.coordinates[1],
 			lng: world.loc.coordinates[0],
