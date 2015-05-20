c8d8425b9a2733767ab534b95c6e42e34b023e81
Steve Rubinstein <slrubinstein@gmail.com>
2015-05-20 13:11:21 -0400

    improve download banner functionality

--- app/src/components/download_banner/downloadBannerDirective.js
+++ app/src/components/download_banner/downloadBannerDirective.js
@@ -2,46 +2,54 @@
 
 app.directive('downloadBanner', downloadBanner);
 
-downloadBanner.$inject = ['$window', '$rootScope'];
+downloadBanner.$inject = ['$window', '$rootScope', 'apertureService'];
 
-function downloadBanner($window, $rootScope) {
+function downloadBanner($window, $rootScope, apertureService) {
 	return {
 		restrict: 'E',
 		templateUrl: 'components/download_banner/downloadBanner.html',
 		scope: {
-
+			aperture: '=aperture'
 		},
 		link: link
 	};
 
 	function link(scope, elem, attr) {
+		var nav = angular.element('.main-nav');
 		var wrap;
-		var nav;
 		var banner;
-		var home;
-		var shelf;
+		var viewContainer;
+		var apertureWatch
 		var routeListener;
+		
+		nav.addClass('banner-offset');
 
+		scope.aperture = apertureService;
 		scope.closeBanner = closeBanner;
 		scope.openApp = openApp;
+
+		apertureWatch = scope.$watch('aperture.state', function(newVal, oldVal) {
+			if (newVal === 'aperture-full') {
+				hideBanner();
+			}
+		});
 
 		_.defer(activate);
 
 		function activate() {
 			wrap = angular.element('.wrap');
-			nav = angular.element('.main-nav');
 			banner = angular.element('#download-banner');
-			home = angular.element('.home');
-			shelf = angular.element('#shelf');
-
+			viewContainer = angular.element('#view-container');
 			setScroll(wrap);
-			nav.addClass('banner-offset');
 		}
 
 		routeListener = $rootScope.$on('$routeChangeSuccess', function() {
-			wrap.off('scroll');
+			if (wrap) {
+				wrap.off('scroll');
+			}
 			_.defer(function() {
 				activate();
+				showBanner();
 			});
 		});
 
@@ -52,15 +60,9 @@ function downloadBanner($window, $rootScope) {
 		var throttledScroll = _.throttle(function() {
 			var st = this.scrollTop;
 			if (st > 0) {
-				nav.removeClass('banner-offset');
-				banner.removeClass('banner-offset');
-				home.addClass('banner-adjust-up');
-				shelf.addClass('banner-adjust-up');
+				hideBanner();
 			} else {
-				nav.addClass('banner-offset');
-				banner.addClass('banner-offset');
-				home.removeClass('banner-adjust-up');
-				shelf.removeClass('banner-adjust-up');
+				showBanner();
 			}
 		}, 100);
 
@@ -71,22 +73,35 @@ function downloadBanner($window, $rootScope) {
 
 		function cleanup() {
 			nav.removeClass('banner-offset');
-			home.addClass('banner-adjust-up');
 			wrap.off('scroll', throttledScroll);
 			routeListener();
+			apertureWatch();
 		}
 
-		// this does not work yet. possible option: https://github.com/philbot5000/CanOpen
-		function openApp() {
-			try {
-				console.log('trying co.kipapp://')
-				$window.open('co.kipapp://');
-			}
-			catch(err) {
-				console.log('Caught error', err)
-				// $location.path()
-			}
+
+		function hideBanner() {
+			viewContainer.css('height', '100vh');
+			viewContainer.css('margin-top', '-80px');
+			nav.removeClass('banner-offset');
+			banner.removeClass('banner-offset');
+			banner.removeClass('banner-adjust-up');
 		}
-		
+
+		function showBanner() {
+			var screenHeight = window.screen.height;
+			viewContainer.css('height', screenHeight - 80 + 'px');
+			viewContainer.css('margin-top', '0px');
+			nav.addClass('banner-offset');
+			banner.addClass('banner-offset');
+		}
+
+
+		// TODO check if app is installed on device
+		// https://github.com/philbot5000/CanOpen
+		// if yes, open app. if no, open link to app store
+		function openApp() {
+			$window.open('http://goo.gl/Lw6S3V');
+		}
+
 	}
 }
