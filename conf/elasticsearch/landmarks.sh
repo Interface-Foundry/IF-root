9932123872c0251f2b953389370cf5e317b5db06
Peter Brandt <peter.m.brandt@gmail.com>
2015-06-08 06:13:05 -0400

    use mongo-connector python thing instead of the elasticsearch river

--- conf/elasticsearch/landmarks.sh
+++ conf/elasticsearch/landmarks.sh
@@ -5,7 +5,6 @@ set -e
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 # first kill all things if they exist
-curl -XDELETE localhost:9200/_river/landmark_river
 curl -XDELETE localhost:9200/if
 
 echo
@@ -13,11 +12,11 @@ echo "sleeping for 5s while clean up executes"
 
 sleep 5
 
-# create the index
+# create the elasticsearch index with a geo mapping
 bash "$DIR"/landmarks/landmarks_index.sh
 
-# create the river
-bash "$DIR"/landmarks/landmarks_river.sh
+# initialize the connector
+nohup mongo-connector -c "$DIR"/landmarks/mongo-connector-config-localhost.json > "$DIR"/mongo-connector.log 2>&1 &
 
 echo
 echo "sleeping for 10s while the river initializes"
