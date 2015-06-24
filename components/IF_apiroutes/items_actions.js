c7e5058ce4842da6ec6db2b98f9f1e36eb458b5a
muchimoto <muchimoto@gmail.com>
2015-06-24 16:43:46 -0400

    routes for adding and deleting tags

--- components/IF_apiroutes/items_actions.js
+++ components/IF_apiroutes/items_actions.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express.Router();
 var db = require('../IF_schemas/db');
-
-/**
- * This should be mounted at /api/items
- */
+var async = require('async')
+    /**
+     * This should be mounted at /api/items
+     */
 
 var mockItems = require('./mock_items.js');
 var USE_MOCK_DATA = true;
@@ -47,7 +47,7 @@ app.post('/:mongoId/comment', function(req, res) {
     if (USE_MOCK_DATA) {
         return res.send(defaultResponse);
     }
-    var comment = new db.worldchat();
+    var comment = new db.Worldchat();
     comment.roomID = req.params.mongoId;
     comment.userID = req.user._id;
     comment.msg = req.body.msg;
@@ -63,24 +63,68 @@ app.post('/:mongoId/deletecomment', function(req, res) {
     if (USE_MOCK_DATA) {
         return res.send(defaultResponse);
     }
-    db.worldchat.findOne({
+    db.Worldchat.findOne({
         '_id': req.params.mongoId
     }).remove(function(err, comment) {
         if (err) return next(err)
-          console.log('comment deleted.')
+        console.log('comment deleted.')
+        res.sendStatus(200);
     })
 });
 
+//front-end will send tags object in post body
+// {
+//   categories: [],
+//   text: []
+//  }
+//note: cloudsight will pull color 
+//which will be auto-matched on backend to nearest color available
 app.post('/:mongoId/tag', function(req, res) {
     if (USE_MOCK_DATA) {
         return res.send(defaultResponse);
     }
+    var tagObj = req.body.tags
+    db.Landmarks.findOne({
+        '_id': req.params.mongoId
+    }, function(err, item) {
+        if (err) return next(err)
+        for (var type in tagObj) {
+            if (tagObj.hasOwnProperty(type)) {
+                tagObj[type].forEach(function(tag) {
+                    item.itemTags[type].push(tag)
+                })
+            }
+        }
+        item.save(function(err, item) {
+            if (err) return next(err)
+            res.sendStatus(200);
+            console.log('Tags added: ', item.itemTags)
+        })
+    })
 });
-
+//front-end will send array of tag strings to delete in post body
 app.post('/:mongoId/deletetag', function(req, res) {
     if (USE_MOCK_DATA) {
         return res.send(defaultResponse);
     }
+    db.Landmarks.findOne({
+        '_id': req.params.mongoId
+    }, function(err, item) {
+        if (err) return next(err)
+        req.body.tags.forEach(function(tagToDelete) {
+            var i = item.itemTags.text.length;
+            while (i--) {
+                if (item.itemTags.text[i] == tagToDelete) {
+                    item.itemTags.text.splice(i, 1);
+                }
+            }
+        })
+        item.save(function(err, item) {
+            if (err) return next(err)
+            res.sendStatus(200);
+            console.log('Tags added.')
+        })
+    })
 });
 
 app.post('/:mongoId/fav', function(req, res) {
