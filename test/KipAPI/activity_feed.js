7c16c1e8c14385e36c203261bbd2f30acc00e9d1
muchimoto <muchimoto@gmail.com>
2015-06-29 17:13:05 -0400

    activity feed tests for snap, comment, fave and follow

--- test/KipAPI/activity_feed.js
+++ test/KipAPI/activity_feed.js
@@ -4,182 +4,247 @@ var TestLocations = require('../TestLocations');
 var should = require('chai').should();
 var mockItems = require('./mock_items');
 
-
 var peachId = UserTools.users.peach._id.toString();
+var peach = UserTools.users.peach;
+var defaultResponse = {
+    status: '(⌒‿⌒)'
+};
+
 
 // helpers to get latest activity
 var getMyLatestActivity = function(callback) {
-  browser.get('/api/users/'+peachId+'/activity/me', function(e, r, b) {
-    should.equal(e, null);
-    should.not.exist(b.err);
-    console.log(b.results);
-    callback(b.results[0]);
-  });
+    browser.get('/api/users/' + peachId + '/activity/me', function(e, r, b) {
+        should.equal(e, null);
+        should.not.exist(b.err);
+        console.log(b.results);
+        callback(b.results[0]);
+    });
 };
 
 var getFollowersLatestActivity = function(callback) {
-  browser.get('/api/users/'+peachId+'/activity/followers', function(e, r, b) {
-    should.equal(e, null);
-    should.not.exist(b.err);
-    callback(b.results.sort(function(a, b) {
-      return a.activityTime > b.activityTime;
-    })[0]);
-  });
+    browser.get('/api/users/' + peachId + '/activity/followers', function(e, r, b) {
+        should.equal(e, null);
+        should.not.exist(b.err);
+        callback(b.results.sort(function(a, b) {
+            return a.activityTime > b.activityTime;
+        })[0]);
+    });
 };
 
 describe.only('activity feed', function() {
 
-  //test item
-  var item;
-  // helper for getting the test item
-  var getTestItem = function(callback) {
-    browser.get('/api/items/' + item._id, function(e, r, body) {
-      callback(body);
-    });
-  };
-
-  UserTools.loginBefore(UserTools.users.peach);
-
-  it.only('should post an activity to my feed when I post a snap', function(done) {
-    var i = mockItems.getExample();
-    browser.post('/api/items/', {
-      body: i
-    }, function(e, r, body) {
-      item = body;
-      getMyLatestActivity(function(activity) {
-        activity.landmarkIds.should.contain(item._id.toString());
-        done();
-      });
-    });
-  });
-
-
-  // delete it after everything's done
-  after(function(done) {
-    browser.post('/api/items/' + item._id + '/delete', done);
-  });
-
-  describe.skip('doing actions while logged in', function() {
-    var peach;
-    before(function (done) {
-      UserTools.login(UserTools.users.peach, function (e, user) {
-        peach = user;
-        done();
-      });
-    });
-
-    it('should show an activity when peach faves an item', function (done) {
-      browser.post('/api/items/' + item._id + '/fave', function (e, r, body) {
-        getTestItem(function (item) {
-          var peachFavesIt = item.faves.reduce(function (p, o) {
-            return p || o.userId === peach._id.toString();
-          }, false);
-          peachFavesIt.should.equal(true);
-          UserTools.loggedIn(function (u) {
-            u.faves.should.contain(item._id.toString());
-            done();
-          });
+    //test item
+    var item;
+    // helper for getting the test item
+    var getTestItem = function(callback) {
+        browser.get('/api/items/' + item._id, function(e, r, body) {
+            callback(body);
         });
-      });
-    });
+    };
 
-    it('should allow peach to un-fave an item', function (done) {
-      browser.post('/api/items/' + item._id + '/unfave', function (e, r, body) {
-        getTestItem(function (item) {
-          var peachFavesIt = item.faves.reduce(function (p, o) {
-            return p || o.userId === peach._id.toString();
-          }, false);
-          peachFavesIt.should.equal(false);
-          UserTools.loggedIn(function (u) {
-            u.faves.should.not.contain(item._id.toString());
-            done();
-          });
+    describe('doing actions while logged in', function() {
+        var peach;
+        before(function(done) {
+            UserTools.login(UserTools.users.peach, function(e, user) {
+                peach = user;
+                done();
+            });
         });
-      });
-    });
 
-    var comment = {
-      comment: 'comment' + (Math.random()*100000000|0),
-      timeCommented: new Date()
-    };
-
-    it('should allow peach to comment on an item', function (done) {
-      browser.post('/api/items/' + item._id + '/comment', {
-        body: comment
-      }, function (e, r, body) {
-        getTestItem(function (item) {
-          console.log(item.comments);
-          var commentExists = item.comments.reduce(function (p, o) {
-            return p || (o.comment === comment.comment);
-          }, false);
-          commentExists.should.equal(true);
-          done();
-        })
-      });
-    });
-
-    it('should allow peach to delete her comment', function (done) {
-      browser.post('/api/items/' + item._id + '/deletecomment', {
-        body: comment
-      }, function (e, r, body) {
-        getTestItem(function (item) {
-          console.log(item.comments);
-          var commentExists = item.comments.reduce(function (p, o) {
-            return p || (o.comment === comment.comment);
-          }, false);
-          commentExists.should.equal(false);
-          done();
-        })
-      });
-    });
-
-
-    var itemTags = {
-      categories: ["category1", "category1000"],
-      text: ["superfluous", "melancholy"]
-    };
-
-    // from mock_items.js
-    var originalTags = {
-      colors: ["000000", "FFFFFF"],
-      categories: ["category1", "category2"],
-      text: ['tag1', 'tag2', 'reallyreallylongtag3']
-    };
-
-    var expectedTags = {
-      colors: ["000000", "FFFFFF"],
-      categories: ["category1", "category2", "category1000"],
-      text: ['tag1', 'tag2', 'reallyreallylongtag3', "superfluous", "melancholy"]
-    };
-
-    var expectedTagsAfterDelete = {
-      colors: ["000000", "FFFFFF"],
-      categories: ["category1", "category2", "category1000"],
-      text: ['tag1', 'tag2', 'reallyreallylongtag3', "melancholy"]
-    };
-
-    it('should allow peach to add tags to her item', function(done) {
-      browser.post('/api/items/' + item._id + '/tag', {
-        body: itemTags
-      }, function (e, r, body) {
-        should.not.exist(body.err);
-        getTestItem(function (item) {
-          item.itemTags.should.eql(expectedTags);
-          done();
+        it('should post an activity to my feed when I post a snap', function(done) {
+            var i = mockItems.getExample();
+            browser.post('/api/items/', {
+                body: i
+            }, function(e, r, body) {
+                item = body;
+                getMyLatestActivity(function(activity) {
+                    activity.privateVisible.should.equal(true);
+                    activity.publicVisible.should.equal(true);
+                    activity.userIds.should.contain(peachId);
+                    activity.landmarkIds.should.contain(item._id.toString());
+                    activity.activityAction.should.equal('item.post')
+                    activity.data.owner.mongoId.should.equal(item.owner.mongoId.toString());
+                    activity.data.owner.profileID.should.equal(item.owner.profileID);
+                    activity.data.owner.name.should.equal(item.owner.name);
+                    activity.data.item.mongoId.should.equal(item._id.toString())
+                    activity.data.item.id.should.equal(item.id)
+                    activity.data.item.name.should.equal(item.name)
+                    activity.data.item.itemImageURL.should.equal(item.ImageURL)
+                    done();
+                });
+            });
         });
-      });
-    });
 
-    it('should allow peach to remove tags from her item', function(done) {
-      browser.post('/api/items/' + item._id + '/deletetag', {
-        body: {type: 'text', value: 'superfluous'}
-      }, function (e, r, body) {
-        should.not.exist(body.err);
-        getTestItem(function (item) {
-          item.itemTags.should.eql(expectedTagsAfterDelete);
-          done();
+
+        it('should post an activity to my feed when I follow someone', function(done) {
+            var sonic = UserTools.users.sonic;
+            browser.post('/api/users/' + peachId + '/follow', {
+                body: sonic
+            }, function(e, r, body) {
+                body.should.equal(defaultResponse)
+                getMyLatestActivity(function(activity) {
+                    activity.privateVisible.should.equal(true);
+                    activity.publicVisible.should.equal(true);
+                    activity.userIds.should.contain(peachId.toString());
+                    activity.userIds.should.contain(sonic._id.toString());
+                    activity.activityAction.should.equal('user.follow')
+                    activity.data.follower.mongoId.should.equal(peach._id.toString());
+                    activity.data.follower.profileID.should.equal(peach.profileID);
+                    activity.data.follower.name.should.equal(peach.name);
+                    activity.data.followed.mongoId.should.equal(sonic._id.toString());
+                    activity.data.follower.profileID.should.equal(sonic.profileID);
+                    activity.data.follower.name.should.equal(sonic.name);
+                    done();
+                });
+            });
         });
-      });
-    })
-  });
+
+        it('should post a private activity to my feed when I unfollow someone', function(done) {
+            var sonic = UserTools.users.sonic;
+            browser.post('/api/users/' + peachId + '/unfollow', {
+                body: sonic
+            }, function(e, r, body) {
+                body.should.equal(defaultResponse)
+                getMyLatestActivity(function(activity) {
+                    activity.privateVisible.should.equal(true);
+                    activity.publicVisible.should.equal(false);
+                    activity.userIds.should.contain(peachId.toString());
+                    activity.userIds.should.contain(sonic._id.toString());
+                    activity.activityAction.should.equal('user.unfollow')
+                    activity.data.follower.mongoId.should.equal(peach._id.toString());
+                    activity.data.follower.profileID.should.equal(peach.profileID);
+                    activity.data.follower.name.should.equal(peach.name);
+                    activity.data.follower.avatar.should.equal(peach.avatar)
+                    activity.data.followed.mongoId.should.equal(sonic._id.toString());
+                    activity.data.followed.profileID.should.equal(sonic.profileID);
+                    activity.data.followed.name.should.equal(sonic.name);
+                    activity.data.followed.avatar.should.equal(sonic.avatar);
+                    done();
+                });
+            });
+        });
+
+        it('should show a private activity when peach faves an item', function(done) {
+            var i = mockItems.getExample();
+            browser.post('/api/items/' + i._id + '/fave', function(e, r, body) {
+                body.should.equal(defaultResponse)
+                getMyLatestActivity(function(activity) {
+                    activity.privateVisible.should.equal(true);
+                    activity.publicVisible.should.equal(false);
+                    activity.userIds.should.contain(peachId.toString());
+                    activity.userIds.should.contain(i.owner.mongoId.toString());
+                    activity.landmarkIds.should.contain(i._id.toString())
+                    activity.activityAction.should.equal('item.fave')
+                    activity.data.faver.mongoId.should.equal(peach._id.toString());
+                    activity.data.faver.profileID.should.equal(peach.profileID);
+                    activity.data.faver.name.should.equal(peach.name);
+                    activity.data.faver.avatar.should.equal(peach.avatar)
+                    activity.data.owner.mongoId.should.equal(i.owner.mongoId.toString());
+                    activity.data.owner.profileID.should.equal(i.owner.profileID);
+                    activity.data.owner.name.should.equal(i.owner.name);
+                    activity.data.item.mongoId.should.equal(i._id.toString())
+                    activity.data.item.id.should.equal(i.id)
+                    activity.data.item.name.should.equal(i.name)
+                    activity.data.item.itemImageURL.should.equal(i.ImageURL)
+                    done();
+                });
+            });
+        });
+
+        it('should show a private activity when peach unfaves an item', function(done) {
+            var i = mockItems.getExample();
+            browser.post('/api/items/' + i._id + '/unfave', function(e, r, body) {
+                body.should.equal(defaultResponse)
+                getMyLatestActivity(function(activity) {
+                    activity.privateVisible.should.equal(false);
+                    activity.publicVisible.should.equal(false);
+                    activity.userIds.should.contain(peachId.toString());
+                    activity.userIds.should.contain(i.owner.mongoId.toString());
+                    activity.landmarkIds.should.contain(i._id.toString())
+                    activity.activityAction.should.equal('item.unfave')
+                    activity.data.faver.mongoId.should.equal(peach._id.toString());
+                    activity.data.faver.profileID.should.equal(peach.profileID);
+                    activity.data.faver.name.should.equal(peach.name);
+                    activity.data.faver.avatar.should.equal(peach.avatar)
+                    activity.data.owner.mongoId.should.equal(i.owner.mongoId.toString());
+                    activity.data.owner.profileID.should.equal(i.owner.profileID);
+                    activity.data.owner.name.should.equal(i.owner.name);
+                    activity.data.item.mongoId.should.equal(i._id.toString())
+                    activity.data.item.id.should.equal(i.id)
+                    activity.data.item.name.should.equal(i.name)
+                    activity.data.item.itemImageURL.should.equal(i.ImageURL)
+                    done();
+                });
+            });
+        });
+
+        var comment = {
+            comment: 'comment' + (Math.random() * 100000000 | 0),
+            timeCommented: new Date()
+        };
+
+        it('should show an activity when peach comments on an item', function(done) {
+            var i = mockItems.getExample();
+            browser.post('/api/items/' + i._id + '/comment', {
+                body: comment
+            }, function(e, r, body) {
+                body.should.equal(defaultResponse)
+                getMyLatestActivity(function(activity) {
+                    activity.privateVisible.should.equal(true);
+                    activity.publicVisible.should.equal(true);
+                    activity.userIds.should.contain(peachId);
+                    activity.userIds.should.contain(i.owner.mongoId.toString());
+                    activity.landmarkIds.should.contain(i._id.toString())
+                    activity.data.commenter.mongoId.should.equal(peach._id.toString());
+                    activity.data.commenter.profileID.should.equal(peach.profileID);
+                    activity.data.commenter.name.should.equal(peach.name);
+                    activity.data.commenter.avatar.should.equal(peach.avatar)
+                    activity.data.owner.mongoId.should.equal(i.owner.mongoId.toString());
+                    activity.data.owner.profileID.should.equal(i.owner.profileID);
+                    activity.data.owner.name.should.equal(i.owner.name);
+                    activity.data.item.mongoId.should.equal(i._id.toString())
+                    activity.data.item.id.should.equal(i.id)
+                    activity.data.item.name.should.equal(i.name)
+                    activity.data.item.itemImageURL.should.equal(i.ImageURL)
+                    activity.activityAction.should.equal('item.comment')
+                    done();
+                });
+            });
+        });
+
+        it('should show a private activity when peach deletes her comment on an item', function(done) {
+            var i = mockItems.getExample();
+            browser.post('/api/items/' + i._id + '/deletecomment', {
+                body: comment
+            }, function(e, r, body) {
+                body.should.equal(defaultResponse)
+                getMyLatestActivity(function(activity) {
+                    activity.privateVisible.should.equal(false);
+                    activity.publicVisible.should.equal(false);
+                    activity.userIds.should.contain(peachId);
+                    activity.userIds.should.contain(i.owner.mongoId.toString());
+                    activity.landmarkIds.should.contain(i._id.toString())
+                    activity.data.commenter.mongoId.should.equal(peach._id.toString());
+                    activity.data.commenter.profileID.should.equal(peach.profileID);
+                    activity.data.commenter.name.should.equal(peach.name);
+                    activity.data.commenter.avatar.should.equal(peach.avatar)
+                    activity.data.owner.mongoId.should.equal(i.owner.mongoId.toString());
+                    activity.data.owner.profileID.should.equal(i.owner.profileID);
+                    activity.data.owner.name.should.equal(i.owner.name);
+                    activity.data.item.mongoId.should.equal(i._id.toString())
+                    activity.data.item.id.should.equal(i.id)
+                    activity.data.item.name.should.equal(i.name)
+                    activity.data.item.itemImageURL.should.equal(i.ImageURL)
+                    activity.activityAction.should.equal('item.deletecomment')
+                    done();
+                });
+            });
+        });
+
+        // it('should show an activity when peach faves a look', function(done) {
+        // });
+        // it('should show an activity when peach unfaves a look', function(done) {
+        // });
+
+    });
 });
--- test/UserTools.js
+++ test/UserTools.js
@@ -1,48 +1,60 @@
 var browser = require('browser');
 
 var users = {
-  kip: {},
-  peach: {
-    creds: {
-      email: 'princesspeach@interfacefoundry.com',
-      password: 'princesspeach'
+    kip: {},
+    peach: {
+        creds: {
+            email: 'princesspeach@interfacefoundry.com',
+            password: 'princesspeach'
+        },
+        _id: '55799f4a76256a9342b03bad',
+        profileID: 'peach101',
+        name: 'Princess Peach',
+        avatar: 'avatarlink'
     },
-    _id: '55799f4a76256a9342b03bad'
-  },
-  sonic: {
-    creds: {
-      email: 'sonic@interfacefoundry.com',
-      password: 'princesspeach'
-    },
-    _id: '558d819ca0d6b1f2c5421080'
-  }
+    sonic: {
+        creds: {
+            email: 'sonic@interfacefoundry.com',
+            password: 'princesspeach'
+        },
+        _id: '558d819ca0d6b1f2c5421080',
+        profileID: 'sonic101',
+        name: 'Sonic',
+        avatar: 'avatarlink'
+    }
 };
 
 module.exports = {
-  logout: function(done) {
-    browser.get('/api/user/logout', function(e, r, body) {
-      done();
-    })
-  },
-  logoutBefore: function() {
-    before(function(done) {
-      module.exports.logout(done);
-    });
-  },
-  login: function(user, done) {
-    browser.post({url: '/api/user/login', json: true, body:  user.creds}, function(e, r, body) {
-      done(e, body);
-    });
-  },
-  loginBefore: function(user) {
-    before(function(done) {
-      module.exports.login(user, done)
-    });
-  },
-  loggedIn: function(done) {
-    browser.get({url: '/api/user/loggedin'}, function(e, r, body) {
-      done(body);
-    });
-  },
-  users: users
+    logout: function(done) {
+        browser.get('/api/user/logout', function(e, r, body) {
+            done();
+        })
+    },
+    logoutBefore: function() {
+        before(function(done) {
+            module.exports.logout(done);
+        });
+    },
+    login: function(user, done) {
+        browser.post({
+            url: '/api/user/login',
+            json: true,
+            body: user.creds
+        }, function(e, r, body) {
+            done(e, body);
+        });
+    },
+    loginBefore: function(user) {
+        before(function(done) {
+            module.exports.login(user, done)
+        });
+    },
+    loggedIn: function(done) {
+        browser.get({
+            url: '/api/user/loggedin'
+        }, function(e, r, body) {
+            done(body);
+        });
+    },
+    users: users
 };
