4032ad8ac04797bab2e2468349141019b05cfdad
muchimoto <muchimoto@gmail.com>
2015-07-09 10:58:20 -0400

    upload service now skips temp file creation and uploads directly to s3

--- IF_services/upload.js
+++ IF_services/upload.js
@@ -3,45 +3,34 @@
 var express = require('express'),
     router = express.Router(),
     _ = require('underscore'),
-    fs = require('fs'),
     q = require('q'),
     async = require('async'),
     AWS = require('aws-sdk')
 
 module.exports = {
-    //Upload pictures for looks
+    //Upload pictures to Amazon S3 for snaps and looks
     uploadPicture: function(profileID, base64) {
         var deferred = q.defer();
+        var buffer = new Buffer(base64.replace(/^data:image\/\w+;base64,/, ""), 'base64')
         var stuff_to_hash = profileID + (new Date().toString());
         var object_key = crypto.createHash('md5').update(stuff_to_hash).digest('hex');
         var fileType = base64.split(';')[0].split('/')[1];
         var date_in_path = (new Date().getUTCFullYear()) + "/" + (new Date().getUTCMonth()) + "/"
         var current = object_key + "." + fileType;
-        var tempPath = "app/dist/temp_general_uploads/" + current;
         var awsKey = date_in_path + current;
-        var writeStream = fs.createWriteStream(tempPath);
-        fs.createReadStream(base64, {
-            encoding: 'base64'
-        }).pipe(writeStream);
-        writeStream.on('close', function() {
-                fs.readFile(tempPath, function(err, fileData) {
-                    if (err) deferred.reject(err)
-                    var s3 = new AWS.S3();
-                    s3.putObject({
-                        Bucket: 'if-server-general-images',
-                        Key: awsKey,
-                        Body: fileData,
-                        ACL: 'public-read'
-                    }, function(err, data) {
-                        if (err) deferred.reject(err)
-                        else {
-                            fs.unlink(tempPath);
-                            var imgURL = "https://s3.amazonaws.com/if-server-general-images/" + awsKey
-                            deferred.resolve(imgURL)
-                        }
-                    }); //s3
-                }); //fs.readFile
-            }) //writeStream.on
+        var s3 = new AWS.S3();
+        s3.putObject({
+            Bucket: 'if-server-general-images',
+            Key: awsKey,
+            Body: buffer,
+            ACL: 'public-read'
+        }, function(err, data) {
+            if (err) deferred.reject(err)
+            else {
+                var imgURL = "https://s3.amazonaws.com/if-server-general-images/" + awsKey
+                deferred.resolve(imgURL)
+            }
+        });
         return deferred.promise
     }
 }
