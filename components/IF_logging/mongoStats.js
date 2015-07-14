57b513ed5ad937814feb11360af429c6e58e2b14
Peter Brandt <peter.m.brandt@gmail.com>
2015-07-14 12:42:33 -0400

    updating logging

--- components/IF_logging/mongoStats.js
+++ components/IF_logging/mongoStats.js
@@ -4,6 +4,15 @@ var logger = require('./if_logger');
 db.map(function(schema) {
     schema.collection.stats(function(e, s) {
         if (typeof s !== 'undefined') {
+            // collapse some data into single strings because it's annoying in kibana
+            if (s.indexDetails) {
+                s.indexDetails = JSON.stringify(s.indexDetails);
+            }
+
+            if (s.wiredTiger) {
+                s.wiredTiger = JSON.stringify(s.wiredTiger);
+            }
+
             logger.log(s)
         }
     })
