da21f2c5914ca758a26cdbbaebf876dc9803bf83
Peter Brandt <peter.m.brandt@gmail.com>
2015-07-14 21:08:00 -0400

    scrape scrape scrape

--- IF_services/IF_forage/shoptiques/clearAlreadyScraped.js
+++ IF_services/IF_forage/shoptiques/clearAlreadyScraped.js
@@ -4,12 +4,12 @@ var db = require('db');
 var redis = require('./redis');
 
 
-db.Landmarks.find({'source_shoptique_item.url': {$exists: true}})
-    .select('source_shoptique_item')
+db.Landmarks.find({'source_shoptiques_item.url': {$exists: true}})
+    .select('source_shoptiques_item')
     .exec()
     .then(function(r) {
         r.map(function(a) {
-            var url = a._doc.source_shoptique_item.url;
+            var url = a._doc.source_shoptiques_item.url;
             redis.lrem('items-toprocess', 0, url, function(e, r) {
                 console.log(url, e, r);
             });
