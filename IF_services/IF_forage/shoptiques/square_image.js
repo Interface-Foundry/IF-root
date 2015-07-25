a890766155848e576bfe5552e05897262f2232d1
Peter Brandt <peter.m.brandt@gmail.com>
2015-07-25 14:06:31 -0400

    better error handling :/

--- IF_services/IF_forage/shoptiques/square_image.js
+++ IF_services/IF_forage/shoptiques/square_image.js
@@ -1,6 +1,7 @@
 var spawn = require('child_process').spawn;
 var db = require('db');
 var fs = require('fs');
+var Promise = require('bluebird');
 
 var AWS = require('aws-sdk');
 var bucketName = 'if.kip.apparel.images';
@@ -18,13 +19,13 @@ var args = "$SRC -resize 640X640> -size 640X640 xc:white +swap -gravity center -
 
 db.Landmarks.findOne({
     world: false,
-    itemImageURL: /shoptiques.net/,
+    'source_shoptiques_item.images': {$exists: true},
     loc: {
         $near: {
             $geometry: {
                 type: "Point" ,
-                //coordinates: [ -73.990638 , 40.7352793 ]
-                coordinates: [ -77.0433437 , 38.9095334 ]
+                coordinates: [ -73.990638 , 40.7352793 ]
+                //coordinates: [ -77.0433437 , 38.9095334 ]
             },
             $maxDistance: 10000
         }
@@ -35,49 +36,53 @@ db.Landmarks.findOne({
         process.exit(0);
     }
 
-    var tasks = 2;
-
     item.source_shoptiques_item.images = item.source_shoptiques_item.images.map(function (u) {
         return u.replace('_m.jpg', '_l.jpg');
     });
 
-    item.itemImageURL = item.source_shoptiques_item.images.map(function (u) {
+    Promise.settle(item.source_shoptiques_item.images.map(function (u) {
+        return new Promise(function(resolve, reject) {
 
-        var key = item.parent.id + '/' + u.split('/').pop();
-        var newUrl = bucketUrlPrefex + key;
-        var a = args.replace('$SRC', u).split(' ');
-        var s = spawn(cmd, a, {stdio: ['pipe', 'pipe', process.stderr]});
+            var key = item.parent.id + '/' + u.split('/').pop();
+            var newUrl = bucketUrlPrefex + key;
+            var a = args.replace('$SRC', u).split(' ');
+            var s = spawn(cmd, a, {stdio: ['pipe', 'pipe', process.stderr]});
 
-        s3.upload({
-            Bucket: bucketName,
-            Key: key,
-            Body: s.stdout,
-            ACL: 'public-read'
-        }, function (err, data) {
-            tasks--;
-            if (err) {
-                console.error('Error uploading ' + newUrl);
-                console.error(err);
-                process.exit(1);
-            }
+            s3.upload({
+                Bucket: bucketName,
+                Key: key,
+                Body: s.stdout,
+                ACL: 'public-read'
+            }, function (err, data) {
+                if (err || s.exitCode !== 0) {
+                    console.error('Error uploading ' + newUrl);
+                    console.error(err);
+                    return reject(err);
+                }
 
-            console.log('Uploaded ' + newUrl);
-            if (tasks === 0) {
-                process.exit(0);
+                console.log('Uploaded ' + newUrl);
+                resolve(newUrl);
+            });
+
+            return newUrl;
+        });
+    })).then(function(results) {
+        item.itemImageURL = results.map(function(r, i) {
+            if (r.isFulfilled()) {
+                return r.value();
+            } else {
+                return item.source_shoptiques_item.images[i];
             }
         });
 
-        return newUrl;
+        item.save(function (e) {
+            if (e) {
+                console.error(e);
+                process.exit(1);
+            }
+
+            process.exit(0);
+        });
     });
 
-    item.save(function (e) {
-        tasks--;
-        if (e) {
-            console.error(e);
-            process.exit(1);
-        }
-        if (tasks === 0) {
-            process.exit(0);
-        }
-    });
 });
