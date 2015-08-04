38a4f996bb21ad23baeeeca68fa146633ec10e38
Peter Brandt <peter.m.brandt@gmail.com>
2015-08-04 10:41:03 -0500

    adding color tools

---
+++ IF_services/ColorIndex/colorTools.js
@@ -0,0 +1,80 @@
+var Vibrant = require('node-vibrant'); // make sure to npm install pbrandt1/node-vibrant
+var Promise = require('bluebird');
+
+/**
+ * Finds the vibrant colors for an image
+ * vibrant
+ * @param url
+ * @returns {Promise} {vibrant: {h: Hue, s: Saturation, l, Luminosity}}
+ */
+var getVibrantColors = function(url) {
+    return new Promise(function(resolve, reject) {
+        var v = new Vibrant(url);
+        v.getSwatches(function(e, s) {
+            if (e) {
+                reject(e);
+            }
+            resolve(s);
+        });
+    });
+};
+
+/**
+ * Gets the vibrant colors for all images in a landmark.
+ * Does not save/mutate the landmark or access the db in any way
+ * @param landmark
+ * @returns {Promise} array of vibrant swatches
+ *  [{Vibrant: ]
+ */
+var findColorsForLandmark = function(landmark) {
+    return Promise.settle(landmarks.itemImageUrl.map(function(url) {
+        return getVibrantColors(url);
+    })).then(function(results) {
+        return results.reduce(function(colors, r) {
+            if (r.isFulfilled()) {
+                colors.push(r.value());
+            }
+            return colors;
+        }, [])
+    });
+};
+
+/**
+ * Simple test
+ */
+var test = function() {
+    var should = require('chai').should();
+
+    //var url = 'https://s3.amazonaws.com/if.kip.apparel.images/annas2198/2de2cf2c-61f9-4ada-b770-6a572898e432_m.jpg';
+    var url = '/Users/peter/Downloads/2899260_fpx.jpeg';
+    getVibrantColors(url)
+        .then(function (colors) {
+            colors.should.have.property('Vibrant');
+            console.log(colors.Vibrant);
+        }).catch(console.error.bind(console));
+
+    var db = require('db');
+    db.Landmarks.findOne({'itemImageUrls.0': {$exists: true}})
+        .then(function(l) {
+            return findColorsForLandmark(l);
+        }).then(function(colors) {
+            colors.should.be.an.array
+            colors[0].should.have.property('Vibrant');
+            console.log(colors);
+        });
+}
+
+/**
+ * Expose a both the main module and the test function
+ * @type {{getImageHSL: Function, findColorsForLandmark: Function, test: Function}}
+ */
+module.exports = {
+    getVibrantColors: getVibrantColors,
+    findColorsForLandmark: findColorsForLandmark,
+    test: test
+};
+
+// by default run tests if not require'd
+if (!module.parent) {
+    test()
+}
