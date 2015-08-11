e32cfcb12d30496cca2e90bcbc859d44c338795a
Peter Brandt <peter.m.brandt@gmail.com>
2015-08-11 09:00:44 -0500

    kue-ifying square image process

--- IF_services/IF_forage/shoptiques/square_image.js
+++ IF_services/IF_forage/shoptiques/square_image.js
@@ -2,6 +2,7 @@ var spawn = require('child_process').spawn;
 var db = require('db');
 var fs = require('fs');
 var Promise = require('bluebird');
+var job = require('job');
 
 var AWS = require('aws-sdk');
 var bucketName = 'if.kip.apparel.images';
@@ -17,72 +18,70 @@ var cmd = "convert";
 var args = "$SRC -resize 640X640> -size 640X640 xc:white +swap -gravity center -composite jpeg:-";
 
 
-db.Landmarks.findOne({
-    world: false,
-    'source_shoptiques_item.images': {$exists: true},
-    loc: {
-        $near: {
-            $geometry: {
-                type: "Point" ,
-                coordinates: [ -73.990638 , 40.7352793 ]
-                //coordinates: [ -77.0433437 , 38.9095334 ]
-            },
-            $maxDistance: 10000
-        }
-    }
-}, function(err, item) {
-    if (!item) {
-        console.log('no item');
-        process.exit(0);
-    }
+var squareImage = job('square-shoptiques-image', function(data, done) {
+    db.Landmarks.findById(data.id).exec(function(e, item) {
 
-    item.source_shoptiques_item.images = item.source_shoptiques_item.images.map(function (u) {
-        return u.replace('_m.jpg', '_l.jpg');
-    });
+        // make sure the source image is the highest quality. the actual used image will be smaller, though
+        item.source_shoptiques_item.images = item.source_shoptiques_item.images.map(function (u) {
+            return u.replace('_m.jpg', '_l.jpg');
+        });
 
-    Promise.settle(item.source_shoptiques_item.images.map(function (u) {
-        return new Promise(function(resolve, reject) {
+        // process all the images
+        Promise.settle(item.source_shoptiques_item.images.map(function (u) {
+            return new Promise(function(resolve, reject) {
 
-            var key = item.parent.id + '/' + u.split('/').pop();
-            var newUrl = bucketUrlPrefex + key;
-            var a = args.replace('$SRC', u).split(' ');
-            var s = spawn(cmd, a, {stdio: ['pipe', 'pipe', process.stderr]});
+                var key = item.parent.id + '/' + u.split('/').pop();
+                var newUrl = bucketUrlPrefex + key;
+                var a = args.replace('$SRC', u).split(' ');
+                var s = spawn(cmd, a, {stdio: ['pipe', 'pipe', process.stderr]});
 
-            s3.upload({
-                Bucket: bucketName,
-                Key: key,
-                Body: s.stdout,
-                ACL: 'public-read'
-            }, function (err, data) {
-                if (err || s.exitCode !== 0) {
-                    console.error('Error uploading ' + newUrl);
-                    console.error(err);
-                    return reject(err);
+                s3.upload({
+                    Bucket: bucketName,
+                    Key: key,
+                    Body: s.stdout,
+                    ACL: 'public-read'
+                }, function (err, data) {
+                    if (err || s.exitCode !== 0) {
+                        console.error('Error uploading ' + newUrl);
+                        console.error(err);
+                        return reject(err);
+                    }
+
+                    console.log('Uploaded ' + newUrl);
+                    resolve(newUrl);
+                });
+
+                return newUrl;
+            });
+        })).then(function(results) {
+            item.itemImageURL = results.map(function(r, i) {
+                if (r.isFulfilled()) {
+                    return r.value();
+                } else {
+                    return item.source_shoptiques_item.images[i];
                 }
-
-                console.log('Uploaded ' + newUrl);
-                resolve(newUrl);
             });
 
-            return newUrl;
-        });
-    })).then(function(results) {
-        item.itemImageURL = results.map(function(r, i) {
-            if (r.isFulfilled()) {
-                return r.value();
-            } else {
-                return item.source_shoptiques_item.images[i];
-            }
-        });
+            item.save(function (e) {
+                if (e) {
+                    console.error(e);
+                    done(e);
+                }
 
-        item.save(function (e) {
-            if (e) {
-                console.error(e);
-                process.exit(1);
-            }
-
-            process.exit(0);
+                done(null, {itemImageURL: item.itemImageURL});
+            });
         });
+    })
+});
+
+db.Landmarks.find({
+    world: false,
+    'source_shoptiques_item.images': {$exists: true},
+    'itemImageURL.0': /shoptiques/
+}).select('_id').exec(function(err, items) {
+    items.map(function(i) {
+        squareImage({
+            id: i._id.toString()
+        })
     });
-
 });
