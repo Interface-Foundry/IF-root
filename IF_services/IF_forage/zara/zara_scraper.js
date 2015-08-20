dd78bbe686cc2137fa2e965b3ab08c7edde6cf63
muchimoto <muchimoto@gmail.com>
2015-08-20 17:19:11 -0400

    Scraping item details page complete

--- IF_services/IF_forage/zara/zara_scraper.js
+++ IF_services/IF_forage/zara/zara_scraper.js
@@ -6,18 +6,99 @@ var async = require('async');
 var uniquer = require('../../uniquer');
 var request = require('request')
 
-function getItem() {
+var Stores = []
+var url = 'http://www.zara.com/us/en/woman/tops/view-all/pleated-top-c733890p2776295.html';
+
+async.waterfall([
+    function(callback) {
+        checkIfScraped(url).then(callback(null,url)).catch(function(err) {
+            callback(err)
+        })
+    },
+    function(url,callback) {
+        getItem(url).then(function(item) {
+            callback(null, item)
+        }).catch(function(err) {
+            callback(err)
+        })
+    },
+    function(item, callback) {
+        getLocations(item).then(function(item) {
+            callback(null, item)
+        }).catch(function(err) {
+            callback(err)
+        })
+    },
+    function(item, callback) {
+        getInventory(item).then(function(inventory) {
+            callback(null, item, inventory)
+        }).catch(function(err) {
+            callback(err)
+        })
+    },
+    function(item, inventory, callback) {
+        updateInventory(inventory, item).then(function(item) {
+            callback(null, item)
+        }).catch(function(err) {
+            callback(err)
+        })
+    },
+    function(item, callback) {
+        saveStores(item).then(function(item) {
+            callback(null, item)
+        }).catch(function(err) {
+            callback(err)
+        })
+    },
+    function(item, callback) {
+        saveItems(item).then(function(items) {
+            callback(null, items)
+        }).catch(function(err) {
+            callback(err)
+        })
+    }
+], function(err, items) {
+    if (err) {
+        console.log(err)
+    }
+    console.log('finished scraping item!!', items)
+});
+
+
+function checkIfScraped(url) {
+    // first check if we have already scraped this thing
     return new Promise(function(resolve, reject) {
-        var url = 'http://www.zara.com/us/en/woman/blazers/crepe-blazer-c756615p2897570.html';
+        db.Landmarks
+            .findOne({
+                'source_zara_item.src': url.trim()
+            })
+            .exec(function(e, l) {
+                if (l) {;
+                    reject('Item already exists!')
+                }
+                if (!l) {
+                    return resolve(url)
+                }
+                if (e) {
+                    //if some mongo error happened here just go ahead with the process
+                    resolve(url)
+                }
+            })
+    })
+}
+
+
+function getItem(url) {
+    return new Promise(function(resolve, reject) {
         //construct newItem object
         var newItem = {
-            src: url, //drop in URL we're scraping from
+            src: url, 
             images: [],
             physicalStores: []
         };
 
         var options = {
-            url: 'http://www.zara.com/us/en/woman/blazers/crepe-blazer-c756615p2897570.html',
+            url: url,
             headers: {
                 'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13'
             }
@@ -71,7 +152,6 @@ function getItem() {
                     console.log('response.statusCode: ', response.statusCode)
                 }
             }
-
         })
     })
 }
@@ -85,20 +165,17 @@ function getLocations(newItem) {
         var options = {
             url: 'http://www.zara.com/webapp/wcs/stores/servlet/StoreLocatorResultPage?showOnlyDeliveryShops=false&isPopUp=false&storeCountryCode=US&catalogId=' + catalogId + '&country=US&categoryId=' + newItem.category + '&langId=-1&showSelectButton=true&storeId=' + newItem.storeId + '&latitude=' + lat + '&longitude=' + lng + '&ajaxCall=true',
             headers: {
-                'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13'
+                'User-Agent': 'Mozilla/5.0 (Windows; U; Windows 8; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13'
             }
         };
 
         request(options, function(error, response, body) {
             if ((!error) && (response.statusCode == 200)) {
                 $ = cheerio.load(body); //load HTML
-                // console.log('body:', body)
                 async.eachSeries($('li'), function(li, callback1) {
-                        // console.log('li', li)
                         var count = 0;
                         var newPhysicalStore = {};
                         async.eachSeries(li.children, function(elem, callback2) {
-                                // console.log('!!!!Elem.attribs', elem.attribs)
                                 if (!elem.attribs) return callback2()
 
                                 if (elem.attribs.class == 'lat') {
@@ -111,6 +188,9 @@ function getLocations(newItem) {
                                     newPhysicalStore.shopType = elem.attribs.value;
                                 }
                                 if (elem.attribs.class == 'storeId') {
+                                    newItem.physicalStores.push({
+                                        zaraStoreId: elem.attribs.value
+                                    })
                                     newPhysicalStore.storeId = elem.attribs.value;
                                 }
                                 if (elem.attribs.class == 'storeAddress') {
@@ -131,27 +211,12 @@ function getLocations(newItem) {
                                 if (elem.attribs.class == 'storeSections') {
                                     newPhysicalStore.storeSections = elem.attribs.value;
                                 }
-                                saveStore(newPhysicalStore).then(function(store) {
-                                    // console.log('Saved store:', store)
-                                    if (newPhysicalStore.storeId !== undefined) {
-                                        // console.log('pushing.')
-                                        newItem.physicalStores.push({
-                                            mongoId: store._id,
-                                            zaraStoreId: newPhysicalStore.storeId
-                                        });
-                                    }
-                                    count++;
-                                    callback2()
-                                }).catch(function(err) {
-                                    console.log(err)
-                                })
-
+                                Stores.push(newPhysicalStore)
+                                callback2()
                             }, function(err) {
                                 if (err) {
                                     console.log('Async inner each err: ', err)
                                 }
-
-
                                 callback1()
                             }) //End of inner each
                     },
@@ -159,12 +224,18 @@ function getLocations(newItem) {
                         if (err) {
                             console.log('Async outer each err: ', err)
                         }
+                        //Get rid of duplicates
                         newItem.physicalStores = newItem.physicalStores.filter(function(val, i, array) {
                             if (i !== 0) {
                                 return array[i].zaraStoreId !== array[i - 1].zaraStoreId
                             }
                         })
-                        console.log('Done processing stores.', newItem.physicalStores)
+                        Stores = Stores.filter(function(val, i, array) {
+                            if (i !== 0) {
+                                return array[i].storeId !== array[i - 1].storeId
+                            }
+                        })
+                        // console.log('Done processing stores.', Stores)
                         resolve(newItem)
                     }); //End of outer each
             } else {
@@ -176,13 +247,12 @@ function getLocations(newItem) {
     })
 }
 
-
 function getInventory(newItem) {
     var result;
     var storeIds = newItem.physicalStores.map(function(obj) {
-        return obj.zaraStoreId
-    })
-    console.log('storeIds: ', storeIds)
+            return obj.zaraStoreId
+        })
+
     return new Promise(function(resolve, reject) {
         var url = 'http://itxrest.inditex.com/LOMOServiciosRESTCommerce-ws/common/1/stock/campaign/' + newItem.campaign + '/product/part-number/' + newItem.partNumber + '?physicalStoreId=' + storeIds.join() + '&ajaxCall=true'
         var options = {
@@ -193,142 +263,133 @@ function getInventory(newItem) {
         };
         request(options, function(error, response, body) {
             if ((!error) && (response.statusCode == 200)) {
+                body = JSON.parse(body)
+                if (!body.stocks) {
+                    console.log('no stocks property in body getInventory()')
+                }
                 var inventory = body
                 resolve(inventory)
             } else {
                 if (error) {
-                    console.log('error: ', error)
+                    console.log('getinventory error ')
                     reject(error)
                 } else {
-                	  console.log('response:', response)
+                    console.log('bad response')
                     reject('Bad response from inventory request')
                 }
             }
         })
+
     })
 }
 
-function saveItem(newItem) {
+function updateInventory(inventory, newItem) {
     return new Promise(function(resolve, reject) {
-        var i = new db.Landmark();
-        i.source_zara_item = newItem;
-        i.save(function(e, i) {
-            if (e) {
-                console.error(e);
-                return reject(e)
-            }
-            // console.log(JSON.stringify(i.toObject()));
-            resolve(i);
-        })
+        console.log('')
+        if (inventory.stocks && inventory.stocks.length > 0) {
+            inventory.stocks.forEach(function(stock) {
+                newItem.physicalStores.forEach(function(store) {
+                    // console.log('stock.physicalStoreId', stock.physicalStoreId, 'store.zaraStoreId', store.zaraStoreId)
+                    if (stock.physicalStoreId.toString().trim() == store.zaraStoreId.toString().trim()) {
+                        // console.log('MATCH')
+                        store.inventory = stock.sizeStocks;
+                    }
+                })
+            })
+            resolve(newItem)
+        } else {
+            console.log('no inventory? ', inventory)
+            resolve(newItem)
+        }
     })
 }
 
-function checkItem(url) {
-    // first check if we have already scraped this thing
-    return new Promise(function(resolve, reject) {
-        db.Landmarks
-            .findOne({
-                'source_zara_item.url': url
-            })
-            .exec(function(e, l) {
-                if (e) {
-                    // console.error(e);
-                    resolve()
-                }
-                if (l) {
-                    // console.log('already found', data.url);
-                    reject('Item already exists!')
-                }
-            })
-    })
-}
 
-function saveStore(store) {
-    // check if we have store in db already
+
+function saveStores(item) {
     return new Promise(function(resolve, reject) {
-        db.Landmarks
-            .findOne({
-                'source_zara_store.storeId': store.storeId
-            })
-            .exec(function(e, s) {
-                if (e) {
-                    //error
-                    console.log('249', e)
-                    reject(e)
-                }
-                if (!s) {
-                    var n = new db.Landmark();
-                    n.source_zara_store = store;
-                    n.world = true;
-                    n.hasloc = true;
-                    n.loc.coordinates[0] = parseFloat(store.lng);
-                    n.loc.coordinates[1] = parseFloat(store.lat);
-                    uniquer.uniqueId('zara_' + store.storeAddress, 'Landmark').then(function(output) {
-                        n.id = output;
-                        n.save(function(e, newStore) {
-                            if (e) {
-                                console.error(e);
-                                return reject(e);
-                            }
-                            // console.log('Saved store! : ', newStore.source_zara_store);
-                            resolve(newStore);
+        var storeIds = []
+        var count = 0
+        async.each(Stores, function(store, callback) {
+            db.Landmarks
+                .findOne({
+                    'source_zara_store.storeId': store.storeId
+                })
+                .exec(function(e, s) {
+                    if (e) {
+                        //error
+                        console.log('Error in saveStores(): ', e)
+                        item.physicalStores[count].mongoId = 'null'
+                        count++;
+                        callback()
+                    }
+                    if (!s) {
+                        var n = new db.Landmark();
+                        n.source_zara_store = store;
+                        n.world = true;
+                        n.hasloc = true;
+                        console.log('LNG: ', parseFloat(store.lng), 'LAT: ', parseFloat(store.lat))
+                        n.loc.coordinates[0] = parseFloat(store.lng);
+                        n.loc.coordinates[1] = parseFloat(store.lat);
+                        uniquer.uniqueId('zara_' + store.storeAddress, 'Landmark').then(function(output) {
+                            n.id = output;
+                            n.save(function(e, newStore) {
+                                if (e) {
+                                    // console.error(e);
+                                    return callback()
+                                }
+                                item.physicalStores[count].mongoId = newStore._id
+                                count++;
+                                callback()
+                            })
                         })
-                    })
-                } else if (s) {
-                    // store exists
-                    console.log('Store exists.')
-                    resolve(s)
-                }
-            })
+                    } else if (s) {
+                        item.physicalStores[count].mongoId = s._id
+                        count++;
+                        callback()
+                    }
+                })
+        }, function(err) {
+            if (err) {
+                // console.log('Error in saveStores()',err)
+                return reject(err)
+            }
+            item.physicalStores = item.physicalStores.filter(function(val, i) {
+                    return val !== 'null'
+                })
+                // console.log('-_- Updated item: ', item)
+            resolve(item)
+        })
     })
 }
 
-async.waterfall([
-    // function(callback) {
-    //     checkItem().then(callback(null)).catch(callback('item exists'))
-    // },
-    function(callback) {
-        console.log(1)
-        getItem().then(function(item) {
-            callback(null, item)
-        }).catch(function(err) {
-            callback(err)
+function saveItems(newItem) {
+    return new Promise(function(resolve, reject) {
+        var savedItems = []
+        async.eachSeries(Stores, function(store, callback) {
+            var i = new db.Landmark();
+            i.source_zara_item = newItem;
+            i.hasloc = true;
+            // console.log('LNG: ', parseFloat(store.lng), 'LAT: ', parseFloat(store.lat))
+            i.loc.coordinates[0] = parseFloat(store.lng);
+            i.loc.coordinates[1] = parseFloat(store.lat);
+            uniquer.uniqueId(newItem.name, 'Landmark').then(function(output) {
+                i.id = output;
+                i.save(function(e, item) {
+                    if (e) {
+                        console.error(e);
+                        return callback();
+                    }
+                    savedItems.push(item)
+                    callback()
+                })
+            })
+        }, function(err) {
+            if (err) {
+                // console.log('Error in saveItems: ',err)
+                reject(err)
+            }
+            resolve(savedItems)
         })
-    },
-    function(item, callback) {
-        console.log(2)
-        getLocations(item).then(function(item) {
-            callback(null, item)
-        }).catch(function(err) {
-            callback(err)
-        })
-    },
-    function(item, callback) {
-        console.log(3)
-        getInventory(item).then(function(inventory) {
-            callback(null, inventory)
-        }).catch(function(err) {
-            callback(err)
-        })
-    },
-    function(inventory, callback) {
-        console.log(4)
-        console.log('INVENTORY: ', inventory)
-    }
-], function(err, result) {
-    console.log(5)
-    if (err) console.log(err)
-});
-
-
-// function scrape() {
-//     getItem().then(function(item) {
-//         getLocations(item).then(function(item) {
-//             getInventory(item)
-//         })
-//     }).catch(function(err) {
-//         console.log('ERROR: ', err)
-//     })
-// }
-
-// scrape()
+    })
+}
