bcba28f7522ab8600629608800ecb56843fa2f34
Peter Brandt <peter.m.brandt@gmail.com>
2015-09-10 16:20:09 -0400

    fixing error message

--- components/IF_auth/resetPassword/static/resetPassword.js
+++ components/IF_auth/resetPassword/static/resetPassword.js
@@ -12,9 +12,14 @@ $(function() {
                 password: $('#password').val(),
                 token: location.hash.split('/')[2]
             }),
-            success: function() {
+            success: function(xhr) {
                 $('p').remove();
-                $('form').parent().append('<p class="green">success</p>');
+                if (xhr.err) {
+                    $('form *').removeAttr('disabled');
+                    $('form').parent().append('<p class="red">' + xhr.err.niceMessage + '</p>');
+                } else {
+                    $('form').parent().append('<p class="green">success</p>');
+                }
             },
             error: function(xhr, errorType, error) {
                 $('p').remove();
