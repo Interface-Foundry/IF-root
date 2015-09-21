7865e1ce15cac872fb6ceb3a6dd4859c1137feff
Peter Brandt <peter.m.brandt@gmail.com>
2015-09-21 10:24:24 -0400

    adding addressText in shoptiques scraper

--- IF_services/IF_forage/shoptiques/scrape_shoptiques.js
+++ IF_services/IF_forage/shoptiques/scrape_shoptiques.js
@@ -57,7 +57,8 @@ var scrapeShoptiques = job('scrape-shoptiques-item', function (data, done) {
                                 name: boutique.name,
                                 id: boutique.name.replace(/[^\w]+/g, '').toLowerCase() + boutique.id,
                                 world: true,
-                                valid: true
+                                valid: true,
+                                addressText: boutique.addressText + ', ' + boutique.state
                             });
                             if (addr) {
                                 store.loc = {
