3272c3cd5d6257c688e7bc8d4c989e0574467d9f
Peter Brandt <peter.m.brandt@gmail.com>
2015-10-06 13:58:33 -0400

    fixing analytics schema

--- components/IF_schemas/analytics_schema.js
+++ components/IF_schemas/analytics_schema.js
@@ -5,7 +5,7 @@ var Schema = mongoose.Schema, ObjectID = Schema.ObjectID;
 // stores any sort of analytics that we might want to use for training algorithms
 var analyticsSchema = mongoose.Schema({
 
-    analyticsUserId: { type: Schema.Types.ObjectId, ref: 'anonusers', required: true}, // required
+    anonId: String,
     userId: String, // optional, can remove if the user opts out of tracking?
     hashedSessionId: String, // for stringing session data together.
                              // we can't store the actual session id,
@@ -14,30 +14,9 @@ var analyticsSchema = mongoose.Schema({
     userTimestamp: Date,
     serverTimestamp: Date,
     sequenceNumber: Number, // starts at zero when a person opens the app, makes sure we know we missed something
-    loc: {
-        type: {
-            type: String //GeoJSON-'point'
-        },
-        coordinates: []
-    },
     action: String, // allows you to prefix stuff if you want. like location.found or location.lostSignal
     data: {} // free-form data logged by the application
 });
-
-// Validate analyticsUserId
-// (automatically checks that it's a mongoid already, just check that it's valid now)
-analyticsSchema.path('analyticsUserId').validate(function(value, cb) {
-    mongoose.model('anonusers').findById(value, function(err, doc) {
-        if (err || !doc) {
-            cb(false);
-        } else {
-            // maybe check the anonuser doc that was returned?
-            cb(true);
-        }
-    });
-});
-
-analyticsSchema.index({loc:'2dsphere'});
 
 // create the model for users and expose it to our app
 module.exports = mongoose.model('analytics', analyticsSchema);
