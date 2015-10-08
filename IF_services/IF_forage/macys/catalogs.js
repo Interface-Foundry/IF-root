9efdd35221eb393f258c4dad5481b89f9085ccba
jrbaldwin <jrbaldwin@gmail.com>
2015-10-08 13:46:32 -0400

    asdf

---
+++ IF_services/IF_forage/macys/catalogs.js
@@ -0,0 +1,12 @@
+//WOMEN
+var wdresses = 'http://www1.macys.com/shop/womens-clothing/dresses/Pageindex,Productsperpage/1,All?id=5449&edge=hybrid&cm_sp=us_hdr-_-women-_-5449_dresses_COL1'
+
+
+var catalogs = [ wdresses
+]
+
+
+
+
+
+module.exports = catalogs
