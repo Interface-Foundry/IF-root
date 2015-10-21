06e6b9265cc17a412a1b8287fd6b4fa1e392752a
muchimoto <muchimoto@gmail.com>
2015-10-21 12:13:23 -0400

    adding npm modules to package.json

--- IF_services/IF_forage/macys/macys_store_scraper.js
+++ IF_services/IF_forage/macys/macys_store_scraper.js
@@ -35,7 +35,7 @@ async.whilst(
                             zipcode = zip.zipcode;
                             //Reset save flag
                             saveFlag = 0;
-                            if (notFoundCount >= 30) {
+                            if (notFoundCount >= 50) {
                                 console.log('Looks like all stores for this state have been scraped, moving on... ', notFoundCount)
                                 notFoundCount = 0;
                                 stateIndex = stateIndex + 1
