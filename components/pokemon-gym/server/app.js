cf35417d5f477224c523fc65a034e3c44cc3ff78
Peter Brandt <peter.m.brandt@gmail.com>
2015-11-16 17:41:45 -0500

    allow cross origin

--- components/pokemon-gym/server/app.js
+++ components/pokemon-gym/server/app.js
@@ -6,6 +6,12 @@ var morgan = require('morgan')
 app.use(bodyParser.json());
 app.use(morgan())
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
+
 //
 // Server status monitoring
 //
