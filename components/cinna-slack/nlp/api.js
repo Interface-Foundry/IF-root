ce7599eb62dd6b55623f2adb1d0b9f25c4451b17
Peter Brandt <peter.m.brandt@gmail.com>
2015-11-19 17:46:41 -0500

    handling simple cases, returning the right object

--- components/cinna-slack/nlp/api.js
+++ components/cinna-slack/nlp/api.js
@@ -1,7 +1,38 @@
 var request = require('request')
 var config = require('config')
 
+var BUCKET = {
+  search: 'search',
+  banter: 'banter',
+  purchase: 'purchase'
+}
+
+var ACTION = {
+  initial: 'initial',
+  similar: 'similar',
+  modified: 'modified',
+  focus: 'focus',
+  more: 'more',
+  back: 'back'
+}
+
+/**
+ * Call this with the text from slack
+ * callback takes (err, response)
+ sampleRes = {
+        bucket: 'search',
+        action: actionS, //initial, similar, modified, focus, more, back
+        searchSelect: [1,2,3], //which item for search select
+        tokens: msg,
+        channel: '3EL18A0M' //example of slack channel (the user who is chatting) --> please send back from python
+    };
+  */
 var parse = module.exports.parse = function(text, callback) {
+  var simpleResult = quickparse(text);
+  if (simpleResult) {
+    return callback(null, simpleResult)
+  }
+
   request({
     method: 'POST',
     url: 'http://localhost:5000/parse',
@@ -13,13 +44,100 @@ var parse = module.exports.parse = function(text, callback) {
     if (e) {
       return callback(e);
     } else {
-      return callback(null, b);
+      return callback(null, nlpToResult(b));
     }
   })
 }
 
+/**
+ * checks for really simple cases which won't require nlp
+ */
+function quickparse(text) {
+  // remove a leading "kip" ("kip find me socks" --> "find me socks")
+  text = text.replace(/^kip[,:;.! ]/i, '')
+  var res = {
+    bucket: BUCKET.search
+  }
+  regexes = {
+    initial: [
+      /^find me a\b/i,
+      /^find me\b/i,
+      /^find\b/i,
+      /^search for a\b/i,
+      /^search for\b/,
+      /^search\b/i
+    ],
+    modified: [
+      /\bbut\b/
+    ],
+    similar: [
+      /like the ([\w]+)\b/i
+    ],
+    focus: []
+  };
+
+  var found = false;
+  Object.keys(regexes).map(function(action) {
+    if (found) { return }
+    regexes[action].map(function(re) {
+      if (found) {
+        return;
+      } else if (text.match(re)) {
+        found = true;
+        res.action = action
+      } else {
+        return;
+      }
+
+      switch (action) {
+        case ACTION.initial:
+          var q = text.replace(re, '').trim();
+          res.tokens = [q];
+          break;
+      }
+    })
+  })
+
+  if (!found) {
+    return false
+  } else {
+    return res
+  }
+}
+
+function nlpToResult(nlp) {
+  console.log(nlp)
+  var isq = isQuestion(nlp);
+  return {
+    bucket: BUCKET.search,
+
+  }
+}
+
+// shit this is hard
+function isQuestion(nlp) {
+  if (nlp.parts_of_speech[nlp.parts_of_speech.length - 1][0] === '?') {
+    return true;
+  }
+
+  // some questions start off with verbs like "is, are, does"
+  for (var i = 0; i < nlp.parts_of_speech.length; i++) {
+    if (i[1] === 'INTJ') {
+      continue;
+    } else if ('is,are,does,which'.indexOf(i[0]) >= 0) {
+      return true;
+    } else {
+      break;
+    }
+  }
+
+  return false;
+}
+
 if (!module.parent) {
   var sentences = [
+    'find me a coffee machine',
+    'search luxury socks',
     'like the frist one but not so derpy',
     'kip find me running leggings',
     'like the first one but orange',
@@ -38,6 +156,7 @@ if (!module.parent) {
       if (e) {
         console.error(e);
       } else {
+        console.log(a);
         console.log(res);
       }
     })
