f9a96f9836e45e64d90143a92701bc28ad204b4a
Peter Brandt <peter.m.brandt@gmail.com>
2015-12-01 10:45:35 -0500

    updates for produciton chat

---
+++ install_node.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget https://nodejs.org/dist/v5.1.0/node-v5.1.0-linux-x64.tar.gz
+tar xzf node-v5*
+rm node-v5*.tar.gz
+cd node-v5*
+echo "export PATH="'$PATH'":$PWD/bin" >> ~/.bashrc
+
