2d28880d836986fbc6340b898b2cf4f7c2c69091
jrbaldwin <jrbaldwin@gmail.com>
2015-12-04 13:42:13 -0500

    added test.js tests

--- components/cinna-slack/nlp/test.js
+++ components/cinna-slack/nlp/test.js
@@ -5,32 +5,201 @@ var nlp = require('./api')
 
 var messages = [
   {
-    m: 'more like 1',
-    r: {
-      bucket: 'search',
-      action: 'similar',
-      searchSelect: [1]
-    }
-  },
-  {
-    m: 'more like 2',
-    r: {
-      bucket: 'search',
-      action: 'similar',
-      searchSelect: [2]
-    }
-  },
-  {
     m: 'skinny black jeans',
     r: {
       action: 'initial',
       bucket: 'search',
       tokens: ['skinny black jeans']
     }
+  },
+  {
+    m: 'more', 
+    r: {
+      bucket: 'search',
+      action: 'more',
+      tokens: ['more']
+    }
+  },
+  {
+    m: 'more like 1',
+    r: {
+      bucket: 'search',
+      action: 'similar',
+      searchSelect: [1],
+      tokens: ['more like 1']
+    }
+  },
+  {
+    m: 'more like 1 and 3',
+    r: {
+      bucket: 'search',
+      action: 'similar',
+      searchSelect: [1,3],
+      tokens: ['more like 1 and 3']
+    }
+  },
+  {
+    m: '1 in blue',
+    r: {
+      bucket: 'search',
+      action: 'modify',
+      searchSelect: [1],
+      tokens: ['1 in blue'],
+      dataModify: {
+        type: 'color',
+        val: ['blue','navy','aqua'] //start with root color, include any additional synonyms
+      }
+    }
+  },
+  {
+    m: '1 in wool',
+    r: {
+      bucket: 'search',
+      action: 'modify',
+      searchSelect: [1],
+      tokens: ['1 in wool'],
+      dataModify: {
+        type: 'material',
+        val: ['wool','cashmere','merino'] //start with root, include any additional synonyms
+      }
+    }
+  },
+  {
+    m: '1 in XL',
+    r: {
+      bucket: 'search',
+      action: 'modify',
+      searchSelect: [1],
+      tokens: ['1 in XL'],
+      dataModify: {
+        type: 'size',
+        val: ['extra large','XL'] //start with spelled out size, then add acronyms (better for amazon)     
+      }
+    }
+  },
+  {
+    m: '1 with collar', 
+    r: {
+      bucket: 'search',
+      action: 'modify',
+      searchSelect: [1],
+      tokens: ['1 with collar'],
+      dataModify: {
+        type: 'genericDetail', //default for modifiers we dont know how to handle
+        val: ['collar']    
+      }
+    }
+  },
+  {
+    m: '1 by Zara',
+    r: {
+      bucket: 'search',
+      action: 'modify',
+      searchSelect: [1],
+      tokens: ['1 by Zara'],
+      dataModify: {
+        type: 'brand',
+        val: ['Zara']    
+      }
+    }
+  },
+  {
+    m: '1 but less', //1 but cheaper
+    r: {
+      bucket: 'search',
+      action: 'similar',
+      searchSelect: [1],
+      tokens: ['1 but less'],
+      dataModify: {
+        type: 'price',
+        param: 'less' //or 'more'
+      }
+    }
+  },
+  {
+    m: '1 but less than $25',
+    r: {
+      bucket: 'search',
+      action: 'similar',
+      searchSelect: [1],
+      tokens: ['1 but less than $25'],
+      dataModify: {
+        type: 'price',
+        param: 'less than', //or 'more than'
+        val: '25' //in USD
+      }
+    }
+  },
+  {
+    m: 'info 1', 
+    r: {
+      bucket: 'search',
+      action: 'similar',
+      searchSelect: [1],
+      tokens: ['info 1']
+    }
+  },
+  {
+    m: 'save 1',
+    r: {
+      action: 'save',
+      bucket: 'purchase',
+      searchSelect: [1],
+      tokens: ['save 1']
+    }
+  },
+  {
+    m: 'get',
+    r: {
+      action: 'checkout',
+      bucket: 'purchase',
+      tokens: ['get']
+    }
+  },
+  {
+    m: 'checkout',
+    r: {
+      action: 'checkout',
+      bucket: 'purchase',
+      tokens: ['checkout']
+    }
+  },
+  {
+    m: 'get 1',
+    r: {
+      action: 'checkout',
+      bucket: 'purchase',
+      searchSelect: [1], //checks out without view cart or product info//
+      tokens: ['get 1']
+    }
+  },
+  {
+    m: 'checkout 1',
+    r: {
+      action: 'checkout',
+      bucket: 'purchase',
+      searchSelect: [1], //checks out without view cart or product info//
+      tokens: ['checkout 1'],
+    }
+  },
+  {
+    m: 'remove 1',
+    r: {
+      action: 'remove',
+      bucket: 'purchase',
+      searchSelect: [1], //can be any array of numbers
+      tokens: ['remove 1']
+    }
+  },
+  {
+    m: 'view cart',
+    r: {
+      action: 'list',
+      bucket: 'purchase',
+      tokens: ['view cart']
+    }
   }
-  /// list of tests to be pasted in tomorrow
 ];
-
 
 // run the module reloader
 request({
