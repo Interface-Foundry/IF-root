a42ade6db3be62fec5e29dde4b99e39e158e3454
muchimoto <muchimoto@gmail.com>
2015-12-04 14:51:59 -0500

    fixed typo in states.js

--- IF_services/IF_forage/ebay/digest.js
+++ IF_services/IF_forage/ebay/digest.js
@@ -121,13 +121,12 @@ function processItem(item) {
             node.type = list
             node.imgSrc = image
             var categoryString = item.category
-            categoryString = categoryString.replace('Clothing, Shoes & Accessories:', '').replace('Wholesale', '').replace(/:/g, ' ').replace(/'/g, '').replace(/[^\w\s]/gi, ' ')
-            item.name = item.name.replace(/[^\w\s]/gi, '')
+            categoryString = categoryString.replace(/Clothing, Shoes & Accessories|Wholesale|Clothing/g, '')
             var firstIteration = item.name.concat(' ' + categoryString);
             firstIteration = _.uniq(firstIteration.split(' '), function(word) {
                 return word.toLowerCase().trim()
-            }).join(' ').replace(/'/g, '').replace(/[^\w\s]/gi, ' ').replace(/\u00a0/g, " ");
-            console.log('Caption: ', firstIteration)
+            }).join(' ').replace(/'/g, '').replace(/[^\w\s]|:/gi, ' ').replace(/\u00a0/g, " ");
+            // console.log('Caption: ', firstIteration)
             node.captions = [firstIteration]
 
             saveNode(node).then(function(saved) {
