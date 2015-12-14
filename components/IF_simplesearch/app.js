9c42f37873edd6b56a01646bf921ac32a2e09b13
jrbaldwin <jrbaldwin@gmail.com>
2015-12-14 17:57:41 -0500

    added cinna parsing query

--- components/IF_simplesearch/app.js
+++ components/IF_simplesearch/app.js
@@ -7,6 +7,7 @@ var uuid = require('uuid');
 var compression = require('compression');
 var base = process.env.NODE_ENV !== 'production' ? __dirname + '/static' : __dirname + '/dist';
 var defaultPage = process.env.NODE_ENV !== 'production' ? __dirname + '/simpleSearch.html' : __dirname + '/dist/simpleSearch.html';
+var querystring = require('querystring');
 
 app.use(bodyParser.json());
 app.use(express.static(base));
@@ -25,62 +26,19 @@ app.use(require('prerender-node').set('protocol', 'https'));
 // }));
 
 
+app.get('/cinna/*', function(req, res, next) {
+   res.redirect(querystring.unescape(req.url.replace('/cinna/',''))); //magic cinna moment
+});
+
 
 app.get('/*', function(req, res, next) {
     res.sendfile(defaultPage);
 });
 
-// app.post('/getLoc', function (req, res, next) {
-//     console.log('got');
-//     //get IP by location
-//     request('https://kipapp.co/api/geolocation', function (error, response, body) {
-//       if (!error && response.statusCode == 200) {
-//         console.log(body) // Show the HTML for the Google homepage.
-//       }
-//     })
 
-
-//     res.send();
-// });
-
-app.post('/search', function(req, res, next) {
-    console.log(req.body);
-
-    // if (req.body && req.body.email && req.body.password  && req.body.token) {
-    //     console.log('resetting password for user', req.body.email);
-    // } else {
-    //     console.log('could not reset password, some field was missing');
-    //     return next('Could not reset password, please try again');
-    // }
-
-    // db.Users.findOne({
-    //     'local.email': req.body.email
-    // }, function(e, u) {
-    //     if (e) { return next(e) }
-    //     if (!u) { return next('Could not reset password, invalid email')}
-    //     if (u.local.resetPasswordToken !== req.body.token) {
-    //         return next('Could not reset password.  Make sure you are using the most recent link we sent you (and that you copy/pasted it correctly if you had to).  Note that each link can only be used once.');
-    //     }
-    //     var salt = bcrypt.genSaltSync(10);
-    //     var hash = bcrypt.hashSync(req.body.password, salt);
-    //     u.local.password = hash;
-    //     u.local.resetPasswordToken = '';
-    //     u.save(function(err, savedUser) {
-    //         if (err || !savedUser) {
-    //             return next('Could not create user.')
-    //         }
-    //         console.log('savedUser: ', savedUser)
-    //         var token = getToken(savedUser);
-    //         res.cookie('kipAuth', token);
-    //         res.json({
-    //             user: savedUser,
-    //             token: token
-    //         });
-    //     })
-
-
-    // })
-})
+// app.post('/search', function(req, res, next) {
+//     console.log(req.body);
+// })
 
 
 if (!module.parent) {
