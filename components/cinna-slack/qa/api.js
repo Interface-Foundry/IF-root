40b7b1dd00b0d75b7c68407e7335797d22464161
Peter Brandt <peter.m.brandt@gmail.com>
2016-01-18 13:21:54 -0500

    adding qa api entrypoint

---
+++ components/cinna-slack/qa/api.js
@@ -0,0 +1,14 @@
+
+
+/**
+ * Pass a question to this question-answering module, supplying optional
+ * source text (like the text of an amazon.com page)
+ */
+module.exports = function(question, source, callback) {
+  if (typeof source === 'function') {
+    callback = source;
+    source = null;
+  }
+
+  callback(null, '42')
+}
