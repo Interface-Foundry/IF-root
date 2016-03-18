c095808e8154eb6ef8311c8355a63876a77303f5
muchimoto <muchimoto@gmail.com>
2016-03-18 17:01:40 -0400

    chanes to devServer

--- components/cinna-slack/support-client/devServer.js
+++ components/cinna-slack/support-client/devServer.js
@@ -12,11 +12,7 @@ var session = require('express-session');
 var cors = require('cors');
 var webpack = require('webpack');
 var config = require('./webpack.config.dev');
-var app = express();
 var compiler = webpack(config);
-
-var passport = require('passport');
-require('./config/passport')(passport);
 
 var User = require('./server/models/User');
 //set env vars
@@ -38,13 +34,11 @@ app.use(require('webpack-hot-middleware')(compiler));
 
 app.use(cors());
 app.use(session({
-  secret: 'keyboard kitty',
+  secret: 'keyboard kitty', 
   resave: false,
   saveUninitialized: false,
   cookie: { maxAge: 600000000 }
 }));
-app.use(passport.initialize());
-app.use(passport.session());
 
 //load routers
 var messageRouter = express.Router();
@@ -52,13 +46,12 @@ var usersRouter = express.Router();
 var channelRouter = express.Router();
 require('./server/routes/message_routes')(messageRouter);
 require('./server/routes/channel_routes')(channelRouter);
-require('./server/routes/user_routes')(usersRouter, passport);
 app.use('/api', messageRouter);
 app.use('/api', usersRouter);
 app.use('/api', channelRouter);
 
 app.use('/', express.static(path.join(__dirname)));
-var webpackServer = app.listen(process.env.PORT, 'localhost', function(err) {
+var webpackServer = http.listen(process.env.PORT, 'localhost',function(err) {
   if (err) {
     console.log(err);
     return;
