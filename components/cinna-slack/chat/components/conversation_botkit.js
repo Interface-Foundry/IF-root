08efeb6bbdfa4a3e8712069ea227b7892b2c03c4
Peter Brandt <peter.m.brandt@gmail.com>
2016-03-22 08:46:14 -0400

    convo w/ admin squad

--- components/cinna-slack/chat/components/conversation_botkit.js
+++ components/cinna-slack/chat/components/conversation_botkit.js
@@ -48,21 +48,27 @@ function askWhoManagesPurchases(response, convo) {
   convo.ask('Who manages the office purchases? You can say something like "me" or "me and @jane".', function(response, convo) {
     console.log(response);
 
+    var user_is_admin = false;
+    var user_id = response.user;
+
     // check for "skip"
     if (response.text.toLowerCase().trim() === 'skip') {
       // by default put the user in charge of everything
+      user_is_admin = true;
       convo.slackbot.meta.office_assistants = [response.user];
     }
 
     // check for "me" or "i do"
     if (response.text.toLowerCase().match(/(\bme\b|\bi do\b)/) && response.text.toLowerCase().indexOf('and') < 0) {
       convo.slackbot.meta.office_assistants = [response.user];
+      user_is_admin = true;
       convo.say("Great!  I'll keep you up-to-date on what your team members are adding to the office shopping cart.")
     }
 
     // check for something like "nobody"
     if (response.text.toLowerCase().match(/^(no one|nobody|noone)/)) {
       convo.slackbot.meta.office_assistants = [response.user];
+      user_is_admin = true;
       convo.say("Well, I'll put you in charge of me, then!")
     }
 
@@ -70,12 +76,13 @@ function askWhoManagesPurchases(response, convo) {
     // for a typed message like "that would be @dan"
     // the response.text would be like  "that would be <@U0R6H9BKN>"
     var office_gremlins = response.text.match(/(\<\@[^\s]+\>|\bme\b)/ig);
-    if (office_gremlins && office_gremlins.length > 0) {
+    if (office_gremlins && office_gremlins.length > 0 && !user_is_admin) {
       convo.slackbot.meta.office_assistants = office_gremlins.map(function(handle) {
         if (handle.toLowerCase() === 'me') {
+          user_is_admin = true;
           return response.user;
         } else {
-          return handle.replace(/\<\@\>/g, '');
+          return handle.replace(/(\<\@|\>)/g, '');
         }
       })
 
@@ -108,9 +115,46 @@ function askWhoManagesPurchases(response, convo) {
     db.Slackbots.update({_id: convo.slackbot._id}, {$set: {'meta.office_assistants': convo.slackbot.meta.office_assistants}}, function(e) {
       if (e) { console.error(e) }
 
-      convo.next()
+      // send the office admins welcome messages and show them all the welcome video
+      convo.slackbot.meta.office_assistants.map(function(id) {
+        if (id === response.user) { return; }
+        console.log('starting admin welcome conversation with ' + id)
+        var slackbot = convo.slackbot;
+
+        var bot = controller.spawn({
+          token: slackbot.bot.bot_access_token
+        })
+
+        bot.startRTM(function(err, bot, payload) {
+          if (err) {
+            throw new Error('Could not connect to Slack');
+          }
+
+          bot.startPrivateConversation({user: id}, function(response, convo) {
+            // inject the slackbot into the convo so that we can save it in the db
+            convo.slackbot = slackbot;
+            convo.on('end', function() {
+              bot.closeRTM();
+              done();
+            })
+            convo.say("Hi! I'm Kip, your office shopping helper bot! <@$user> told me to make you an admin for the team, so I'll keep you up-to-date on what team members are adding to the office shopping cart.".replace('$user', user_id))
+            welcomeVid(response, convo);
+          });
+        });
+
+      })
+
+      // show this user the welcome video
+      welcomeVid(response, convo);
     })
   })
+}
+
+// welcome video
+function welcomeVid(response, convo) {
+  // TODO
+  convo.say('Check out this 🔥 vid the squad made')
+  convo.next()
 }
 
 
