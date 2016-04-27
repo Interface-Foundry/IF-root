69390175444b4bf25dfb1a3a2842c261662ac9b3
Peter Brandt <peter.m.brandt@gmail.com>
2016-04-27 09:41:45 -0400

    fixing bug

--- components/cinna-slack/chat/components/refresh_team.js
+++ components/cinna-slack/chat/components/refresh_team.js
@@ -1,20 +1,26 @@
 var db = require('db');
 var co = require('co');
-var request = require('request');
+var request = require('request-promise');
+require('promisify-global');
+
+var debug = process.env.NODE_ENV === 'development' ? console.log.bind(console) : function(){};
 
 // i guess this is a lot like init_team
 module.exports = function(team_id) {
+  console.log('refreshing team ' + team_id);
   return co(function*() {
-    var bot = yield db.Slackbots.find({
+    var bot = yield db.Slackbots.findOne({
       team_id: team_id
     }).exec();
 
-    var r = yield request.promise('https://slack.com/api/users.list?token=' + bot.bot.bot_access_token);
-    users = JSON.parse(r.body).members;
+    debug(bot);
+
+    var r = yield request('https://slack.com/api/users.list?token=' + bot.bot.bot_access_token);
+    users = JSON.parse(r).members;
 
     // now collect all the DM channels for each user which doesn't have one in the db yet
-    r = yield request.promise('https://slack.com/api/im.list?token=' + bot.bot.bot_access_token);
-    imlist = JSON.parse(r.body).ims;
+    r = yield request('https://slack.com/api/im.list?token=' + bot.bot.bot_access_token);
+    imlist = JSON.parse(r).ims;
     var dmhash = imlist.reduce((hash, im) => {
       hash[im.user] = im.id;
       return hash;
@@ -26,12 +32,19 @@ module.exports = function(team_id) {
       u.dm = dmhash[u.id];
 
       // love thee, upsert
-      return db.Chatusers.collection.update.promise({
-        id: u.id
-      },
-      u, {
-        upsert: true
-      });
-    })
+      return db.Chatusers.update({
+          id: u.id
+        },
+        u, {
+          upsert: true
+        })
+    });
+    console.log('done refreshing team ' + team_id);
   });
+}
+
+if (!module.parent) {
+  module.exports('T0R6J00JW').catch(function(e) {
+    console.log(e.stack);
+  })
 }
