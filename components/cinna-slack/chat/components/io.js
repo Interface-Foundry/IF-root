eafd9778fc3a9b126d5116a435bfc80a1dfda1aa
muchimoto <muchimoto@gmail.com>
2016-04-28 12:23:25 -0400

    removed redis stuff

--- components/cinna-slack/chat/components/io.js
+++ components/cinna-slack/chat/components/io.js
@@ -219,56 +219,59 @@ var newEmail= function(envelope) {
     console.log('Got new Email!', envelope.from_address);
 
     // **Question what to do is a user with same email is participating in multiple slackbots?
-     redisClient.rpush('chat_email', JSON.stringify(envelope), function(err, reply) {
-            if (err) {
-                err.niceMessage = 'Error with redis queue';
-                err.devMessage = 'EMAIL REDIS QUEUE ERR';
-                return
-            } else {
-                console.log('Added new email to redis queue!')
+     // redisClient.rpush('chat_email', JSON.stringify(envelope), function(err, reply) {
+     //        if (err) {
+     //            err.niceMessage = 'Error with redis queue';
+     //            err.devMessage = 'EMAIL REDIS QUEUE ERR';
+     //            return
+     //        } else {
+     //            console.log('Added new email to redis queue!')
+     //        }
+     //  });    
+
+    //update message 
+    //use time stamps 
+
+    Chatuser.find({'profile.email':{$regex: envelope.from_address.toString().trim(), $options:'i'}}).exec(function(err, users) {
+        if(err){
+            console.log('saved chat user retrieval error');
+        } else {     
+            if (!users || users.length == 0) {
+                var mailOptions = {
+                    to: envelope.from_address,
+                    from: 'Kip Bot <hello@kip.ai>',
+                    subject: 'You are not currently in a chat!',
+                    text: 'You are currently not taking part in any Kip Bot chats...'
+                };
+                mailerTransport.sendMail(mailOptions, function(err) {
+                    if (err) console.log(err);
+                    console.log('User was not found in Chatusers db. Sent notification to user.');
+                });
             }
-      });    
+            else if (users[0] && users[0].team_id && slackUsers[users[0].team_id] ) {
+               // console.log('This should be the active chat: ', slackUsers[users[0].team_id])
+               var emailCommand = {
+                    source: {
+                        'origin':'slack',
+                        'channel':users[0].dm,
+                        'org':slackUsers[users[0].team_id].activeTeamId,
+                        'id':users[0].team_id + "_" + users[0].dm, 
+                        'user': slackUsers[users[0].team_id].activeUserId
+                    },
+                    'msg': envelope.text.toString().trim(),
+                    'flags': {'email': true},
+                    'emailInfo': {
+                        to: envelope.from_address,
+                        from: 'Kip Bot <hello@kip.ai>',
+                        subject: 'Reply from Kip Bot!',
+                        text: ''
+                    }
+                };
+                preProcess(emailCommand);
+            }
+        }
+    });
 
-
-    // Chatuser.find({'profile.email':{$regex: envelope.from_address.toString().trim(), $options:'i'}}).exec(function(err, users) {
-    //     if(err){
-    //         console.log('saved chat user retrieval error');
-    //     } else {     
-    //         if (!users || users.length == 0) {
-    //             var mailOptions = {
-    //                 to: envelope.from_address,
-    //                 from: 'Kip Bot <hello@kip.ai>',
-    //                 subject: 'You are not currently in a chat!',
-    //                 text: 'You are currently not taking part in any Kip Bot chats...'
-    //             };
-    //             mailerTransport.sendMail(mailOptions, function(err) {
-    //                 if (err) console.log(err);
-    //                 console.log('User was not found in Chatusers db. Sent notification to user.');
-    //             });
-    //         }
-    //         else if (users[0] && users[0].team_id && slackUsers[users[0].team_id] ) {
-    //            // console.log('This should be the active chat: ', slackUsers[users[0].team_id])
-    //            var emailCommand = {
-    //                 source: {
-    //                     'origin':'slack',
-    //                     'channel':users[0].dm,
-    //                     'org':slackUsers[users[0].team_id].activeTeamId,
-    //                     'id':users[0].team_id + "_" + users[0].dm, 
-    //                     'user': slackUsers[users[0].team_id].activeUserId
-    //                 },
-    //                 'msg': envelope.text.toString().trim(),
-    //                 'flags': {'email': true},
-    //                 'emailInfo': {
-    //                     to: envelope.from_address,
-    //                     from: 'Kip Bot <hello@kip.ai>',
-    //                     subject: 'Reply from Kip Bot!',
-    //                     text: ''
-    //                 }
-    //             };
-    //             preProcess(emailCommand);
-    //         }
-    //     }
-    // });
 }
 
 //load slack users into memory, adds them as slack bots
@@ -2225,15 +2228,9 @@ var sendResponse = function(data,flag){
                         username:'Kip',
                         attachments: attachThis
                     };
-                    slackUsers_web[data.source.org].chat.postMessage(data.source.channel, '', msgData, function() {
-
-                    });
-
+                    slackUsers_web[data.source.org].chat.postMessage(data.source.channel, '', msgData, function() {});
                 });
-
             }
-
-
         }else {
             console.log('error: slackUsers channel missing', slackUsers);
         }
@@ -2583,75 +2580,75 @@ function encode_utf8(s) {
 }
 
 
-client.on("connect", function(err) {
-    console.log("Connected to email redis queue...");
-});
+// client.on("connect", function(err) {
+//     console.log("Connected to email redis queue...");
+// });
 
-var timer = new InvervalTimer(function() {
-    client.lrange('chat_email', 0, -1, function(err, emails) {
-            // console.log('Email Queue: ' + emails.length)
-            if (emails.length > 0) {
-                console.log('Pausing timer')
-                timer.pause();
-                console.log(emails.length + ' email(s) for processing.')
-                async.eachSeries(emails, function iterator(email_str, callback) {
-                    var envelope = JSON.parse(email_str);
-                    console.log('Incoming email: ', JSON.stringify(envelope));
-                    Chatuser.find({'profile.email':{$regex: envelope.from_address.toString().trim(), $options:'i'}}).exec(function(err, users) {
-                        console.log(0);
-                        if(err){
-                            console.log('saved chat user retrieval error');
-                        } else {     
-                            if (!users || users.length == 0) {
-                                console.log(1);
-                                var mailOptions = {
-                                    to: envelope.from_address,
-                                    from: 'Kip Bot <hello@kip.ai>',
-                                    subject: 'You are not currently in a chat!',
-                                    text: 'You are currently not taking part in any Kip Bot chats...'
-                                };
-                                mailerTransport.sendMail(mailOptions, function(err) {
-                                    if (err) console.log(err);
-                                    console.log('User was not found in Chatusers db. Sent notification to user.');
-                                });
-                            }
-                            else if (users[0] && users[0].team_id ) {
-                                console.log(2);
-                               var emailCommand = {
-                                    source: {
-                                        'origin':'slack',
-                                        'channel':users[0].dm,
-                                        'org':slackUsers[users[0].team_id].activeTeamId,
-                                        'id':users[0].team_id + "_" + users[0].dm, 
-                                        'user': slackUsers[users[0].team_id].activeUserId
-                                    },
-                                    'msg': envelope.text.toString().trim(),
-                                    'flags': {'email': true},
-                                    'emailInfo': {
-                                        to: envelope.from_address,
-                                        from: 'Kip Bot <hello@kip.ai>',
-                                        subject: 'Reply from Kip Bot!',
-                                        text: ''
-                                    }
-                                };
-                                preProcess(emailCommand);
-                                client.lrem('chat_email', 1, email_str);
-                                timer.resume()
-                            } else {
-                                console.log(3);
-                                console.log('wtf mate: slackUsers: ', slackUsers, ' users: ', users);
-                                client.lrem('chat_email', 1, email_str);
-                                timer.resume()
-                            }
-                        }
-                    });
-                }, function complete(err, results) {
-                    console.log('Resuming timer!')
-                    timer.resume()
-                });
-            }
-        }) // end of client lrange, callback)
-}, 5000);
+// var timer = new InvervalTimer(function() {
+//     client.lrange('chat_email', 0, -1, function(err, emails) {
+//             // console.log('Email Queue: ' + emails.length)
+//             if (emails.length > 0) {
+//                 console.log('Pausing timer')
+//                 timer.pause();
+//                 console.log(emails.length + ' email(s) for processing.')
+//                 async.eachSeries(emails, function iterator(email_str, callback) {
+//                     var envelope = JSON.parse(email_str);
+//                     console.log('Incoming email: ', JSON.stringify(envelope));
+//                     Chatuser.find({'profile.email':{$regex: envelope.from_address.toString().trim(), $options:'i'}}).exec(function(err, users) {
+//                         console.log(0);
+//                         if(err){
+//                             console.log('saved chat user retrieval error');
+//                         } else {     
+//                             if (!users || users.length == 0) {
+//                                 console.log(1);
+//                                 var mailOptions = {
+//                                     to: envelope.from_address,
+//                                     from: 'Kip Bot <hello@kip.ai>',
+//                                     subject: 'You are not currently in a chat!',
+//                                     text: 'You are currently not taking part in any Kip Bot chats...'
+//                                 };
+//                                 mailerTransport.sendMail(mailOptions, function(err) {
+//                                     if (err) console.log(err);
+//                                     console.log('User was not found in Chatusers db. Sent notification to user.');
+//                                 });
+//                             }
+//                             else if (users[0] && users[0].team_id ) {
+//                                 console.log(2);
+//                                var emailCommand = {
+//                                     source: {
+//                                         'origin':'slack',
+//                                         'channel':users[0].dm,
+//                                         'org':slackUsers[users[0].team_id].activeTeamId,
+//                                         'id':users[0].team_id + "_" + users[0].dm, 
+//                                         'user': slackUsers[users[0].team_id].activeUserId
+//                                     },
+//                                     'msg': envelope.text.toString().trim(),
+//                                     'flags': {'email': true},
+//                                     'emailInfo': {
+//                                         to: envelope.from_address,
+//                                         from: 'Kip Bot <hello@kip.ai>',
+//                                         subject: 'Reply from Kip Bot!',
+//                                         text: ''
+//                                     }
+//                                 };
+//                                 preProcess(emailCommand);
+//                                 client.lrem('chat_email', 1, email_str);
+//                                 timer.resume()
+//                             } else {
+//                                 console.log(3);
+//                                 console.log('wtf mate: slackUsers: ', slackUsers, ' users: ', users);
+//                                 client.lrem('chat_email', 1, email_str);
+//                                 timer.resume()
+//                             }
+//                         }
+//                     });
+//                 }, function complete(err, results) {
+//                     console.log('Resuming timer!')
+//                     timer.resume()
+//                 });
+//             }
+//         }) // end of client lrange, callback)
+// }, 5000);
 
 
 function InvervalTimer(callback, interval) {
