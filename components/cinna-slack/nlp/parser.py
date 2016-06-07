e569151667c0a12cd28666daf235f2d1e06e7c63
graham <graham.annett@gmail.com>
2016-06-07 01:00:21 -0700

    updating with __future__ for print functions

--- components/cinna-slack/nlp/parser.py
+++ components/cinna-slack/nlp/parser.py
@@ -1,11 +1,14 @@
+from __future__ import print_function
 from easydict import EasyDict as edict
 
 # data.text is the original text
 # data.blob is the output of TextBlob
 # data.doc is the output of spaCy
+
+
 def parse(data):
-    print 'entering parser method'
-    print data.text
+    print('entering parser method')
+    print(data.text)
 
     # Create the object we will return from the api
     res = edict({})
@@ -71,7 +74,6 @@ def parse(data):
         pos.append([token.orth_, token.pos_])
     res.parts_of_speech = pos
 
-
     # ALL nouns
     res.nouns = set()
     for n in data.blob.noun_phrases:
@@ -96,6 +98,5 @@ def parse(data):
             res.verbs.append('checkout')
         if (token[0] == 'info'):
             res.verbs.append('info')
-
 
     return res
--- components/cinna-slack/nlp/server.py
+++ components/cinna-slack/nlp/server.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from easydict import EasyDict as edict
 
 import parser
@@ -9,30 +10,35 @@ from flask import (
     jsonify
 )
 
-print 'loading data, usually takes 75 seconds on my mac'
 import os
 from spacy.en import English, LOCAL_DATA_DIR
 data_dir = os.environ.get('SPACY_DATA', LOCAL_DATA_DIR)
+print('loading data, usually takes 75 seconds on my mac')
 nlp = English(data_dir=data_dir)
 
 app = Flask(__name__)
 
+
 @app.route('/parse', methods=['GET', 'POST'])
 def parse_message():
-    print 'at parse_message'
-    print request.json
+    print('at parse_message')
+    print(request.json)
     data = edict({})
     data.text = request.json['text']
     data.blob = TextBlob(data.text)
     data.doc = nlp(u"{}".format(data.text), tag=True, parse=True, entity=True)
-    
-    return jsonify(parser.parse(data))
+    parsed_data = parser.parse(data)
+    print(parsed_data)  # debugging
+    return jsonify(parsed_data)
+
 
 @app.route('/reload')
 def reload_parse():
     reload(parser)
     return 'ok'
 
+
 if __name__ == '__main__':
-    print 'running app on port 8083'
-    app.run(host="0.0.0.0", port=8083, use_debugger=True, debug=True, use_reloader=False)
+    print('running app on port 8083')
+    app.run(host="0.0.0.0", port=8083, use_debugger=True,
+            debug=True, use_reloader=False)
