5c9532969be701063ec4985a2cad580ecc61b275
graham <graham.annett@gmail.com>
2016-06-09 13:10:11 -0700

    temp get mongodb messages to df

--- components/cinna-slack/nlp2/mcparser.py
+++ components/cinna-slack/nlp2/mcparser.py
@@ -1,3 +1,14 @@
+import pandas as pd
+
+
+def retrieve_from_db():
+    from pymongo import MongoClient
+    client = MongoClient()
+    db = client.foundry
+    cursor = db.messages.find({})
+    df_messages = pd.DataFrame(list(cursor))
+
+    return df_messages
 
 
 class McParser:
@@ -10,4 +21,10 @@ class McParser:
         pass
 
     def parser_text():
+        pass
+
+    def last_three_messages(self):
+        '''
+        given user, return last three messages from dataframe
+        '''
         pass
