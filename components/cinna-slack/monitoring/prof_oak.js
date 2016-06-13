4575f14d5205356ab46bc88959151e8a9dd62a8e
Peter Brandt <peter.m.brandt@gmail.com>
2016-06-13 19:33:16 -0400

    fixing prof_oak/production confgi

--- components/cinna-slack/monitoring/prof_oak.js
+++ components/cinna-slack/monitoring/prof_oak.js
@@ -1,9 +1,8 @@
 var slack = require('@slack/client');
-debugger;
 var token = 'xoxb-50573012182-9gJAJ1IBPDRjM3xrfcIFdTO1';
 var web = new slack.WebClient(token);
 
-var channel = 'G06BTTZGW'; // back-end devs
+var channel = process.env.NODE_ENV === 'production' ? 'G06BTTZGW' : 'D1GGV0CMU'; // G06BTTZGW is back-end devs (D1GGV0CMU is peter <-> prof oak)
 
 module.exports.say = function(text) {
   var data = {
