0c64f927d83c6a0efdf96382349a126e9946c356
Peter Brandt <peter.m.brandt@gmail.com>
2016-06-13 18:45:00 -0400

    adding a monitoring bot for mongodb

---
+++ components/cinna-slack/monitoring/monitor.js
@@ -0,0 +1,78 @@
+var assert = require('assert');
+var _ = require('lodash');
+var cron = require('cron');
+var co = require('co');
+var config = require('config');
+var prof_oak = require('./prof_oak');
+
+var defaults = {
+  cron: '0 0 0 * * *', // every hour (second, minute, hour, day of month, month, day of week)
+  check: function* (){ throw new Error('Error check not implemented') }, // default to throw error.
+  title: 'omg no name 😱'
+};
+
+function Monitor(options) {
+  options = options || {};
+  options = _.merge({}, defaults, options)
+
+  var state = {
+    okay: true,
+    errors: [],
+    errors_in_row: 0,
+    last_error: false
+  };
+
+  var monitor = new cron.CronJob(options.cron, function() {
+    console.log('checking ' + options.title);
+    co(options.check).then(() => {
+      console.log(options.title + ' is ok')
+      state.okay = true;
+      state.errors_in_row = 0;
+    }).catch(e => {
+      var err = {
+        stack: e.stack,
+        message: e.message,
+        time: new Date()
+      };
+
+      console.log('error with ' + options.title);
+      console.log(e.stack);
+
+      state.last_error = err;
+      state.errors.push(err);
+      state.errors_in_row++;
+
+      if (state.okay) {
+        // first error, so we should do something special.
+        state.okay = false;
+        console.log('whoops, first alert');
+        prof_oak.say('There was an error with ' + options.title + ',\n`' + e.stack + '`');
+      } else {
+        console.log('it happened again');
+        if (state.errors_in_row % 10 === 0) {
+          prof_oak.say('the error with ' + options.title + ' is still happening, @peter maybe you should fix this before it\'s too late.');
+        }
+      }
+    })
+  });
+
+  monitor.start();
+}
+
+
+// check that mongodb is still up.
+var mongo = Monitor({
+  title: 'mongodb',
+  cron: '0 * * * * *', // every effing minute
+  check: function *() {
+    var MongoClient = require('mongodb').MongoClient;
+
+    var db = yield MongoClient.connect('mongodb://' + config.mongodb.url);
+    var messages = db.collection('messages');
+    var m = yield messages.findOne({});
+    assert(m);
+    db.close();
+  }
+});
+
+module.exports = Monitor;
---
+++ components/cinna-slack/monitoring/prof_oak.js
@@ -0,0 +1,18 @@
+var slack = require('@slack/client');
+debugger;
+var token = 'xoxb-50573012182-9gJAJ1IBPDRjM3xrfcIFdTO1';
+var web = new slack.WebClient(token);
+
+var channel = 'D1GGV0CMU'; // back-end devs
+
+module.exports.say = function(text) {
+  var data = {
+    username: 'Professor Oak',
+    icon_url: 'https://avatars.slack-edge.com/2016-06-13/50574232438_554dfff64aed8d91939e_72.jpg'
+  };
+  web.chat.postMessage(channel, text, data);
+}
+
+if (!module.parent) {
+  module.exports.say("Hi I'm Professor Oak.");
+}
