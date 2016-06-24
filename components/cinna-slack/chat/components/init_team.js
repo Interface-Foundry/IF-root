1898186cb1a9232b5ba321b66ce403dfeda9e557
Peter Brandt <peter.m.brandt@gmail.com>
2016-06-24 16:31:18 -0400

    adding #general channel fixes #263

--- components/cinna-slack/chat/components/init_team.js
+++ components/cinna-slack/chat/components/init_team.js
@@ -1,21 +1,41 @@
 var db = require('db')
 var kip = require('kip')
-var request = require('request')
+var request = require('request-promise')
 var debug = require('debug')('chat')
 var refresh_team = require('./refresh_team')
+var co = require('co');
 
 // Needs scopes 'identify,team:read,users:read,im:read,bot'
 // (bot+users%3Aread+im%3Aread+team%3Aread)
 
 module.exports = function(bot, cb) {
   cb = cb || function() {};
-  scrape_team_info(bot, function(err, users) {
-    if (err || !users) {
-      console.error('Could not scrape users for slackbot ' + bot._id);
-      // maybe send email here?
-      return;
-    }
 
+  co(function*() {
+    var identity = yield request({url: 'https://slack.com/api/auth.test?token=' + bot.access_token, json: true});
+
+    bot.meta.addedBy = identity.user_id;
+    bot.meta.office_assistants = [bot.meta.addedBy];
+    console.log('GRATUITOUS CONSOLE.LOG HERE !!!!!!!!111 ZZZZZZZZZZZZZ');
+    console.log('PIZZA ROLLS ARE DONE');
+    console.log('ZZZZZZZZZZZZZZZZ TEST ZZZZZZZZZZZZZZZZZZZZ')
+
+    // try to add #general
+    var channels = yield request({url: 'https://slack.com/api/channels.list?token=' + bot.bot.bot_access_token, json: true});
+    if (channels.ok) {
+      channels = channels.channels;
+      channels.map(c => {
+        if (c.is_general) {
+          bot.meta.cart_channels.push(c.id);
+        }
+      });
+    }
+    bot.save();
+    var users = yield refresh_team(bot.team_id, null, null, null, null, 12, bot, bot.name, console.log);
+
+    if (users.ok) {
+      users = users.users
+    }
     // find the user that added the slackbot
     var addedBy = users.filter(function(u) {
       return u.id === bot.meta.addedBy;
@@ -28,40 +48,14 @@ module.exports = function(bot, cb) {
     }
 
     // convert array to regular object
-    addedBy = addedBy[0];
+    return addedBy[0];
 
-    //
-    // Onboarding conversation start
-    //
+  }).then(addedBy => {
+    kip.debug('addedBy', addedBy);
     cb(null, addedBy);
+  }).catch(e => {
+    kip.err('error initializing team.  that sounds pretty bad');
+    // TODO send email here
+    kip.err(e);
   })
-}
-
-// callback is a team, the list of chatusers for this bot
-var scrape_team_info = function(bot, callback) {
-    //console.log('scraping team for bot ' + bot._id);
-    request('https://slack.com/api/auth.test?token=' + bot.access_token, function(e, r, b) {
-      if (kip.error(e)) return;
-
-      r.body = JSON.parse(r.body)
-
-      if (!r.body.user_id && typeof bot.meta.addedBy === 'undefined') {
-        return kip.error('Could not find the user who added slackbot ' + bot._id)
-      } else {
-        bot.meta.addedBy = r.body.user_id || bot.meta.addedBy;
-      }
-
-      bot.meta.office_assistants = [bot.meta.addedBy];
-      console.log('ASSISTANTS HERE!!!!!! ',bot.meta.office_assistants);
-
-      if (typeof bot.save === 'function') {
-        bot.save();
-      } else {
-        console.error('Could not save addedBy as ' + r.body.user_id + ' for bot ' + bot._id);
-      }
-
-      refresh_team(bot.team_id).then(u => {
-        callback(null, u);
-      }, callback);
-    })
 }
