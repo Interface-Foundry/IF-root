0cd29079c99fc445d6b8f783aa17c9b2a60d7acd
graham <graham.annett@gmail.com>
2016-06-28 15:19:53 -0700

    uploaded new model with masking (possibly not as good)

--- components/cinna-slack/nlp_training/training.py
+++ components/cinna-slack/nlp_training/training.py
@@ -16,33 +16,45 @@ import tensorflow as tf
 
 def model():
     '''full bidirectional'''
-    sess = tf.Session()
-    K.set_session(sess)
+    with tf.name_scope('session'):
+        sess = tf.Session()
+        K.set_session(sess)
+
     with tf.name_scope('inputs'):
         sequence = Input(shape=(data.shape[1],), dtype='int32')
-        embed = Embedding(tk.nb_words, 256,
-                          input_length=data.shape[1])(sequence)
+        embed = Embedding(input_dim=tk.nb_words,
+                          output_dim=256,
+                          input_length=data.shape[1],
+                          mask_zero=True)(sequence)
 
-    # apply forwards + backwards LSTM
     with tf.name_scope('forwards'):
+        # apply forwards LSTM1
         forwards = LSTM(128, return_sequences=True)(embed)
+        # forwards = LSTM(128, return_sequences=True)(forwards)
+        # forwards = LSTM(64, return_sequences=True)(forwards)
         forwards = LSTM(128)(forwards)
 
     with tf.name_scope('backwards'):
+        # apply forwards LSTM1
         backwards = LSTM(128, return_sequences=True, go_backwards=True)(embed)
+        # backwards = LSTM(128, return_sequences=False)(backwards)
+        # backwards = LSTM(64, return_sequences=True, go_backwards=True)(after_dp)
         backwards = LSTM(128)(backwards)
 
-    with tf.name_scope('merging'):
+    with tf.name_scope('merge1'):
         # concat the outputs of the 2 LSTMs
         merged = merge([forwards, backwards], mode='concat', concat_axis=-1)
         after_dp = Dropout(0.5)(merged)
-        output = Dense(action_codes.shape[1], activation='softmax')(after_dp)
+
+        # apply forwards + backwards LSTM1
 
     with tf.name_scope('complete_model'):
+        # to dense output
+        output = Dense(action_codes.shape[1], activation='softmax')(after_dp)
         model = Model(input=sequence, output=output)
 
     with tf.name_scope('optimizer'):
-        rmsprop = RMSprop(lr=0.00005, rho=0.9, epsilon=1e-08)
+        rmsprop = RMSprop(lr=0.00001, rho=0.9, epsilon=1e-08)
 
     with tf.name_scope('model_compiled'):
         model.compile(optimizer=rmsprop,
@@ -65,7 +77,8 @@ if __name__ == '__main__':
     save_model(model)
     print(model.summary())
     tb_callback = TBC(log_dir='logs/', histogram_freq=2, write_graph=True)
-    mc = MC(filepath='models/latest_model.hdf5', verbose=1, save_best_only=True)
+    mc = MC(filepath='models/latest_model.hdf5',
+            verbose=1, save_best_only=True)
 
     model.fit(data, action_codes,
               validation_split=.2,
