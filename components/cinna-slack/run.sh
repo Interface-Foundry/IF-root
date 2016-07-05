dd548aae0617efbfb72154f8ec25f54f228da933
Peter Brandt <peter.m.brandt@gmail.com>
2016-07-05 11:44:08 -0400

    updaintg kubectl scripts

--- components/cinna-slack/run.sh
+++ components/cinna-slack/run.sh
@@ -2,9 +2,11 @@ VERSION=0.7.4.1
 
 kubectl run replylogic --image=gcr.io/kip-styles/reply_logic:$VERSION
 kubectl run web --image=gcr.io/kip-styles/web:$VERSION --port=8000
+kubectl run facebook --image=gcr.io/kip-styles/facebook:$VERSION --port=8000
 kubectl run nlp --image=gcr.io/kip-styles/nlp:$VERSION --port=8083
 kubectl run picstitch --image=gcr.io/kip-styles/picstitch:$VERSION --port=5000
 
 kubectl expose deployment web --type="LoadBalancer"
+kubectl expose deployment facebook --type="LoadBalancer"
 kubectl expose deployment nlp
 kubectl expose deployment picstitch
