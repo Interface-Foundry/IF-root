0731b18ffa64e62abebe68ce33ffbea57a866816
graham <graham.annett@gmail.com>
2016-08-02 01:04:59 -0700

    better refactoring

--- src/image_processing/picstitch.py
+++ src/image_processing/picstitch.py
@@ -6,6 +6,7 @@ import io
 import textwrap
 import logging
 from PIL import Image, ImageFont, ImageDraw
+from config import make_image_configs
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(levelname)s - %(message)s')
@@ -26,17 +27,21 @@ def load_number_images():
     return images
 
 
+def load_fonts():
+    fonts_file = os.path.join('fonts', 'HelveticaNeue-Regular.ttf')
+    font = {}
+    font_size = [12, 13, 14, 15, 16, 20, 28]
+    for s in font_size:
+        font[s] = ImageFont.truetype(fonts_file, s)
+    return font
+
+
 def load_review_stars():
-    # images = []
-    # star_images = [x * .5 for x in range(1, 11, 1)]
-    # for i in star_images:
-    #     f = 'review_stars/' + repr(i) + '.png'
-    #     images.append(Image.open(f))
-    star_images = [0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5]
+    star_images = [0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0]
     rs_dict = {}
     for i in star_images:
-        f = THIS_FOLDER + '/review_stars/' + repr(i) + '.png'
-        rs_dict[i] = Image.open(f)
+        f = THIS_FOLDER + '/review_stars/' + str(i) + '.png'
+        rs_dict[str(i)] = Image.open(f)
     return rs_dict
 
 
@@ -52,177 +57,182 @@ def download_image(url):
     return im
 
 
-def upload_image_to_s3(image, bucket, bucket_name='if-kip-chat-images'):
+class PicStitch:
     '''
+    {'price': '$15.98',
+     'name': ['Size: Large', 'Amoluv', 'Amoluv owns its own trademarks.
+      Trademark number:86522196, The package contains Amoluv Tag'],
+     'origin': 'slack',
+     'reviews': {'rating': 3.2, 'reviewCount': '258'},
+     'prime': '0',
+     'url': 'http://ecx.images-amazon.com/images/I/51cgwKrncWL.jpg'}
     '''
-    s3_file = str(uuid.uuid4())
-    tmp_img = io.BytesIO()
-    image.save(tmp_img, 'PNG', quality=90)
-    k = bucket.new_key(s3_file)
-    k.set_contents_from_string(tmp_img.getvalue(),
-                               headers={"Content-Type": "image/png"})
-    url_string = 'https://s3.amazonaws.com/' + bucket_name + '/' + s3_file
-    return url_string
 
+    def __init__(self,
+                 img_req,
+                 bucket,
+                 amazon_prime_image,
+                 review_stars_images,
+                 font_dict):
+        '''
+        '''
+        self.thumbnails = []
+        self.origin = None
 
-def create_image(images, REVIEW_STARS, AMAZON_PRIME):
-    # get all the posted files
+        self.img_req = img_req[0]
+        self.bucket = bucket
+        self.bucket_name = 'if-kip-chat-images'
 
-    CHAT_WIDTH = 365
-    CHAT_HEIGHT = 140
-    PADDING = 5
-    BGCOLOR = 'white'
-    length = 3
-    biggest_width = 0
-    biggest_height = 0
-    thumbnails = []
-    PIC_SIZE = 130, 130
-    CHAT_WIDTH = 365
-    CHAT_HEIGHT = 140
-    # where to draw main pics
-    PIC_COORDS = [{'x': 14, 'y': 5}, {'x': 24, 'y': 174}, {'x': 24, 'y': 336}]
-    # where to draw choice numbers
-    TEXTBOX_COORDS = [{'x': 190, 'y': 10}, {'x': 190, 'y': 174}, {
-        'x': 190, 'y': 336}]  # where to draw text boxes
+        self.amazon_prime_image = amazon_prime_image
+        self.review_stars_images = review_stars_images
+        self.font_dict = font_dict
 
-    # messenger ratio
-    if images[0]['origin'] and images[0]['origin'] == 'facebook':
-        CHAT_HEIGHT = 223
-        CHAT_WIDTH = 425
-        PIC_COORDS = [{'x': 5, 'y': 5}]  # where to draw main pics
-        TEXTBOX_COORDS = [{'x': 250, 'y': 5}]  # where to draw text boxes
-        PIC_SIZE = 223, 223
+        if self.img_req['prime']:
+            self.prime = True
+        else:
+            self.prime = False
 
-    if images[0]['origin'] and images[0]['origin'] == 'skype':
-        CHAT_HEIGHT = 230
-        CHAT_WIDTH = 381
-        PIC_COORDS = [{'x': 20, 'y': 50}]  # where to draw main pics
-        TEXTBOX_COORDS = [{'x': 250, 'y': 100}]  # where to draw text boxes
-        PIC_SIZE = 250, 250
+        self._get_config()
+        self._make_image()
+        self._upload_image_to_s3()
 
-    # add images
-    for i, data in enumerate(images):
-        im = download_image(data['url'])
-        im.thumbnail(PIC_SIZE, Image.ANTIALIAS)
-        thumbnails.append(im)
+    def _get_config(self):
+        if self.img_req['origin']:
+            if self.img_req['origin'] in ['facebook', 'slack', 'skype']:
+                self.origin = self.img_req['origin']
+        else:
+            self.origin = 'slack'
+            logging.critical('NO_ORIGIN__ASSUMING_SLACK')
+        self.config = make_image_configs(self.font_dict, self.origin)
 
-    # image object
-    img = Image.new('RGB', (CHAT_WIDTH, CHAT_HEIGHT), BGCOLOR)
+    def _make_image(self):
+        # should be 1 image_req...
+        # image_data = self.img_req
 
-    for i, im in enumerate(thumbnails):
-        # add pics
-        x = PIC_COORDS[i]['x']
-        y = PIC_COORDS[i]['y']
-        img.paste(im, (x, y))
+        # create blank image based on source (slack/facebook/skype)
+        img = Image.new(mode='RGB',
+                        size=(self.config['CHAT_WIDTH'],
+                              self.config['CHAT_HEIGHT']),
+                        color=self.config['BGCOLOR'])
 
-    # add names, text wrapped
-    font = ImageFont.truetype(
-        THIS_FOLDER + "/HelveticaNeue-Regular.ttf", 16)  # price
-    font2 = ImageFont.truetype(THIS_FOLDER + "/HelveticaNeue-Regular.ttf", 13)
-    review_count_font = ImageFont.truetype(
-        THIS_FOLDER + "/HelveticaNeue-Regular.ttf", 16)
+        # get image in thumbnail format
+        thumb_img = download_image(self.img_req['url'])
+        thumb_img.thumbnail(self.config['PIC_SIZE'], Image.ANTIALIAS)
 
-    if images[0]['origin'] and images[0]['origin'] in ['skype', 'facebook']:
-        font = ImageFont.truetype(
-            THIS_FOLDER + "/HelveticaNeue-Regular.ttf", 28)  # price
-        font2 = ImageFont.truetype(
-            THIS_FOLDER + "/HelveticaNeue-Regular.ttf", 20)
+        # post image thumbnail
+        img.paste(thumb_img,
+                  (self.config['PIC_COORDS'][0]['x'],
+                   self.config['PIC_COORDS'][0]['y']))
 
-    for i, im in enumerate(images):
+        # post text
         last_y = 5
-        x = TEXTBOX_COORDS[i]['x'] - 30
-        y = TEXTBOX_COORDS[i]['y']
+        x = self.config['TEXTBOX_COORDS'][0]['x'] - 30
+        y = self.config['TEXTBOX_COORDS'][0]['y']
         draw = ImageDraw.Draw(img)
 
-        if images[0]['origin'] and images[0]['origin'] in ['skype']:
+        if self.origin is 'skype':
             last_y = last_y + 50
 
-        if images[0]['origin'] and images[0]['origin'] in ['skype', 'facebook']:
+        if self.origin in ['skype', 'facebook']:
             draw.rectangle(((205, 5), (329, 160)), fill="white")
 
         # add price
-        draw.text((x, last_y), im['price'], font=font, fill="#f54740")
+        draw.text((x, last_y),
+                  self.img_req['price'],
+                  font=self.config['font1'],
+                  fill="#f54740")
 
         # add prime logo
-        if im['prime'] == '1' and images[0]['origin'] != 'skype':
-            # , mask=AMAZON_PRIME)
-            img.paste(AMAZON_PRIME, (x + 110, last_y + 2))
+        if self.prime and self.origin is not 'skype':
+            img.paste(self.amazon_prime_image, (x + 110, last_y + 2))
 
         last_y = last_y + 28
 
         # move reviews down a bit
-        if images[0]['origin'] in ['skype', 'facebook']:
+        if self.origin in ['skype', 'facebook']:
             last_y = last_y + 10
 
         # draw - (Review Number)
-        if 'reviews' in im and 'rating' in im['reviews']:
-            image_revs_rating = im['reviews']['rating']
+        if 'reviews' in self.img_req and 'rating' in self.img_req['reviews']:
+            image_revs_rating = self.img_req['reviews']['rating']
             if image_revs_rating <= 0.5:  # ignoring if 0.0 < rating
                 selectRating = 0.5
             elif image_revs_rating <= 1.0:
-                selectRating = 1
+                selectRating = 1.0
             elif image_revs_rating <= 1.5:
                 selectRating = 1.5
-            elif image_revs_rating <= 2:
-                selectRating = 2
+            elif image_revs_rating <= 2.0:
+                selectRating = 2.0
             elif image_revs_rating <= 2.5:
                 selectRating = 2.5
-            elif image_revs_rating <= 3:
-                selectRating = 3
+            elif image_revs_rating <= 3.0:
+                selectRating = 3.0
             elif image_revs_rating <= 3.5:
                 selectRating = 3.5
-            elif image_revs_rating <= 4:
-                selectRating = 4
+            elif image_revs_rating <= 4.0:
+                selectRating = 4.0
             elif image_revs_rating <= 4.5:
                 selectRating = 4.5
             else:  # ignoring if rating < 5
                 selectRating = 5
 
-            img.paste(REVIEW_STARS[selectRating],
+            selectRating = str(selectRating)
+
+            img.paste(self.review_stars_images[selectRating],
                       (x, last_y + 3),
-                      mask=REVIEW_STARS[selectRating])
+                      mask=self.review_stars_images[selectRating])
 
-            if 'reviewCount' in im['reviews']:
+            # make number count in blue to right of stars
+            if 'reviewCount' in self.img_req['reviews']:
                 draw.text((x + 82, last_y),
-                          ' - ' + im['reviews']['reviewCount'],
-                          font=review_count_font,
+                          ' - ' + self.img_req['reviews']['reviewCount'],
+                          font=self.config['font1'],
                           fill="#2d70c1")
-
             last_y = last_y + 20
 
-        # # #fake reviews for skype!! lmao
-        elif images[0]['origin'] and images[0]['origin'] == 'skype':
+        # #fake reviews for skype!! lmao <--- uhhh
+        if self.origin is 'skype':
             selectRating = random.randint(6, 8)
-            img.paste(REVIEW_STARS[selectRating],
+            img.paste(self.review_stars_images[selectRating],
                       (x, last_y + 3),
-                      mask=REVIEW_STARS[selectRating])
+                      mask=self.review_stars_images[selectRating])
             # selectRating = random.randint(6,7)
             reviewCount = random.randint(15, 1899)
             # img.paste(REVIEW_STARS[7], (x, last_y), mask=REVIEW_STARS[7])
-            draw.text((x + 80, last_y), ' - ' + str(reviewCount),
-                      font=font2, fill="#2d70c1")
+            draw.text((x + 80, last_y),
+                      ' - ' + str(reviewCount),
+                      font=self.config['font2'],
+                      fill="#2d70c1")
             last_y = last_y + 20
 
         last_y = last_y + 5
 
-        if images[0]['origin'] and images[0]['origin'] in ['skype', 'facebook']:
-            BOX_WIDTH = 22
-        else:
-            BOX_WIDTH = 30
-
-        for z in im['name']:
+        for z in self.img_req['name']:
             # draw.text((x, last_y), z, font=font2, fill="#2d70c1")
             countLines = 0
-            for line in textwrap.wrap(z, width=BOX_WIDTH):
+            for line in textwrap.wrap(z, width=self.config['BOX_WIDTH']):
                 countLines += 1
                 if countLines < 3:
                     filler = ''
                     if countLines == 3:
                         filler = '...'
-                    draw.text((x - 3, last_y), line + filler,
-                              font=font2, fill="#909497")
-                    last_y += font2.getsize(line)[1]
+                    draw.text((x - 3, last_y),
+                              line + filler,
+                              font=self.config['font2'],
+                              fill="#909497")
+                    last_y += self.config['font2'].getsize(line)[1]
                     last_y = last_y + 2
-        y += font.getsize(line)[1]
+        y += self.config['font1'].getsize(line)[1]
         last_y = y
-    return img
+
+        self.created_image = img
+
+    def _upload_image_to_s3(self):
+        s3_file = str(uuid.uuid4())
+        tmp_img = io.BytesIO()
+        self.created_image.save(tmp_img, 'PNG', quality=90)
+        k = self.bucket.new_key(s3_file)
+        k.set_contents_from_string(tmp_img.getvalue(),
+                                   headers={"Content-Type": "image/png"})
+        url_string = 'https://s3.amazonaws.com/' + self.bucket_name + '/' + s3_file
+        self.s3_url = url_string
--- src/image_processing/server.py
+++ src/image_processing/server.py
@@ -1,10 +1,14 @@
 import logging
 
-from flask import Flask, request, g
+from flask import Flask, request
 import boto
 
-from picstitch import load_review_stars, load_amazon_prime, \
-    upload_image_to_s3, create_image
+from picstitch import load_review_stars, load_amazon_prime, load_fonts, \
+    PicStitch
+
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 port_num = 5000
 REGION = 'us-east-1'
@@ -13,12 +17,9 @@ BUCKET_NAME = 'if-kip-chat-images'
 # initialize stuff
 review_star_images = load_review_stars()
 amazon_images = load_amazon_prime()
+font_dict = load_fonts()
 conn = boto.s3.connect_to_region(REGION)
 bucket_kip_chat_images = conn.get_bucket(BUCKET_NAME)
-
-
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 app = Flask(__name__)
@@ -29,19 +30,15 @@ def main():
     '''
     return upload_image_tos_s3(create_image(request.json))
     '''
-    logging.info('image requested...')
     img_req = request.json
-    g.amazon_images = amazon_images
-    g.review_star_images = review_star_images
+    logging.info('image requested...')
+    pic = PicStitch(img_req,
+                    bucket_kip_chat_images,
+                    amazon_images,
+                    review_star_images,
+                    font_dict)
 
-    new_img = create_image(images=img_req,
-                           REVIEW_STARS=g.review_star_images,
-                           AMAZON_PRIME=g.amazon_images)
-
-    aws_filename = upload_image_to_s3(image=new_img,
-                                      bucket=bucket_kip_chat_images,
-                                      bucket_name=BUCKET_NAME)
-    return aws_filename
+    return pic.s3_url
 
 if __name__ == '__main__':
     # Constants bestowed upon us by a higher power (slack)
