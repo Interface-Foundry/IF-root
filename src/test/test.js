c161cdeb565fe7bb7772ebcc493f547fbef1282b
muchimoto <muchimoto@gmail.com>
2016-08-15 16:39:05 -0400

    test2

--- src/test/test.js
+++ src/test/test.js
@@ -4,9 +4,14 @@
 
 var assert = require('assert');
 var expect = require('chai').expect;
-var queue = require('./queue-mongo');
+var queue = require('../chat/components/queue-mongo');
 
-
+var json = { 
+  text: 'hello',
+  incoming: true,
+  original_text: 'hello',
+  origin: 'facebook' };
+var key  = 'facebook_unit_test_' + Date.now
 
 describe('the sum of', function() {
   describe('2 and 2', function() {
@@ -21,7 +26,11 @@ describe('the sum of', function() {
 describe('when we send', function(){
     describe('a hello message to the queue', function(){
 	it('should return the standard response', function(){
-	    assert(true, false);
+        queue.publish('incoming', json, key);
+        queue.topic('incoming').subscribe(incoming => {
+        		expect.incoming.to.exist
+	        	// assert.equal();
+        })
 	})
     })
 })
