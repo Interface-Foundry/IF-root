77be88c999a52b83b437994ffdd2a9d8ff1c6a1b
muchimoto <muchimoto@gmail.com>
2016-08-15 16:12:03 -0400

    test

--- src/test/test.js
+++ src/test/test.js
@@ -4,7 +4,6 @@
 
 var assert = require('assert');
 var expect = require('chai').expect;
-const snapclient = require('../snapclient')
 
 
 
@@ -17,15 +16,3 @@ describe('the sum of', function() {
 });
 
 
-// the reference to 'done()' is part of the async capability
-// in Mocha, but I'm not actually using it here
-
-describe('when we call the Snap microservice', function(){
-    describe('the endpoint /user/<id>', function() {	
-	it('should return something', function(done) {
-	    var jsondoc = snapclient.talkToSnap();	
-	    expect(jsondoc).to.exist;
-	    done();
-	});
-    });
-});
