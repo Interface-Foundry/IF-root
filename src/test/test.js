19b96d79dc14f21facf8c30a06969e348b7513ca
Dexter Taylor <binarymachineshop@gmail.com>
2016-08-15 18:42:00 -0400

    puzzling out async tests

--- src/test/test.js
+++ src/test/test.js
@@ -5,18 +5,24 @@
 var assert = require('assert');
 var expect = require('chai').expect;
 var queue = require('../chat/components/queue-mongo');
+var db = require('../components/db');
 
 var json = { 
-  text: 'hello',
-  incoming: true,
-  original_text: 'hello',
-  origin: 'facebook' };
-var key  = 'facebook_unit_test_' + Date.now
+    text: 'hello',
+    thread_id: 'facebook_1000206960095603',
+    incoming: true,
+    original_text: 'hello',
+    origin: 'facebook',
+    ts: Date.now(), 
+    
+};
+
+var key  = 'facebook_unit_test_' + Date.now();
 
 describe('the sum of', function() {
   describe('2 and 2', function() {
     it('should return 4', function() {
-	assert.equal(2+2, 4);
+        assert.equal(2+2, 4);
     });
   });
 });
@@ -25,20 +31,22 @@ describe('the sum of', function() {
 
 describe('when we send', function(){
     describe('a hello message to the queue', function(){
-	it('should return the standard response', function(){
+        var returnData = null;
         queue.publish('incoming', json, key);
-	    var returnData;
-	    beforeEach(function(callback) {
-	    
-		queue.topic('incoming').subscribe(incoming => {		    
-		    
-		    returnData = incoming;
-		    callback();
-		})
-		
-	    })
-            expect.returnData.to.exist;
-	})
+
+        beforeEach(function(done) {
+            setTimeout(function(){
+                queue.topic('incoming').subscribe(incoming => {                             
+                    returnData = incoming;
+                })
+                done();
+            }, 2000);
+            returnData = 'foobar';
+        });
+
+        it('should return the standard response', function(){        
+            expect(returnData).to.exist;
+        })
     })
 })
 
