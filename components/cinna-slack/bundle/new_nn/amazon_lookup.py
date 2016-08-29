0715d07b1428c736b0fc1179e45d26d98b21b831
Irwin Li <irwin2014@gmail.com>
2016-08-29 13:04:34 -0400

    Fixed python scraper for amazon product api to lookup titles, asins, and categories

--- components/cinna-slack/bundle/new_nn/amazon_lookup.py
+++ components/cinna-slack/bundle/new_nn/amazon_lookup.py
@@ -1,18 +1,53 @@
 
-import bottlenose, json
-import time
+import bottlenose, json, time, re
+import xml.etree.cElementTree as ElementTree
+from urllib.error import HTTPError
+from pprint import pprint
+from bs4 import BeautifulSoup
+import pandas as pd
+
+
+def error_handler(err):
+    ex = err['exception']
+    if isinstance(ex, HTTPError) and ex.code == 503:
+        time.sleep(random.expovariate(0.1))
+        return True
 
 AMAZON_ACCESS_KEY = "AKIAIKMXJTAV2ORZMWMQ"
 AMAZON_SECRET_KEY = "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"
 AMAZON_ASSOC_TAG = "quic0b-20"
 
-amazon = bottlenose.Amazon(AMAZON_ACCESS_KEY, AMAZON_SECRET_KEY, AMAZON_ASSOC_TAG)
+amazon = bottlenose.Amazon(AMAZON_ACCESS_KEY, 
+						   AMAZON_SECRET_KEY, 
+						   AMAZON_ASSOC_TAG, 
+						   Parser=BeautifulSoup,
+						   ErrorHandler=error_handler)
 
-with open('test_amazon_products', 'w') as f:
-	with open('test_asins.txt', 'r') as f1:
-		while True:
-			line = f1.readline()
+with open('test.txt', 'w') as f:
+	with open('asins.txt', 'r') as f1:
+		i = 0
+		while i < 100:
+			# each line contains an ASIN.
+			# this will bulk read from the file and search.
+			line = ','.join([f1.readline() for i in range(10)])
+			i += 1
+			#line = f1.readline()
+
+			response = amazon.ItemLookup(ItemId=line.strip(), ResponseGroup="ItemAttributes, BrowseNodes")
+			titles = response.find_all('title')
+			for title in titles:
+				f.write(title.text + '\n')
 			
+			#asins = response.find_all('asin')
+			
+			#categories = [tree.find_all('name') for tree in response.find_all('browsenodes')]
 
-			json.dump(data, f, ensure_ascii=False)
+			time.sleep(1.1)
 			if not line: break
+
+
+
+
+
+
+
