6a5b18950d6084472263874a3e15cf92ccc9f7ca
Dexter Taylor <binarymachineshop@gmail.com>
2016-09-21 12:52:10 -0400

    refactored service objects, added init files

--- src/chat/components/delivery.com/dsx_services.js
+++ src/chat/components/delivery.com/dsx_services.js
@@ -24,9 +24,57 @@ class DSXClient{
             this.restaurantSearchEndpoint = 'restaurants';
             this.contextEndpoint = 'context';
 
+            this._get = function(endpoint, request_params) {
+                
+                return new Promise((resolve, reject) => {
+                    var final_uri = this.getURI().concat('/').concat(endpoint);                
+                    var params = request_params;
+                
+                    request({
+                        method: 'GET',
+                        uri: final_uri, 
+                        qs: params
+                    },
+                   function(error, response, body){                                              
+                       if(!error && response.statusCode == 200) {
+                           resolve(response.body);
+                       }
+                       else{
+                           reject(error);
+                       }
+                       
+                   });
+                });
+            }
 
+
+            this._post = function(endpoint, request_body) {
+                
+                return new Promise((resolve, reject) => {
+                    var final_uri = this.getURI().concat('/').concat(endpoint);                
+                    var params = request_params;
+                
+                    request({
+                        method: 'POST',
+                        uri: final_uri, 
+                        body: request_body
+                    },
+                   function(error, response, body){                                              
+                       if(!error && response.statusCode == 200) {
+                           resolve(response.body);
+                       }
+                       else{
+                           reject(error);
+                       }
+                       
+                   });
+                });
+            }
+
+            
             this.getNearbyRestaurants = function(address) {
-
+                return this._get(this.restaurantSearchEndpoint, {'address': address })
+                /*
                 return new Promise((resolve, reject) => {
                     var final_uri = this.getURI().concat('/').concat(this.restaurantSearchEndpoint);                
                     var params = {'address': address };
@@ -46,10 +94,14 @@ class DSXClient{
                        
                    });
                 });
+                */
             }
 
-            this.createDeliveryContext = function(address, fulfillment_type, team_id, team_admin_id) {
+            this.createDeliveryContext = function(address, fulfillment_type, team_id, team_admin_id) {                
+                var request_body = {'address': address, 'fulfillment_type': fulfillment_type, 'team_id': team_id, 'team_admin_id': team_admin_id };
+                return this._post(this.contextEndpoint, request_body);
 
+                /*
                 return new Promise((resolve, reject) => {
                     var final_uri = this.getURI().concat('/').concat(this.contextEndpoint);                
                     var body = {'address': address, 'fulfillment_type': fulfillment_type, 'team_id': team_id, 'team_admin_id': team_admin_id };
@@ -69,6 +121,7 @@ class DSXClient{
                        
                    });
                 });
+                */
             }
 
 
