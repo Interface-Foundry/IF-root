657bd77773446a0697473015093a2bff610b9c3b
muchimoto <muchimoto@gmail.com>
2016-09-27 13:58:44 -0400

    committing before merge

--- src/chat/components/delivery.com/delivery.com.js
+++ src/chat/components/delivery.com/delivery.com.js
@@ -251,52 +251,25 @@ handlers['food.begin'] = function* (session) {
 
 handlers['food.store_context'] = function* (session) {
     kip.debug('\n\n\n GETTING TO FOOD.STORE_CONTEXT: ', session,'\n\n\n\n');
-//     GETTING TO FOOD.STORE_CONTEXT:  {
-//   "action": "store_context",
-//   "mode": "food",
-//   "_id": "57e56cee20d29ea27d88402c",
-//   "text": "902 broadway, new york",
-//   "incoming": true,
-//   "thread_id": "D0HLZLBDM",
-//   "original_text": "902 broadway, new york",
-//   "user_id": "U0HLZP0A2",
-//   "origin": "slack",
-//   "source": {
-//     "type": "message",
-//     "channel": "D0HLZLBDM",
-//     "user": "U0HLZP0A2",
-//     "text": "902 broadway, new york",
-//     "ts": "1474653422.000012",
-//     "team": "T0HLZP09L"
-//   "__v": 0,
-//   "urlShorten": [],
-//   "client_res": [],
-//   "execute": [],
-//   "tokens": [],
-//   "resolved": false,
-//   "ts": "2016-09-23T17:57:02.159Z"
-// }
     var addr = session.text;
-    
     try {
         yield dsxClient.createDeliveryContext(addr, 'none', session.source.team, session.source.user)
-        var component = new ui.UIComponentFactory(session.origin).buildButtonGroup('Select your order method.', ['Delivery', 'Pickup'], null);
-                kip.debug('COMPONENT RENDER LOOKS LIKE: ', component.render());
-
-        replyChannel.send(session, 'food.context_update', component.render());
     } catch (err) {
-      kip.debug('dsxClient.createDeliveryContext ERROR: ', err);
-      var component = new ui.UIComponentFactory(session.origin).buildTextMessage('Error: ', JSON.stringify(err));
-      replyChannel.send(session, 'food.sys_error', component.render());
+     kip.debug(err)
     }
-    // yield dsxClient.createDeliveryContext(addr, 'none', session.source.team, session.source.user)
-    // .then(function(result){
-    //       console.log('\n\n\ndeliveryContext returned successful promise: ', result,'\n\n\n\n\n');
-    // }, function(err) {
-    //       console.log('\n\n\ndeliveryContext returned failed promise: ', err,'\n\n\n\n\n');
-    // });
+    try {
+      var component = new ui.UIComponentFactory(session.origin).buildButtonGroup('Select your order method.', ['Delivery', 'Pickup'], null);
+    } catch (err) {
+          kip.debug(err)
 
-   
+    }
+    try {
+     replyChannel.send(session, 'food.context_update', component.render());
+    } catch (err) {
+          kip.debug(err)
+
+    }
+    
 }
 
 
