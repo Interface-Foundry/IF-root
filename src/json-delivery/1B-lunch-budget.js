9d213ebaa87ddaba811fd14eaaf66344092d9180
gchoi1 <g.choi07@gmail.com>
2016-10-05 17:04:23 -0400

    json-delivery-upload

---
+++ src/json-delivery/1B-lunch-budget.js
@@ -0,0 +1,38 @@
+{
+    "text": "What is the lunch budget per team member?",
+    "attachments": [
+        {
+            "text": "Team members will be able to add up to 20% above the set budget.",
+            "fallback": "You are unable to choose a game",
+            "callback_id": "wopr_game",
+            "color": "#3AA3E3",
+            "attachment_type": "default",
+            "actions": [
+                {
+                    "name": "chess",
+                    "text": "$10",
+                    "type": "button",
+                    "value": "chess"
+                },
+                {
+                    "name": "maze",
+                    "text": "$15",
+                    "type": "button",
+                    "value": "maze"
+                },
+				{
+                    "name": "maze",
+                    "text": "$20",
+                    "type": "button",
+                    "value": "maze"
+                },
+				{
+                    "name": "maze",
+                    "text": "Enter Amount",
+                    "type": "button",
+                    "value": "maze"
+                }
+            ]
+        }
+    ]
+}
