e4f7d662d33272da755915d3c120c5557c7ae106
Peter Brandt <peter.m.brandt@gmail.com>
2016-10-05 17:20:58 -0400

    adding new address test

--- src/chat/components/test/delivery.com/s1.js
+++ src/chat/components/test/delivery.com/s1.js
@@ -29,5 +29,34 @@ describe('greeting', () => {
     _.get(msg, 'attachments[0].actions[0].text', '').should.equal('Delivery')
     _.get(msg, 'attachments[0].actions[1].text', '').should.equal('Pickup')
 
+    // goes to s2
+
+  })
+
+  it('should allow an admin to start an order with a new address', function * () {
+    var user = yield mock.Admin()
+    user.chatuser.id.should.equal('admin_yolo')
+    user.chatuser.team_id.should.equal('yolo')
+
+    // Start the food convo with the admin
+    var msg = yield user.text('food.begin')
+
+    // choose to input a new address
+    msg = yield user.tap(msg, 1, 1)
+    msg.text.should.equal("What's the delivery address?")
+    _.get(msg, 'attachments[0].text', '').should.equal("Type your address below")
+
+    // type the address
+    var msg = yield user.text('902 Broadway, New York, NY 10010')
+    msg.text.should.equal("Is `902 Broadway, New York, NY 10010` your address?")
+    _.get(msg, 'attachments[0].text', '').should.equal('')
+    _.get(msg, 'attachments[0].actions.length', 0).should.equal(2)
+    _.get(msg, 'attachments[0].actions[0].text', '').should.equal('Confirm')
+    _.get(msg, 'attachments[0].actions[1].text', '').should.equal('Edit')
+
+    // tap on confirm
+    msg = yield user.tap(msg, 0, 0)
+
+    // goes to S2
   })
 })
