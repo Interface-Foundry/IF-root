0a5815976d4cdabcd5ac2b1921a2fd62ee282629
graham <graham.annett@gmail.com>
2016-10-06 12:58:30 -0700

    initial delivery_schema

---
+++ src/db/delivery_schema.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose')
+
+/**
+ * Save delivery sessions
+ */
+var deliverySchema = mongoose.Schema({
+  teamMembers: [],
+  chosen_location: {},
+  possible_locations: [],
+  pickup: false,
+  delivery: false,
+
+  // admin or whomever to use for picking restaurant and various other
+  convoInitiater: String,
+  fulfillment_method: String,
+  time_started: {
+    type: Date,
+    default: Date.now
+  },
+  mode: String,
+  action: String,
+  data: {},
+  conversations: {}
+})
+
+var delivery = mongoose.model('delivery', deliverySchema, 'delivery')
+
+module.exports = delivery
