428c08f211693f8bb1b515af99da9d9c79a95b26
Dexter Taylor <binarymachineshop@gmail.com>
2016-10-07 12:34:26 -0400

    preliminary schemas for delivery.com cart and item

---
+++ src/db/delivery_cart_schema.js
@@ -0,0 +1,77 @@
+var mongoose = require('mongoose');
+var accounting = require('accounting');
+
+/**
+ *  The data format we get back from Delivery.com when we inspect the cart
+ * 
+ */
+
+var deliveryCart = mongoose.Schema({
+
+    // For team carts it is a team id, for personal carts it is their user id
+    slack_id: String,
+
+    laundry_delivery_times: [],
+
+    tax: Number,
+
+    delivery_fee: Number,
+
+    fees: [Number],
+
+    message: [String],
+
+    total: Number,
+
+    delivery_points_multiplier: Number,
+
+    Xo_after tax charges: [Number],
+
+    item_count: Number,
+
+    order_time: "2016-10-04T19:00:00+0000", // TODO: find out about Mongoose's date/time formatting 
+
+    asap: Boolean,
+
+    convenience_fee: Number,
+
+    delivery_points: Number,
+
+   
+    cart: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'DeliveryItem'
+    }],
+
+    purchased: {
+        type: Boolean,
+    default: false
+    },
+
+    deleted: {
+        type: Boolean,
+    default: false
+    },
+
+    created_date: {
+        type: Date,
+    default: Date.now
+    },
+
+    purchased_date: {
+        type: Date
+    },
+
+    type: String,
+
+    link: String
+
+});
+
+
+deliveryCart.set('toObject', { getters: true, virtuals: true });
+deliveryCart.set('toJSON', { getters: true, virtuals: true });
+
+var DeliveryCart = mongoose.model('DeliveryCart', deliveryCart);
+
+module.exports = DeliveryCart;
---
+++ src/db/delivery_item_schema.js
@@ -0,0 +1,38 @@
+var mongoose = require('mongoose');
+
+/**
+ * Food item on order from Delivery.com
+ */
+var deliveryItem = mongoose.Schema({
+    cart_id: mongoose.Schema.ObjectId,
+
+    item_label: String,
+
+    name: String,
+
+    laundry_type: String,
+
+    item_key: Number,
+
+    price: Number,
+
+    price_compare_item: Boolean,
+
+    anonymous_id: String,
+
+    images: [String],
+
+    id: String,
+
+    type: String,
+
+    options: [deliveryItem],
+
+    quantity: Number
+
+});
+
+
+var DeliveryItem = mongoose.model('DeliveryItem', deliveryItem);
+
+module.exports = DeliveryItem;
