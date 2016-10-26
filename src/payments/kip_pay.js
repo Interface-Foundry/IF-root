4528bccc4ca471549c4769b7354ca2d4a1eb7768
alyx <alyx@kipthis.com>
2016-10-26 13:48:24 -0400

    more payments

--- src/payments/kip_pay.js
+++ src/payments/kip_pay.js
@@ -17,32 +17,39 @@
        (Giant Clam)            (Cooked Egg)             (Shrimp)
 */
 
-// See keys here: https://dashboard.stripe.com/account/apikeys
-var stripe = require("stripe")("sk_test_3dsHoF4cErzMfawpvrqVa9Mc"); //NOTE: change to production key
-var path = require("path")
-var request = require("request")
-var crypto = require("crypto")
-
-var mongoose = require("mongoose")
-require('kip');
-var Payment = db.Payment;
-var Slackbot = db.Slackbot;
-
-var bodyParser = require('body-parser')
-var express = require("express");
-var app = express();
-var jsonParser = bodyParser.json()
+var pay_const = require('./pay_const.js')
 
 //base URL for pay.kipthis.com linking
 if(process.env.NODE_ENV == 'development_alyx'){
 	var baseURL = 'https://b75f53de.ngrok.io'
+	var stripe_id = pay_const.stripe_test_id
 }else {
 	var baseURL = 'https://pay.kipthis.com'
+	var stripe_id = pay_const.stripe_production_id
 }
 
-//so user can't manipulate charge amounts
-//key indexed with kip id passed between front and back end
-var orderKey = {}
+// See keys here: https://dashboard.stripe.com/account/apikeys
+var stripe = require("stripe")(stripe_id) //NOTE: change to production key
+var path = require("path")
+var request = require("request")
+var crypto = require("crypto")
+var co = require("co")
+
+var mongoose = require("mongoose")
+require('kip')
+var Payment = db.Payment
+var Slackbot = db.Slackbot
+
+var bodyParser = require('body-parser')
+var express = require("express")
+var app = express()
+var jsonParser = bodyParser.json()
+
+
+
+//import kip cc
+var cc = require("./secrets/kip_cc.js")
+var pay_utils = require("./pay_utils.js")
 
 //this serves the checkout page for new credit card
 app.get("/", function(req, res) {
@@ -52,7 +59,6 @@ app.get("/", function(req, res) {
 
 //post a new charge for kip user
 app.post("/charge", jsonParser, function(req, res) { 
-
 
 	//include KEY with new POST req to /charge to verify authentic kip request
 	var secret_key = 'mooseLogicalthirteen$*optimumNimble!Cake' 
@@ -118,7 +124,7 @@ app.post("/charge", jsonParser, function(req, res) {
 	    "convo_initiater": {
 	        "id" : "USER_ID",
 	        "name" : "Alyx",
-	        "email": "asdf@asdf.com",
+	        "email": "asdf@asdfkipsun.com",
 	        "phone_number": "723433321",
 	        "first_name": "Alyx",
 	        "last_name": "Baldwin"
@@ -131,9 +137,17 @@ app.post("/charge", jsonParser, function(req, res) {
 	    "guest_token": "GUEST_TOKEN_123123123",
 	    "order": {
 	        "total": 5000,
+	        "tip":1000,
 	        "order_type": "delivery"
 	    }
 	}
+
+	//SAVED CARD
+	// "saved_card": {
+ //    	"vendor": "stripe",
+ //    	"customer_id": "cus_9RcJXBqg6vR4tx",
+ //    	"card_id": "card_198j4tI2kQvuYJlV9jt11NLz"
+ //    }
 
 	//params: 
 	//stripe ID 
@@ -158,7 +172,9 @@ app.post("/charge", jsonParser, function(req, res) {
 		if(o.saved_card && o.saved_card.customer_id){
 			//we have card to charge
 			if(o.saved_card.card_id){
-				charge_by_id(o)
+				charge_by_id(p,function(r){
+					console.log('SAVED CHARGE RESULT ',r)
+				})
 				var v = {
 					newAcct: false,
 					processing: true,
@@ -196,13 +212,6 @@ app.post("/charge", jsonParser, function(req, res) {
 	}
 });
 
-
-//get list of cards for user
-app.get("/list", function(req, res) {
-
-
-})
-
 //get session by token
 app.post("/session", jsonParser, function(req, res){
 	if(req.body && req.body.session_token){
@@ -217,6 +226,8 @@ app.post("/session", jsonParser, function(req, res){
 app.post("/process", jsonParser, function(req, res) { 
 
 	if(req.body && req.body.token && req.body.session_token){
+
+		res.sendStatus(200)
 
 		//this is a stripe token for the user inputted credit card details
 		var token = req.body.token.replace(/[^\w\s]/gi, '') //clean special char
@@ -243,24 +254,23 @@ app.post("/process", jsonParser, function(req, res) {
 
 				}).then(function(charge) {
 
-					console.log('STRIPE CUSTOER^#^$#&$^#$&#^$#&$^#$ ',charge)
-
 					if(charge){
 						pay.charge = charge
 					    pay.save(function (err,x) {
 					        if(err) {
 					            console.error('ERROR!');
 					        }
-					        console.log('UPDATED PAY ORDER',x)
 					    });
 					}
 					
 					if (charge.status == 'succeeded'){
 
+						//pay delivery.com							
+						pay_delivery_com(pay)
+
 						//save stripe info to slack team
 						Slackbot.findOne({team_id: pay.order.team_id}, function(err,obj) { 
 
-							console.log('& & & & & ',obj)
 							//update stripe / push cards into array
 							if(err){
 								console.error('error: cant find team to save stripe info')
@@ -282,27 +292,13 @@ app.post("/process", jsonParser, function(req, res) {
 										email: charge.source.name //this should work...
 									}
 								})
-
 							    obj.save(function (err,z) {
 							        if(err) {
 							            console.error('ERROR!');
 							        }
-							        console.log('UPDATED TEAM ',z)
 							    });
 							}
-
-						
-							//pay delivery.com							
-							pay_delivery_com(pay,charge,function(err,outcome){
-								//message 
-								console.log(err)
-								console.log(outcome)
-
-								//* * * * * * * * * * * //
-								//IF SUCCESS, POST TO MONGO QUEUE PUBLISH
-								//* * * * * * * **  ** //
-
-							})
+								
 						})	
 					}
 					else {
@@ -313,68 +309,117 @@ app.post("/process", jsonParser, function(req, res) {
 				});			
 			}
 		})
-
-
+	}else {
+		res.status(500).send('charge token missing')
 	}
 });
 
 //make a charge 
-function charge_by_id(o){
+function charge_by_id(p){
+
 	// STRIPE CHARGE BY ID 
 	// When it's time to charge the customer again, retrieve the customer ID!
 	stripe.charges.create({
-	  amount: amount, // Amount in cents
+	  amount: p.order.order.total, // Amount in cents
 	  currency: "usd",
-	  customer: customerId, // Previously stored, then retrieved
-	  card: cardId
+	  customer: p.order.saved_card.customer_id, // Previously stored, then retrieved
+	  card: p.order.saved_card.card_id
 	}).then(function(charge){
-		console.log('CHARGED?? ',charge)
+
+		if(charge){
+			p.charge = charge
+			p.save(function (err,z) {
+		        if(err) {
+		            console.error('ERROR!');
+		        }
+		    });
+		}
+		
+		if (charge.status == 'succeeded'){
+			//POST TO MONGO QUEUE SUCCESS PAYMENT
+			pay_delivery_com(p)
+		}
+		else {
+			console.log('DIDNT PROCESS STRIPE CHARGE: ',charge.status)
+			console.log('OUTCOME: ',charge.outcome)
+		}
+
 	});		
-	return 'success?'
+	
 }
 
 //pay delivery.com
-function pay_delivery_com(pay,charge,callback){
+function pay_delivery_com(pay,callback){
 
 	var err = null //lol idk 
 
 	//payment amounts should match
-	if(charge.amount == pay.order.order.total){
+	//NOTE: THIS MUST BE THE TOTAL PAYMENT + TOP TO COMPARE TO CHARGE VAL
+	if(pay.charge.amount == pay.order.order.total){
 
-		//POST /api/guest/cart/{merchant_id}/checkout
+		if (!pay.order.chosen_location.addr.special_instructions){
+			pay.order.chosen_location.addr.special_instructions = ""
+		}
 
-		// {
-		//    "order_type":"delivery",
-		//    "order_time":"ASAP",
-		//    "street":"199 Water St",
-		//    "city":"New York",
-		//    "state":"NY",
-		//    "zip_code":"10038",
-		//    "payments":[
-		//       {
-		//          "type":"credit_card",
-		//          "card":{
-		//             "cc_number":"4111111111111111",
-		//             "exp_year":"2024",
-		//             "exp_mon":"08",
-		//             "cvv":"123",
-		//             "billing_zip":"11223",
-		//             "save":false
-		//          }
-		//       }
-		//    ],
-		//    "instructions":"",
-		//    "sms_notify":false,
-		//    "isOptingIn" : true,
-		//    "phone_number":"2125551212",
-		//    "tip":"3.90",
-		//    "merchant_id":"69391",
-		//    "first_name":"sa",
-		//    "last_name":"yo",
-		//    "email":"sa@yo.com",
-		//    "client_id":"xxxxxxxxxxxxxxxx",
-		//    "uhau_id" : 12345
-		// }	
+		//build guest checkout obj
+		var guestCheckout = {	
+			"client_id": pay_const.delivery_com_client_id,
+			"order_type": pay.order.order.order_type,
+			"order_time": new Date().toISOString(),
+			"payments":[
+			  {
+			     "type":"credit_card",
+			     "card":{
+			        "cc_number":cc.cc_number,
+			        "exp_year":cc.exp_year,
+			        "exp_mon":cc.cc_month,
+			        "cvv":cc.cvv,
+			        "billing_zip":cc.billing_zip,
+			        "save":false
+			     }
+			  }
+			],
+			"sms_notify":true,
+			"isOptingIn":false,
+			"phone_number":pay.order.convo_initiater.phone_number,
+			"merchant_id":pay.order.chosen_restaurant.id,
+			"first_name":pay.order.convo_initiater.first_name,
+			"last_name":pay.order.convo_initiater.last_name,
+			"email":pay.order.convo_initiater.email,
+			"uhau_id" : 'kipthis-dot-com'
+		}
+
+		//convert tips to double if exists
+		if (pay.order.order.tip){
+			guestCheckout.tip = (pay.order.order.tip/100).toFixed(2)
+		}
+
+		//limit special delivery instructions to 100 char
+		if (pay.order.chosen_location.special_instructions){
+			var si = pay.order.chosen_location.special_instructions
+			if(si.length > 100){
+				si = si.substring(0,100);
+			} 
+			guestCheckout.instructions = si
+		}else {
+			guestCheckout.instructions = ""
+		}
+
+		//for physical delivery
+		if(pay.order.chosen_location.addr){
+			guestCheckout.street = pay.order.chosen_location.addr.address_1+", "+pay.order.chosen_location.addr.address_2
+			guestCheckout.city = pay.order.chosen_location.addr.city
+			guestCheckout.state = pay.order.chosen_location.addr.state
+			guestCheckout.zip_code = pay.order.chosen_location.addr.zip_code
+		}
+
+		//pos to delivery
+		co(function *(){
+		  yield pay_utils.payForItemFromKip(guestCheckout,pay.order.guest_token)
+		  
+		}).catch(function(err){
+			console.error(err.stack);
+		});	
 
 	}
 	else {
@@ -383,115 +428,6 @@ function pay_delivery_com(pay,charge,callback){
 	}
 	
 
-}
-
-// function sendPayment(order){
-
-// 	switch(order.service){
-// 		case 'delivery.com':
-
-// 			//POST /api/guest/cart/{merchant_id}/checkout
-
-// 			// {
-// 			//    "order_type":"delivery",
-// 			//    "order_time":"ASAP",
-// 			//    "street":"199 Water St",
-// 			//    "city":"New York",
-// 			//    "state":"NY",
-// 			//    "zip_code":"10038",
-// 			//    "payments":[
-// 			//       {
-// 			//          "type":"credit_card",
-// 			//          "card":{
-// 			//             "cc_number":"4111111111111111",
-// 			//             "exp_year":"2024",
-// 			//             "exp_mon":"08",
-// 			//             "cvv":"123",
-// 			//             "billing_zip":"11223",
-// 			//             "save":false
-// 			//          }
-// 			//       }
-// 			//    ],
-// 			//    "instructions":"",
-// 			//    "sms_notify":false,
-// 			//    "isOptingIn" : true,
-// 			//    "phone_number":"2125551212",
-// 			//    "tip":"3.90",
-// 			//    "merchant_id":"69391",
-// 			//    "first_name":"sa",
-// 			//    "last_name":"yo",
-// 			//    "email":"sa@yo.com",
-// 			//    "client_id":"xxxxxxxxxxxxxxxx",
-// 			//    "uhau_id" : 12345
-// 			// }
-// 		break;
-// 	}
-
-// 	// {
-// 	// 	"amount":"888000",
-// 	// 	"kipId":"slack_TEAMID_USERID",
-// 	// 	"description":"Los Alamos Cantina",
-// 	// 	"email":"hello@kipthis.com",
-// 	// 	"order":{
-// 	// 		"service":"delivery.com",
-			
-// 	// 	}
-// 	// }
-
-// }
-
-
-function chargeToken(token,order,callback){
-
-	//create Stripe charge using this credit card token
-	var charge = stripe.charges.create({
-	  amount: order.amount, 
-	  currency: "usd",
-	  source: token,
-	  description: order.description
-
-	}, function(err, charge) {
-
-		callback(charge,err)
-
-		// if (err && err.type === 'StripeCardError') {
-		// 	// The card has been declined
-		// 	console.log('STRIPE CHARGE ',err)
-
-
-
-		// 	// 	switch (err.type) {
-		// 	// 	  case 'StripeCardError':
-		// 	// 	    // A declined card error
-		// 	// 	    //send msg back to user card exp.
-		// 	// 	    break;
-		// 	// 	  case 'RateLimitError':
-		// 	// 	    // Too many requests made to the API too quickly
-		// 	// 	    break;
-		// 	// 	  case 'StripeInvalidRequestError':
-		// 	// 	    // Invalid parameters were supplied to Stripe's API
-		// 	// 	    break;
-		// 	// 	  case 'StripeAPIError':
-		// 	// 	    // An error occurred internally with Stripe's API
-		// 	// 	    break;
-		// 	// 	  case 'StripeConnectionError':
-		// 	// 	    // Some kind of error occurred during the HTTPS communication
-		// 	// 	    break;
-		// 	// 	  case 'StripeAuthenticationError':
-		// 	// 	    // You probably used an incorrect API key
-		// 	// 	    break;
-		// 	// 	  default:
-		// 	// 	    // Handle any other types of unexpected errors
-		// 	// 	    break;
-		// 	// 	}
-
-		// 	//should return err back to /process so we can display to user
-		// }
-		// else {
-			
-		// }
-
-	});	
 }
 
 var port = process.env.PORT || 8080;
