043fbee9e46c207d88bd895e51b52e587b4a4d31
muchimoto <muchimoto@gmail.com>
2016-11-08 16:32:32 -0500

    adding files

---
+++ src/chat/components/slack/button_templates.js
@@ -0,0 +1,104 @@
+var slack_home_default = module.exports.slack_home_default = [{
+     name: "more",
+     text: "See More Result",
+     style: "default",
+     type: "button",
+     value: "more"
+   },
+   {
+     name: "home_btn",
+     text: "🐧",
+     style: "default",
+     type: "button",
+     value: "home"
+   }]
+
+
+var slack_home = module.exports.slack_home =[{
+     name: "help_btn",
+     text: "Help",
+     style: "default",
+     type: "button",
+     value: "help_btn"
+   },
+   {
+     name: "settings",
+     text: "Settings",
+     style: "default",
+     type: "button",
+     value: "home",
+   },
+   {
+     name: "team",
+     text: "Team Members",
+     style: "default",
+     type: "button",
+     value: "home",
+   }, 
+   {
+     name: "view_cart_btn",
+     text: "View Cart",
+     style: "default",
+     type: "button",
+     value: "view_cart_btn"
+     }, 
+   {
+     name: "back_btn",
+     text: "< Back",
+     style: "default",
+     type: "button",
+     value: "back"
+   }];
+
+
+var slack_settings_default = module.exports.slack_settings_default = [{
+      name: "exit",
+      text: "Exit Settings",
+      style: "primary",
+      type: "button",
+      value: "exit"
+    },{
+     name: "help_btn",
+     text: "Help",
+     style: "default",
+     type: "button",
+     value: "help_btn"
+    },{
+      name: "home_btn",
+      text: "🐧",
+      style: "default",
+      type: "button",
+      value: "home_btn"
+    }];
+
+
+var slack_settings = module.exports.slack_settings = [{
+     name: "help_btn",
+     text: "Help",
+     style: "default",
+     type: "button",
+     value: "help_btn"
+   },
+   {
+     name: "team",
+     text: "Team Members",
+     style: "default",
+     type: "button",
+     value: "home",
+   }, 
+   {
+     name: "view_cart_btn",
+     text: "View Cart",
+     style: "default",
+     type: "button",
+     value: "view_cart_btn"
+     }, 
+   {
+     name: "back_btn",
+     text: "< Back",
+     style: "default",
+     type: "button",
+     value: "back"
+   }];
+
+
