3161244b3e593369be71cd7d1286c7daeb81c7f9
pbrandt1 <peter.m.brandt@gmail.com>
2016-11-21 17:43:18 -0500

    adding - View Menu link #427

--- src/chat/components/delivery.com/utils.js
+++ src/chat/components/delivery.com/utils.js
@@ -398,7 +398,7 @@ function * buildRestaurantAttachment (restaurant) {
   var shortenedRestaurantUrl = yield googl.shorten(restaurant.summary.url.complete)
 
   var obj = {
-    'text': `<${shortenedRestaurantUrl}|*${restaurant.summary.name}*>`,
+    'text': `<${shortenedRestaurantUrl}|*${restaurant.summary.name}*> - <${shortenedRestaurantUrl}|View Menu>`,
     'image_url': realImage,
     'color': '#3AA3E3',
     'callback_id': restaurant.id,
