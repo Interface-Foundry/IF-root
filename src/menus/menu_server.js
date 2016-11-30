aac18cab057851d31e3259a8f080cfc6f12405dc
Hannah Katznelson <hannah.katznelson@gmail.com>
2016-11-30 11:56:41 -0500

    rewrite

--- src/menus/menu_server.js
+++ src/menus/menu_server.js
@@ -8,61 +8,67 @@ var app = express();
 var jsonParser = bodyParser.json();
 var path = require('path');
 var volleyball = require('volleyball');
+var crypto = require('crypto');
+
+var Menu = require('../chat/components/delivery.com/Menu.js');
+
 app.use(volleyball);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(jsonParser);
 
 app.use('/', express.static('template'));
 app.use('/ang', express.static('ang'));
 
 //require('../chat/components/delivery.com/scrape_menus.js');
+//<===========>
 
-//DB stuff
-var Menu = require('../chat/components/delivery.com/Menu.js');
-var ObjectId = require('mongoose').Types.ObjectId;
+//handle post request with a binder full of data
+app.post('/cafe', function (req, res) {
 
-//TODO get id of chosen restaurant in whichever delivery is currently active
-//NOTE: then can probably remove req param from /menu get
+  var rest_id = req.body.rest_id;
+  var team_id = req.body.team_id; //etc, or whatever we need
 
-//GET serve up menu
-app.get('/menu/:id', function (req, res) {
-  db.Menu.findOne({_id: ObjectId(req.params.id)}).exec()
+  //should probably query db at this point
+
+  db.Menu.findOne({merchant_id: rest_id})
   .then(function (result) {
     if (result) {
-      var bigMenu = Menu(result.raw_menu).allItems();
-      var smallMenu = [];
-      for (var i = 0; i < bigMenu.length; i++) {
-        smallMenu.push({
-          children: bigMenu[i].children,
-          price: bigMenu[i].price,
-          description: bigMenu[i].description,
-          name: bigMenu[i].name
-        });
-      }
-      console.log('small', smallMenu);
-      res.send(smallMenu);
+      res.send(Menu(result.raw_menu).allItems());
     }
     else res.send('no menu found')
   })
   .catch(function (err) {
     res.send(err);
   });
+
 });
 
-//TODO: change to take its own (unofficial) ID
-//serve up restaurant name
-app.get('/name/:id', function (req, res) {
-  db.Menu.findOne({_id: ObjectId(req.params.id)}).exec()
-  .then(function (result) {
-    console.log('merchantId', result);
-    return db.Merchant.findOne({id: result.merchant_id}).exec();
-  })
-  .then(function (result) {
-    res.send(result.data.summary.name);
-  })
-  .catch(function (err) {
-    console.log(err);
-    res.send(err);
-  });
-});
+//save data locally
+
+//generate key
+
+//return a url w a key in a query string
+
+//when user hits that url up, post to /session w/key and gets correct pg
+
+//<===========>
+//
+// //TODO: change to take its own (unofficial) ID
+// //serve up restaurant name
+// app.get('/name', function (req, res) {
+//   db.Menu.findOne({_id: ObjectId(req.params.id)}).exec()
+//   .then(function (result) {
+//     console.log('merchantId', result);
+//     return db.Merchant.findOne({id: result.merchant_id}).exec();
+//   })
+//   .then(function (result) {
+//     res.send(result.data.summary.name);
+//   })
+//   .catch(function (err) {
+//     console.log(err);
+//     res.send(err);
+//   });
+// });
 
 //PUT update item quantity
 
