c197ac1a6ff1a3a8fe7e02a5430679fc53c22ed0
Christopher Barry <me@chrisb.xyz>
2016-12-05 15:16:14 -0500

    don't error out in onboard if a link is pasted

--- src/chat/components/reply_logic.js
+++ src/chat/components/reply_logic.js
@@ -153,6 +153,8 @@ function * processProductLink(message) {
     }
     if (!fail) {
       message.text = 'view cart';
+      message.mode = 'shopping';
+      message.action = 'initial';
       yield message.save();
     }
   } 
