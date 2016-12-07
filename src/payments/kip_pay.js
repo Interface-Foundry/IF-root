001cce7e08d8af1c9f49f896f6677e0839a2096b
alyx <alyx@kipthis.com>
2016-12-07 13:14:59 -0500

    added coupons

--- src/payments/kip_pay.js
+++ src/payments/kip_pay.js
@@ -84,21 +84,55 @@ app.post('/charge', jsonParser, (req, res) => co(function * () {
 
     // COUPONS
     if (body.team_id === 'T2X0BLHGX') { // alyx testing team
-      body.order.coupon = 0.5 // in percentage off
-    } else if (body.team_id === 'T02PN3B25') { // kipsearch team
       body.order.coupon = 0.99 // in percentage off
-    } else if (body.team_id === 'T02QUPKHW') { // quibb team
+    } 
+    else if (body.team_id === 'T02PN3B25') { // kipsearch team
+      
       body.order.coupon = 0.99 // in percentage off
-    } else if (body.team_id === 'T1198BQV8') { // message.io team
-      body.order.coupon = 0.5 // in percentage off
-    } else if (body.team_id === 'T3AHPU2N9') { // eden tester
+    } 
+    else if (body.team_id === 'T02QUPKHW') { // quibb team
       body.order.coupon = 0.99 // in percentage off
-    }else if (body.team_id === 'T3BCG4CP4') { // woodside friends 
+    } 
+    else if (body.team_id === 'T1198BQV8') { // message.io team
       body.order.coupon = 0.99 // in percentage off
-    }else if (body.team_id === 'T3AM3RZSL') { // vegan house
+    } 
+    else if (body.team_id === 'T3AHPU2N9') { // eden tester
+      body.order.coupon = 0.99 // in percentage off
+    }
+    else if (body.team_id === 'T3BCG4CP4') { // woodside friends 
+      body.order.coupon = 0.99 // in percentage off
+    }
+    else if (body.team_id === 'T3AM3RZSL') { // vegan house
       body.order.coupon = 0.99 // in percentage off
     }
 
+    //phase 2
+    else if (body.team_id === 'T3AN3CDJ5') { // Berkshire Inc.
+      body.order.coupon = 0.99 // in percentage off
+    }
+    else if (body.team_id === 'T024F5PNW') { // Betaworks
+      body.order.coupon = 0.99 // in percentage off
+    }
+    else if (body.team_id === 'T2U751DHD') { // Marketing605
+      body.order.coupon = 0.99 // in percentage off
+    }
+    else if (body.team_id === 'T3BCWK9MG') { // Tawa
+      body.order.coupon = 0.99 // in percentage off
+    }
+
+    //check for order over $600
+    if (body.order.coupon == 0.99){
+      //not processing over $600!!
+      if (body.order.total > 60000){
+        var v = {
+          msg: 'Eep this order size is too large for the 99% off coupon. Please contact hello@kipthis.com with questions',
+          newAcct: false,
+          processing: false
+        }
+        res.status(500).send(v)
+        return
+      }
+    }
 
     // new payment
     var payment = new Payment({
