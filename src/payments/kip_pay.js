a6c25e55c19432a06f498ddf651952ae9bcb2775
alyx <alyx@kipthis.com>
2016-12-08 10:44:32 -0500

    coupon

--- src/payments/kip_pay.js
+++ src/payments/kip_pay.js
@@ -120,6 +120,9 @@ app.post('/charge', jsonParser, (req, res) => co(function * () {
     else if (body.team_id === 'T3BCWK9MG') { // Tawa
       body.order.coupon = 0.99 // in percentage off
     }
+    else if (body.team_id === 'T3BBJ9Y2F') { // Reel Memoriez Photography
+      body.order.coupon = 0.99 // in percentage off
+    }
 
     //check for order over $510 (pre coupon)
     if (body.order.coupon == 0.99){
