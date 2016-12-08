8821802445dc6f6ee80918af88a8022f5975dd4e
Christopher Barry <me@chrisb.xyz>
2016-12-08 14:24:57 -0500

    Another attempt to make the shopping button fast

--- src/chat/components/reply_logic.js
+++ src/chat/components/reply_logic.js
@@ -451,8 +451,7 @@ queue.topic('incoming').subscribe(incoming => {
         logging.info('👽 passing to nlp: ', message.text)
         if (message.execute && message.execute.length >= 1 || message.mode === 'food') {
           replies = yield execute(message)
-        } else if (message.text === 'shopping' || (message.execute && message.execute.length >= 1) || 
-          (message.action === 'switch' && (message.text === 'shopping' || !message.text))) {
+        } else if ((message.text.includes('shop') && !message.execute) || (message.action === 'switch' && (message.text === 'shopping' || !message.text))) {
           message.mode = 'shopping'
           message.action = 'initial'
           message.execute.push({
