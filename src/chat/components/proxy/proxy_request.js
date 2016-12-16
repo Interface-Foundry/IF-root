705840cd637070f5d89cde823e0651914592d8c8
graham <grahamannett@users.noreply.github.com>
2016-12-16 09:06:59 -0800

    Revert "Allow requests to be sent via luminati in kubernetes" (#610)

--- src/chat/components/proxy/proxy_request.js
+++ src/chat/components/proxy/proxy_request.js
@@ -2,8 +2,8 @@ const hutil = require('hutil');
 const etask = require('hutil').etask;
 var exec = require('child_process').exec;
 var kip = require('../../../kip');
-var config = require('../../../config');
 var async = require('async');
+var request = require('request');
 var db = require('../../../db');
 var mesh_request = require('./mesh');
 var luminati_request = require('./lib_luminati').luminati_request;
@@ -15,47 +15,62 @@ var sets = require('./settings').sets;
 var sets_index = require('./settings').current_set;
 var IntervalTimer = require('./IntervalTimer').IntervalTimer;
 var async = require('async');
-
+var argv = require('minimist')(process.argv.slice(2));
+var test_mode = argv.proxy ? argv.proxy : false;
+// e.g. run example for test mode: node reply_logic --proxy --interval=6000
 proxy = {};
 options = {};
-
-if (config.proxy.luminati.runLocal) {
-  console.log('initializing local luminati proxy.');
+if (test_mode) {
+  console.log('Running proxy test mode..')
   async.eachSeries(sets, function iterator(set, callback) {
-    if (proxy) {
-      console.log('\n\n🤖stopping previous proxy...\n\n');
-      proxy.stop();
-    }
+   if (proxy) {
+    console.log('\n\n🤖stopping previous proxy...\n\n');
+    proxy.stop();
+   }
     proxy_status.current_index = sets_index;
     // console.log('\n\n\n\n🤖starting #', sets_index,' proxy with options: ', set.config,' ...\nfor ',set.time_in_seconds,' seconds...\n\n\n\n');
     proxy = new Luminati(sets[sets_index].config);
     proxy.listen(24000, 'localhost');
-
     setTimeout(function() {
-      console.log('\n\n🤖time to live elapsed... trying next set...\n\n');
+      console.log('\n\n🤖time to live elapsed... try ing next set...\n\n');
       sets_index = sets_index + 1;
       proxy.stop();
       return callback(null);
     }, set.time_in_seconds * 1000);
   }, function complete(err, res) {
-    proxy.stop();
+      proxy.stop();
   });
-} else {
-  console.log('using remote luminati proxy: ' + config.proxy.luminati.addr);
-  proxy = undefined;
+} 
+else {
+  // console.log('Running proxy normal mode..')
+
+  options = {
+      customer: 'kipthis', 
+      password: 'e49d4ega1696', 
+      zone: 'gen'
+  };
+  // try {
+  //   proxy = new Luminati(options);
+  //   proxy.listen(24000, '127.0.0.1')
+  // } catch(err) {
+  //   console.log('\n\nUnable to start Luminati Proxy.. try a global npm install.. ', err,'\n\n');
+  // }
 }
 
 var request = function(url) {
       var status = proxy_status.check();
       var res;
       if (status.ready) {
-        // console.log('\nfiring luminati...\n');
-        res = luminati_request(url, proxy, status.status);
-      } else {
-        // console.log('\nfiring mesh...\n');
-        res = mesh_request(url, status.status);
+          // console.log('\nfiring luminati...\n')
+          res = luminati_request(url, proxy, status.status);
       }
+      else{
+        // console.log('\nfiring mesh...\n')
+        res = mesh_request(url, status.status)
+      }
+      
       // stopProxy(proxy, 10000);
+
       return res;
 };
 
@@ -77,10 +92,15 @@ var ensured_request = function(url) {
 
 
 function stopProxy(proxy, ms) {
-   setTimeout(function(){ proxy.stop(); }, ms);
+   setTimeout(function(){
+        // if (res instanceof Error || res == null) { 
+          // console.log('\n\n\nProxy error: ', res.statusCode,'\n\n\n');
+          proxy.stop();
+        // }
+      },ms)
 }
 
 module.exports = {
   request: request,
   ensured_request: ensured_request
-};
+}
--- src/chat/components/proxy/status.js
+++ src/chat/components/proxy/status.js
@@ -2,7 +2,6 @@ var async = require('async');
 var request = require('request');
 var status = {};
 var cheerio = require('cheerio');
-var config = require('../../../config');
 var sets = require('./settings').sets;
 var proxy_opt = {
       customer: 'kipthis', 
@@ -15,27 +14,25 @@ var proxy_opt = {
       // log: 'NONE'
   };
 module.exports = { check: check, options: proxy_opt, current_index: 0};
-var ping_interval = config.proxy.luminati.intervalMillis;
+var argv = require('minimist')(process.argv.slice(2));
+var test_mode = argv.proxy ? argv.proxy : false;
+var ping_interval = (argv.proxy && argv.interval) ? argv.interval : 300000;
 
 async.whilst(
     function () { 
       return true; 
     },
     function (callback) {
-      var proxy_url = config.proxy.luminati.addr;
-      if (proxy_url.indexOf('http') < 0) proxy_url = 'http://' + proxy_url;
-
-      var amazon_url = 'https://www.amazon.com/gp/product/B01AW25FPU/';
+      var amazon_url = 'https://www.amazon.com/gp/product/B01AW25FPU/'
       var macys_url = 'http://www1.macys.com/shop/mens-clothing/mens-shoes?id=65&edge=hybrid&cm_sp=c2_1111US_catsplash_men-_-row4-_-icon_shoes'
-      var test_url = 'http://kipthis.com/DONTTOUCH.txt';
+      var test_url = 'http://kipthis.com/DONTTOUCH.txt'
       var url = amazon_url;
       var options = {
-        url: url,
-        proxy: proxy_url,
+        url: url, 
+        proxy: 'http://127.0.0.1:24000',
         timeout: 3000
       };
-
-      var begin = Date.now();
+      var begin= Date.now();
       status.last_ping= Date.now();
       request(options,function(err, res, body){ 
         if (err) {
@@ -46,7 +43,7 @@ async.whilst(
           // console.log('\nstatus: ',status,'\n');
           db.Metrics.log('proxy', { proxy: 'luminati', check: true,request_url: url, delay_ms: status.latency, success: false, error: err, status: status, options: {id: module.exports.current_index ,config: sets[module.exports.current_index].config } }); 
           setTimeout(function () {
-            callback();
+            callback()
           }, ping_interval);
         } 
         else if (res && res.statusCode == 200 && body.length > 0) {
@@ -57,7 +54,7 @@ async.whilst(
           status.data_retrieved = reviews ? true : false;
           status.age = (Date.now() - status.last_ping)/10000;
           // console.log('\nstatus: ',status,'\n');
-          db.Metrics.log('proxy', { proxy: 'luminati', check: true, request_url: url, delay_ms: status.latency, success: true, status: status, options: {id: module.exports.current_index ,config: sets[module.exports.current_index].config}});
+          db.Metrics.log('proxy', { proxy: 'luminati', check: true,request_url: url, delay_ms: status.latency, success: true, status: status, options: {id: module.exports.current_index ,config: sets[module.exports.current_index].config}});
           setTimeout(function () {
             callback()
           }, ping_interval);
