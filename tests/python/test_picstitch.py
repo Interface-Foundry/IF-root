47cac052effed93049937558c7e113a7f2c6a7b4
graham <graham.annett@gmail.com>
2016-12-17 18:30:57 -0800

    basic testing for picstitch

---
+++ tests/python/test_picstitch.py
@@ -0,0 +1,38 @@
+import sys
+import json
+import pytest
+sys.path.append('../../src/image_processing/')
+import server
+
+# import pytest
+# import requests
+
+# # run server and let it load
+# import subprocess
+
+# # time.sleep(5)
+
+facebook_data = [{
+    'price': '$5.99',
+    'prime': '0',
+    'origin': 'facebook',
+    'reviews': {
+        'rating': 3.8,
+        'reviewCount': '73'
+    },
+    'url': 'http://ecx.images-amazon.com/images/I/51Z99-v-fWL.jpg',
+    'name': [
+        'Artist: Firm',
+        'Interscope --Fontana--',
+        'Format: Explicit Lyrics'
+    ]
+}]
+
+
+def test_server():
+    test_client = server.application.test_client()
+    response = test_client.post('/',
+                                data=json.dumps(facebook_data),
+                                content_type='application/json')
+
+    assert response.status_code == 200
