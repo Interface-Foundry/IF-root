00d201003fae384b5393e881cb5e19fe9f5fc92f
Hannah Katznelson <hannah.katznelson@gmail.com>
2017-01-05 12:44:44 -0500

    fixed budget attachment issue

--- src/chat/components/delivery.com/handlers_budget.js
+++ src/chat/components/delivery.com/handlers_budget.js
@@ -31,31 +31,51 @@ handlers['food.admin.team_budget'] = function * (message) {
   if (message.text) {
     var num = parseNumber(message.text)
 
-    message.data = {};
-    message.data.value = {};
-    message.data.value.budget = num;
-    message.data.value.new = true;
-    yield handlers['food.admin.confirm_budget'](message);
+    if (num) {
+      message.data = {};
+      message.data.value = {};
+      message.data.value.budget = num;
+      message.data.value.new = true;
+      yield handlers['food.admin.confirm_budget'](message);
+    }
+    else {
+      yield $replyChannel.send(message, 'food.admin.team_budget', {type: message.origin, data: {
+        attachments: [{
+          'mrkdwn_in': [
+            'text'
+          ],
+          'text': "I didn\'t understand that - please type a number or click one of buttons",
+          'color': '#fc9600',
+          'attachment_type': 'default',
+          'actions': [],
+          'callback_id': 'food.admin.team_budget',
+          'fallback': "I didn\'t understand that - please type a number or click one of buttons",
+        }]
+      }})
+    }
   }
 
   var msg_json = {
-    'attachments': [
-      {
+    'attachments': []
+  }
+
+  for (var i = 0; i < budget_options.length; i++) {
+    if (i == 0 || i % 5 == 0) {
+      // console.log('new attachment', i)
+      msg_json.attachments.push({
         'mrkdwn_in': [
           'text'
         ],
-        'text': msg_text,
+        'text': (i == 0 ? msg_text : ''),
         'fallback': 'I am fallback hear me fall back!',
         'callback_id': 'food.admin.team_budget',
         'color': '#3AA3E3',
         'attachment_type': 'default',
         'actions': []
-      }
-    ]
-  }
-
-  for (var i = 0; i < budget_options.length; i++) {
-    msg_json.attachments[0].actions.push({
+      })
+    }
+    // console.log(i, budget_options[i])
+    msg_json.attachments[Math.floor(i / 5)].actions.push({
       'name': 'food.admin.confirm_budget',
       'text': `$${budget_options[i]}`,
       'style': 'default',
@@ -67,13 +87,30 @@ handlers['food.admin.team_budget'] = function * (message) {
     })
   }
 
-  msg_json.attachments[0].actions.push({
+  var noneButton = {
     'name': 'passthrough',
     'text': 'None',
     'style': 'default',
     'type': 'button',
     'value': 'food.admin_polling_options'
-  })
+  };
+
+  if (budget_options.length % 5 == 0) {
+    msg_json.attachments.push({
+      'mrkdwn_in': [
+        'text'
+      ],
+      'text': "",
+      'fallback': 'I am fallback hear me fall back!',
+      'callback_id': 'food.admin.team_budget',
+      'color': '#3AA3E3',
+      'attachment_type': 'default',
+      'actions': [noneButton]
+    })
+  }
+  else {
+    msg_json.attachments[msg_json.attachments.length - 1].actions.push(noneButton)
+  }
 
   if (!message.text) {
     msg_json.attachments.push({
@@ -106,8 +143,12 @@ handlers['food.admin.confirm_budget'] = function * (message) {
             break;
           }
         }
-        if (Number(budget) > Number(budget_options[budget_options.length])) {
+        if (Number(budget) > Number(budget_options[budget_options.length-1])) {
+          console.log('new highest budget!:', budget, "-- previously", budget_options[budget_options.length-1])
           budget_options.push(budget)
+        }
+        else {
+          console.log('no higher budget added', budget, "-- previously", budget_options[budget_options.length-1])
         }
         locations[i].budgets = budget_options
       }
