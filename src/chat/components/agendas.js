9a50ebdd94feeaae1c0cadcc630c8dab988b4b28
muchimoto <muchimoto@gmail.com>
2017-01-06 15:55:28 -0500

    basic email notification for cart statuses to admins

--- src/chat/components/agendas.js
+++ src/chat/components/agendas.js
@@ -4,15 +4,8 @@ var agenda = new Agenda({db: {address: config.mongodb.url}});
 var co = require('co');
 var _ = require('lodash');
 
-// var jobTypes = ['email'];
-// jobTypes.forEach(function(type) {
 require('./jobs/email')(agenda);
 require('./jobs/reminder')(agenda);
-// })
-
-// if(jobTypes.length) {
-  // agenda.start();
-// }
 
 agenda.on('ready', function () {
   agenda.start();
