38a1104c9491dc200ce3083408c644aeadeec6ba
Hannah Katznelson <hannah.katznelson@gmail.com>
2017-01-11 15:26:28 -0500

    cuisine-specific emojis work, cuisine-specifically

--- src/chat/components/delivery.com/handlers_votes.js
+++ src/chat/components/delivery.com/handlers_votes.js
@@ -746,17 +746,23 @@ handlers['food.admin.restaurant.confirm'] = function * (message) {
   }
   var url = yield googl.shorten(merchant.summary.url.complete)
 
+  console.log('HEGEL\'S BAGELS', merchant.summary.cuisines[0])
+
   logging.data('using merchant for food service', merchant.id)
   foodSession.chosen_restaurant = {
     id: merchant.id,
     name: merchant.summary.name,
     url: url,
-    minimum: merchant.ordering.minimum
+    minimum: merchant.ordering.minimum,
+    cuisine: merchant.summary.cuisines[0]
   }
 
+  console.log('foodSession.chosen_restaurant', foodSession.chosen_restaurant)
   foodSession.menu = yield api.getMenu(merchant.id)
 
   foodSession.save()
+
+  console.log('moment o\' truth', foodSession.chosen_restaurant)
 
   return yield handlers['food.admin.restaurant.collect_orders'](message, foodSession)
 }
