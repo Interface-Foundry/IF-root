2a1c5319bd3bac71487501633b130d3a2a46df5a
Timothy Wong <twong001@citymail.cuny.edu>
2017-01-17 12:27:17 -0500

    pokemon gym 2 files added

---
+++ src/pokemon-gym2/client/main.js
@@ -0,0 +1,30 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import "./admin1/assets/plugins/3d-bold-navigation/js/modernizr.js"
+import "./admin1/assets/plugins/jquery/jquery-3.1.0.min.js"
+import "./admin1/assets/plugins/jquery-ui/jquery-ui.min.js"
+import "./admin1/assets/plugins/pace-master/pace.min.js"
+import "./admin1/assets/plugins/jquery-blockui/jquery.blockui.js"
+import "./admin1/assets/plugins/bootstrap/js/bootstrap.min.js"
+import "./admin1/assets/plugins/jquery-slimscroll/jquery.slimscroll.min.js"
+import "./admin1/assets/plugins/switchery/switchery.min.js"
+import "./admin1/assets/plugins/uniform/js/jquery.uniform.standalone.js"
+import "./admin1/assets/plugins/offcanvasmenueffects/js/classie.js"
+import "./admin1/assets/plugins/waves/waves.min.js"
+import "./admin1/assets/plugins/3d-bold-navigation/js/main.js"
+import "./admin1/assets/plugins/waypoints/jquery.waypoints.min.js"
+import "./admin1/assets/plugins/toastr/toastr.min.js"
+import "./admin1/assets/plugins/flot/jquery.flot.min.js"
+import "./admin1/assets/plugins/flot/jquery.flot.time.min.js"
+import "./admin1/assets/plugins/flot/jquery.flot.symbol.min.js"
+import "./admin1/assets/plugins/flot/jquery.flot.resize.min.js"
+import "./admin1/assets/plugins/flot/jquery.flot.tooltip.min.js"
+import "./admin1/assets/plugins/curvedlines/curvedLines.js"
+import "./admin1/assets/plugins/chartjs/Chart.bundle.min.js"
+import "./admin1/assets/js/meteor.min.js"
+import "./admin1/assets/js/pages/dashboard.js"
+
+import './main.html';
+
+
