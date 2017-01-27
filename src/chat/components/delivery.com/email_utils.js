c5b75adfd02dad6d6a3a12712a95031e95bc98a0
Hannah Katznelson <hannah.katznelson@gmail.com>
2017-01-27 10:32:46 -0500

    more qp structuring

--- src/chat/components/delivery.com/email_utils.js
+++ src/chat/components/delivery.com/email_utils.js
@@ -34,7 +34,7 @@ utils.quickpickHTML = function * (foodSession, slacklink, email) {
     html += '<tr>';
     for (var j = 0; j < row_length; j++) {
       var item_url = yield menu_utils.getUrl(foodSession, user.id, [quickpicks[row_length*i+j].id])
-      html += `<td style="width:300px;padding:10px;" bgcolor=${ryan_grey}><a style="color:black;text-decoration:none;display:block;width:100%;height:100%" href="` + `${item_url}` + `">`
+      html += `<td style="width:300px;padding:10px;position:absolute;" bgcolor=${ryan_grey}><a style="color:black;text-decoration:none;display:block;width:100%;height:100%" href="` + `${item_url}` + `">`
       html += this.formatItem(i, j, quickpicks) + '</a>' + '</td>';
     }
     html += '</tr>';
@@ -46,7 +46,10 @@ utils.quickpickHTML = function * (foodSession, slacklink, email) {
   html += `<p><a style="color:${kip_blue};" href="' + merch_url + '">Click to View Full Menu `
   html += menu_utils.cuisineEmoji(resto.data.summary.cuisines[0]) + '</a>' + br + br
 
-  html += `<table border="0" style="padding:10px;width:600px;background-color:${kip_blue};"><p>DRACULA\'S WEDDING</p></table>` + br
+  html += `<table border="0" style="padding:10px;width:600px;background-color:${kip_blue};">`
+  html += `<tr></tr>`
+  html += `<tr></tr>`
+  html += `</table>` + br
 
   html += `<a style="color:${kip_blue};text-decoration:none;" href="${slacklink}">Join your team on Slack!</a>` + br + br
   html += `<a style="color:${kip_blue};text-decoration:none;" href="https://kipthis.com/legal.html">Terms of Service</a>`
@@ -56,12 +59,13 @@ utils.quickpickHTML = function * (foodSession, slacklink, email) {
 }
 
 utils.formatItem = function (i, j, quickpicks) {
-  return `<table border="0">` +
-  `<tr><td style="font-weight:bold;">${quickpicks[row_length*i+j].name}</td></tr>` +
-  `<tr><td>${quickpicks[row_length*i+j].description}</td></tr>` +
-  `<tr><td style="padding:8px 0 8px 0;font-weight:bold;">$${parseFloat(quickpicks[row_length*i+j].price).toFixed(2)}</td></tr>` +
-  `<tr><td><div style="display:inline-block;background-color:white;border-radius:8px;border-color:${kip_blue};border-style:solid;border-width:2px;"><p style="font-weight:bold;color:${kip_blue}">&nbsp;&nbsp;&nbsp;Add to Cart&nbsp;&nbsp;&nbsp;</p></div></td></tr>` +
-  `</table>`;
+  return `<table border="0" style="position:absolute;"><tr style="position:absolute;top:30px;"><td>` +
+  `<table><tr><td style="font-weight:bold;">${quickpicks[row_length*i+j].name}</td></tr>` +
+  `<tr><td>${quickpicks[row_length*i+j].description}</td></tr></table></td></tr>` +
+  `<tr><td><table><tr><td style="padding:8px 0 8px 0;font-weight:bold;">$${parseFloat(quickpicks[row_length*i+j].price).toFixed(2)}</td></tr>` +
+  `<tr><td><div style="display:inline-block;background-color:white;border-radius:8px;border-color:${kip_blue};border-style:solid;border-width:2px;">` +
+  `<p style="font-weight:bold;color:${kip_blue}">&nbsp;&nbsp;&nbsp;Add to Cart&nbsp;&nbsp;&nbsp;</p></div></td></tr></table>` +
+  `</td></tr></table>`;
 }
 
 module.exports = utils;
