65de3110ddd955d0ef1586aba8574ba5d371bee1
muchimoto <muchimoto@gmail.com>
2017-01-27 17:29:45 -0500

    starting dashboard: added db scripts and dashboard frame

--- src/chat/components/sales/imports/ui/CSVDrop.jsx
+++ src/chat/components/sales/imports/ui/CSVDrop.jsx
@@ -3,34 +3,97 @@ import Dropzone from 'react-dropzone';
 import csv from 'csvtojson';
 import _ from 'lodash';
 import { Metrics } from '../api/metrics.js';
+import subMonths from 'date-fns/sub_months';
+import addMonths from 'date-fns/add_months';
+
+var propz;
 
 export default class CSVDrop extends Component {
-    async onDrop (acceptedFiles, rejectedFiles) {
-      console.log('Accepted files: ', acceptedFiles);
+
+    constructor(props) {
+      super(props)
+      propz = props;
+    }
+
+  // 0:"Sports & Outdoors"
+  // 1:"Fallout Vault Tec Pip Boy PipBoy Cosplay Morale PS4 XBOX PVC Rubber 3D Velcro Patch"
+  // 2:"B01B9KUGGM"
+  // 3:"2016-10-07 17:15:19"
+  // 4:"1"
+  // 5:"8.95"
+  // 6:"PA-API"
+  // 7:"eileenog-20"
+  // 8:"ndi"
+  // 9:"DESKTOP"
+    onDrop (acceptedFiles, rejectedFiles) {
+      var results = [];
+      var count = 0;
       if (acceptedFiles.length > 0) {
           acceptedFiles.forEach((file)=> {
            Papa.parse( file, {
-              header: true,
+              header: false,
               complete( data, file ) {
-                console.log('results:', data);
-                if(data.data.length > 1) {
-                  data = data.data.slice(1, data.length);
-                  data.map((datum) => {
-                    let item = datum.__parsed_extra;
-                    let metrics = Metrics.find({'metric':'cart.link.click', 'data.asin': _.get(item,'[0]')}).fetch();
-                    console.log(' metrics in db', _.get(item,'[0]'), metrics, Metrics.find().count());                  
-                  });
-                } else {
-                  console.log('data length:', data);
-                };
-                // Handle the upload here.
+                  if(data.data.length > 1) {
+                    data = data.data;
+                    data.map((item) => {
+                      count++;
+                      console.log(count);
+                      if (item[1]) {
+                        let iasin = _.get(item,'[2]')
+                        // console.log('item: ', item[1]);
+                        let purchaseDate = new Date(_.get(item,'[3]'));
+                        let allowance = new Date(addMonths(purchaseDate, 3));
+                        // console.log('iasin',iasin)
+                        let clm = Metrics.find({"metric":"cart.link.click",  "data.asins": iasin }).fetch();
+                        if (clm && clm.length > 0) {
+                          console.log('found!', iasin)
+                          clm.map( (m) => {
+                             // && (purchaseDate < allowance)
+                            if ((purchaseDate > m.data.ts )) {
+                              m.data.category = _.get(item,'[0]');
+                              m.data.purchased = _.get(item,'[3]');
+                              m.data.purchase_quantity = _.get(item,'[4]');
+                              m.data.price = _.get(item,'[5]');
+                              m.data.device =  _.get(item,'[9]');
+                              m.data.type = 'cart-link';
+                              var updated = Metrics.update({'_id': m._id},m);
+                              console.log('updated1: ', updated);
+                              results.push(m);
+                            }
+                          })
+                         }
+                         let ilm = Metrics.find({'metric':'item.link.click',  "data.asins": iasin }).fetch();
+                         if (ilm && ilm.length > 0) {
+                          ilm.map( (m) => {
+                             // && (purchaseDate < allowance)
+                            if ((purchaseDate > m.data.ts )) {
+                              m.data.category = _.get(item,'[0]');
+                              m.data.purchased = _.get(item,'[3]');
+                              m.data.purchase_quantity = _.get(item,'[4]');
+                              m.data.price = _.get(item,'[5]');
+                              m.data.device =  _.get(item,'[9]');
+                              m.data.type = 'item-link';
+                              var updated = Metrics.update({'_id': m._id},m);
+                              console.log('updated2: ', updated)
+                              results.push(m);
+                            }
+                          })
+                         }
+                        }
+                      })
+                    console.log('total updated: ', results);
+                    propz.getMetrics(results);   
+                  } else {
+                    console.log('data length:', data);
+                  };
               }
             });
           });
-
+       } else{
+          console.log('Rejected files: ', rejectedFiles);
        }
-      console.log('Rejected files: ', rejectedFiles);
     }
+
     render() {
       return (
           <div>
@@ -40,4 +103,9 @@ export default class CSVDrop extends Component {
           </div>
       );
      }
+
 };
+
+CSVDrop.propTypes = {
+  getMetrics: PropTypes.func.isRequired
+}
