5cc413819de432794a25960247cef6ce470ea6c3
Timothy Wong <twong001@citymail.cuny.edu>
2017-01-31 18:42:32 -0500

    fixed conflict in merge

--- src/chat/components/dash/build/assets.js
+++ src/chat/components/dash/build/assets.js
@@ -1,4 +1,2 @@
-<<<<<<< HEAD
-=======
+
 module.exports = {"main":{"js":"/assets/main.js?2c626062daf32c240859"}};
->>>>>>> alter certain message and order queries for easier use in the dashboard. added a flowchart page to the sidebar
