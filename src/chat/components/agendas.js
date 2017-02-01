c65fc5505d16d1c140075ab043c23db4d85c6924
muchimoto <muchimoto@gmail.com>
2017-02-01 17:30:30 -0500

    setting time zone for agenda jobs

--- src/chat/components/agendas.js
+++ src/chat/components/agendas.js
@@ -7,8 +7,8 @@ require('./jobs/reminder')(agenda);
 require('./jobs/initial_reminder')(agenda);
 require('./jobs/feature_rollout')(agenda);
 
-let cartEmailInterval = (process.env.JOB_CART_EMAIL && typeof process.env.JOB_CART_EMAIL == 'string') ? process.env.JOB_CART_EMAIL : '00 18 * * 3';
-let featureInterval = (process.env.JOB_FEATURE_ROLLOUT && typeof process.env.JOB_FEATURE_ROLLOUT == 'string') ? process.env.JOB_FEATURE_ROLLOUT : '00 18 * * *';
+let cartEmailInterval = (process.env.JOB_CART_EMAIL && typeof process.env.JOB_CART_EMAIL == 'string') ? process.env.JOB_CART_EMAIL : '30 17 * * 3';
+let featureInterval = (process.env.JOB_FEATURE_ROLLOUT && typeof process.env.JOB_FEATURE_ROLLOUT == 'string') ? process.env.JOB_FEATURE_ROLLOUT : '31 17 * * *';
 
 agenda.on('ready', function () {
 	logging.info('Send cart status email jobs to run at: ', cartEmailInterval);
@@ -22,11 +22,10 @@ agenda.on('ready', function () {
 		if (err) console.log(err);
 	});	
 	if (cartEmailInterval) { 
-		agenda.every(cartEmailInterval, 'send cart status email', {});
+		agenda.create('send cart status email', {}).schedule('6:01pm').repeatEvery('1 day', { timezone: 'America/New_York'}).save();
 	}
   if (featureInterval) {
-		logging.info('Set up feature roll out jobs.');
-  	agenda.every(featureInterval, 'feature rollout', { feature: 'oregano'});
+  	agenda.create('feature rollout', { feature: 'oregano'}).schedule('6:01pm').repeatEvery('1 day', { timezone: 'America/New_York'}).save();
   }
   agenda.start();
 });
