e073f2faace9e10f0be42c99f3d31155a3a208c1
muchimoto <muchimoto@gmail.com>
2017-02-01 15:49:49 -0500

    Feature rollout and weeky cart snapshot jobs implemented and ready for testing

--- src/chat/components/jobs/email.js
+++ src/chat/components/jobs/email.js
@@ -1,50 +1,64 @@
+// jobs/email.js
 var mailer = require('../../../mail/IF_mail.js')
 var fs = require('fs');
 var kipcart = require('../cart');
 var co = require('co');
 var _ = require('lodash');
+var async = require('async');
+var kipcart = require('../cart');
+var utils = require('../slack/utils');
+
 
 module.exports = function(agenda) {
   agenda.define('send cart status email', function (job, done) {
-    db.Chatusers.find(job.attrs.data.userId, function(err, user) {
-      if(err) return done(err);
-      var html = `Thank you for using Kip! Here is the list of items in your team cart:\n\n<table border="1"><thead><tr><th>Item</th><th>Price</th><th>Quantity</th><th>Added By</th></tr></thead>`
-      kipcart.getCart(job.attrs.data.cart_id).then(function(cart, err){
-
-        co(function*(){
-            if (!_.get(cart,'aggregate_items')) {
-              kip.debug('/jobs/email.js:22: There are no items in cart. Aborting weekly cart status email.');
-              return
-            }
-            var userNames = [];
+  co( function * () {
+    logging.info('started job: send cart status email');
+    let slackbots = yield db.Slackbots.find({'meta.weekly_status_enabled': true});
+    let teams = yield slackbots.map( function * (team) { 
+      let admins = yield utils.findAdmins(team);
+      return { team: team, admins: admins }
+    });
+    logging.info('\nfound teams in db: ', teams.length,'\n');
+    async.eachSeries(teams, function(obj, callback) {
+      co( function * () {
+        if (!(_.get(obj,'admins') && _.get(obj,'admins').length > 0)) return callback();
+        let carts = yield db.Carts.find({"slack_id": obj.team.team_id}).populate('items').exec();
+        let cart = _.get(carts,'[0]');
+        if (!cart || !(cart && _.get(cart,'aggregate_items') && _.get(cart,'aggregate_items').length > 0)) return callback()
+        async.eachSeries(obj.admins, function(admin, callback2){
+          co(function * () {
+            let html = `Thank you for using Kip! Here is the list of items in your team cart:\n\n<table border="1"><thead><tr><th>Item</th><th>Price</th><th>Quantity</th><th>Added By</th></tr></thead>`
+            let userNames = [];
             yield cart.aggregate_items.map( function * (item) {
-              var addedUser = yield db.Chatusers.find({'id': item.added_by[0]}).limit(1).exec();
+              let addedUser = yield db.Chatusers.find({'id': item.added_by[0]}).limit(1).exec();
               userNames[_.get(addedUser,'[0].id')] = _.get(addedUser,'[0].name')
             })
-                 
-            var orders = cart.aggregate_items.map( function (item) {
-              var names = item.added_by.map(function(id) { return userNames[id] })
+            let orders = cart.aggregate_items.map( function (item) {
+              let names = item.added_by.map(function(id) { return userNames[id] })
               html += `<tr><td>${_.get(item,'title')}</td><td>${_.get(item,'price')}</td><td>${_.get(item,'quantity')}</td><td>${names.join(' ')}</td></tr>`
             });
-
             html += `<tr><a href=${cart.link}>Check out now!</a></tr>`
-            var payload = {
-              to: `"${_.get(user,'[0].name')}" <${job.attrs.data.to}>`,
+            let payload = {
+              to: `"${_.get(admin,'name')}" <${_.get(admin,'profile.email')}>`,
               from: `Kip Café <hello@kipthis.com>`,
-              subject: job.attrs.data.subject,
+              subject: 'This is your weekly team cart status email from Kip!',
               html: html
             }
             mailer.sendMail(payload, function(err, info) {
               if (err) return kip.debug(' \n\n\n\n /jobs/email.js: error sending email: ', err, ' \n\n\n\n ');
-              done();
+              logging.info('job: sent cart snapshot email to ', admin.name, ' for team ', obj.team.team_name, ' ',obj.team.team_id, ' info: ', info);
+              return setTimeout(callback2, 2000);
             });
-        })
-
-      });
-    });
-  });
-
-
-
-  // More email related jobs
+          })
+        }, function(err){
+          setTimeout(callback, 2000)
+        }) 
+     })
+    }, function(err){
+      if(err) logging.err('job: weekly cart snapshots err', err)
+      logging.info('finished job: send cart status email');
+      done()
+    })
+  })
+ });
 }
