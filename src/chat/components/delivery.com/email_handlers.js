f4f5522c4aad8f2b1ab9cdf692c746fa47434680
hannahlorane <hannah.katznelson@gmail.com>
2017-02-01 19:01:22 -0500

    Email (#764)
    * email square are links
    
    * hyperlink styling removed
    
    * styling from menu link removed
    
    * cuisine emoji
    
    * email styling
    
    * add to slack link
    
    * remove on delete bug fixed
    
    * grey table
    
    * more than egg emojis
    
    * flexible grid dimensions
    
    * refactored qp email
    
    * pretty rounded buttons
    
    * more qp structuring
    
    * footer formatted
    
    * added text
    
    * kip pay ngrok updated
    
    * height of add to cart increased
    
    * footer restyled
    
    * headers
    
    * line items
    
    * misc styling
    
    * item instructions
    
    * address and delivery instructions
    
    * emails no longer sent
    
    * links in receipt
    
    * first draft of receipt email
    
    * kip head instead of slack
    
    * onsuccess has been moved to handlers_checkout area

--- src/chat/components/delivery.com/email_handlers.js
+++ src/chat/components/delivery.com/email_handlers.js
@@ -21,8 +21,10 @@ handlers['food.admin.team.add_order_email'] = function * (message) {
 handlers['food.admin.team.remove_order_email'] = function * (message) {
   var foodSession = yield db.delivery.findOne({team_id: message.source.team, active: true}).exec()
   var e = message.source.callback_id;
-  foodSession.email_users.splice(foodSession.email_users.indexOf(e), 1);
-  yield db.delivery.update({team_id: message.source.team, active: true}, {$set: {email_users: foodSession.email_users}});
+  if (foodSession.email_users.indexOf(e) > -1) {
+    foodSession.email_users.splice(foodSession.email_users.indexOf(e), 1);
+    yield db.delivery.update({team_id: message.source.team, active: true}, {$set: {email_users: foodSession.email_users}});
+  }
   yield handlers['food.admin.team.email_members'](message);
 }
 
