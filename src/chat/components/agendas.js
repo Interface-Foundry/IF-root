6aa3569c13eb3f342ab01a04cc75923ecb9daa28
muchimoto <muchimoto@gmail.com>
2017-02-02 15:12:22 -0500

    feature rollout script instead of agenda

--- src/chat/components/agendas.js
+++ src/chat/components/agendas.js
@@ -5,7 +5,6 @@ var agenda = new Agenda({db: {address: config.mongodb.url}});
 require('./jobs/email')(agenda);
 require('./jobs/reminder')(agenda);
 require('./jobs/initial_reminder')(agenda);
-require('./jobs/feature_rollout')(agenda);
 
 agenda.on('ready', function () {
 	agenda.cancel({name: 'send cart status email'}, function(err, numRemoved) {
@@ -15,7 +14,6 @@ agenda.on('ready', function () {
 		if (err) console.log(err);
 	});	
 	// agenda.create('send cart status email', {}).schedule('11:21am').repeatEvery('1 day', { timezone: 'America/New_York'}).save();
-	// agenda.create('feature rollout', { feature: 'oregano'}).schedule('11:21am').repeatEvery('1 day', { timezone: 'America/New_York'}).save();
   agenda.start();
 });
 
