67913bec3cc2659d28f0fc6aea242e2512f322d3
graham <graham.annett@gmail.com>
2017-02-01 19:40:13 -0800

    remove the refresh ims for time beign

--- src/chat/components/delivery.com/handlers_admin_initial.js
+++ src/chat/components/delivery.com/handlers_admin_initial.js
@@ -56,7 +56,8 @@ handlers['food.admin.select_address'] = function * (message, banner) {
   // loading chat users here for now, can remove once init_team is fully implemented tocreate chat user objects
   logging.debug('doing slackutils stuff in here')
   var team = yield db.Slackbots.findOne({team_id: message.source.team}).exec()
-  yield [slackUtils.refreshAllUserIMs(team.bot.bot_access_token), slackUtils.getTeamMembers(team)]
+  // yield [slackUtils.refreshAllUserIMs(team.bot.bot_access_token), slackUtils.getTeamMembers(team)]
+  yield [slackUtils.getTeamMembers(team)]
 
   message.state = {}
   var foodSession = yield utils.initiateDeliverySession(message)
