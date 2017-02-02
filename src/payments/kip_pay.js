9be525fb719f5bc9281214b0dc7ea27159f7a4ed
graham <graham.annett@gmail.com>
2017-02-01 16:00:06 -0800

    onsuccess has been moved to handlers_checkout area

--- src/payments/kip_pay.js
+++ src/payments/kip_pay.js
@@ -279,7 +279,7 @@ app.post('/process', (req, res) => co(function * () {
 
   try {
     // send success messages to order members
-    yield payUtils.onSuccess(payment)
+    // yield payUtils.onSuccess(payment)
     profOak.say(`order completed for team: ${payment.order.team_id}`)
   } catch (err) {
     logging.error('error onSuccess of payment', err)
