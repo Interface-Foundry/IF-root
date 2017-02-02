5f1cfd409b4f630672698b7db0fc17565d92de91
muchimoto <muchimoto@gmail.com>
2017-02-02 11:29:57 -0500

    turning off jobs for now

--- src/chat/components/agendas.js
+++ src/chat/components/agendas.js
@@ -14,8 +14,8 @@ agenda.on('ready', function () {
 	agenda.cancel({name: 'feature rollout'}, function(err, numRemoved) {
 		if (err) console.log(err);
 	});	
-	agenda.create('send cart status email', {}).schedule('11:21am').repeatEvery('1 day', { timezone: 'America/New_York'}).save();
-	agenda.create('feature rollout', { feature: 'oregano'}).schedule('11:21am').repeatEvery('1 day', { timezone: 'America/New_York'}).save();
+	// agenda.create('send cart status email', {}).schedule('11:21am').repeatEvery('1 day', { timezone: 'America/New_York'}).save();
+	// agenda.create('feature rollout', { feature: 'oregano'}).schedule('11:21am').repeatEvery('1 day', { timezone: 'America/New_York'}).save();
   agenda.start();
 });
 
