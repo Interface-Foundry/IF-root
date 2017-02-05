20351c766c6ab6959799a5d6039bef87626123b3
pbrandt1 <peter.m.brandt@gmail.com>
2017-02-05 13:02:31 -0500

    removing agendas for now

--- src/chat/components/agendas.js
+++ src/chat/components/agendas.js
@@ -14,9 +14,8 @@ agenda.on('ready', function () {
 	agenda.cancel({name: 'feature rollout'}, function(err, numRemoved) {
 		if (err) console.log(err);
 	});
-	agenda.create('send cart status email', {}).schedule('17:33pm').repeatEvery('1 day', { timezone: 'America/New_York'}).save();
-	agenda.create('feature rollout', { feature: 'oregano'}).schedule('6:30pm').repeatEvery('1 day', { timezone: 'America/New_York'}).save();
-  agenda.start();
+	// agenda.create('send cart status email', {}).schedule('17:33pm').repeatEvery('1 day', { timezone: 'America/New_York'}).save();
+  // agenda.start();
 });
 
 module.exports = agenda;
