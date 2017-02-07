5b394c8fca9910270b0f93d0650ad9bef69ba0dd
Christopher Barry <me@chrisb.xyz>
2017-02-07 16:52:47 -0500

    some cleanup

--- src/chat/components/jobs/clear_response_url.js
+++ src/chat/components/jobs/clear_response_url.js
@@ -4,12 +4,10 @@ var ObjectId = require('mongodb').ObjectID;
 module.exports = function(agenda) {
   agenda.define('clear response', function(job, done) {
     let message_id = job.attrs.data.msgId;
-    kip.debug(`😜  Deleting response_url from ${JSON.stringify(job.attrs.data, null, 2)}`);
     co(function * () {
       var prevMessage = yield db.Message.findOne({
         '_id': new ObjectId(message_id)
       }).exec();
-      kip.debug(`🍔  trying to delete from ${JSON.stringify(prevMessage, null, 2)}`);
       if (prevMessage != null) {
         delete prevMessage.source.response_url;
         prevMessage.markModified('source.response_url');
