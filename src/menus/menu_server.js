b2c1588a3bc96ffc8b4d4f5a69642d336db6ff0d
Hannah Katznelson <hannah.katznelson@gmail.com>
2017-02-09 16:48:29 -0500

    removed logs

--- src/menus/menu_server.js
+++ src/menus/menu_server.js
@@ -152,12 +152,6 @@ router.post('/order', function (req, res) {
 
       console.log('calculated money spent')
 
-      // var orders = foodSession.confirmed_orders
-      // logging.debug('orders', orders)
-      // orders.push(user_id)
-
-      console.log('registered user as having ordered')
-
       yield Delivery.update({active: true, _id: ObjectId(deliv_id)}, {$set: {cart: cart, user_budgets: user_budgets}});
 
       console.log('updated the delivery object')
