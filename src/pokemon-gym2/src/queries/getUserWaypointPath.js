c261dd3fc026f7aa2d9c277b790712a29323bec0
Timothy Wong <twong001@citymail.cuny.edu>
2017-02-10 11:28:49 -0500

    Query for waypoint path added

--- src/pokemon-gym2/src/queries/getUserWaypointPath.js
+++ src/pokemon-gym2/src/queries/getUserWaypointPath.js
@@ -9,26 +9,28 @@ const getUserWaypointPath = (waypoints,user) =>
         },
       },
       {
+        $sort: {'timestamp': 1}
+      },
+      {
         $group: {
           _id: {
             delivery_id: '$delivery_id',
-            waypoint: '$waypoint',
-            timestamp: '$timestamp',
+            //waypoint: '$waypoint',
+            //timestamp: '$timestamp',
             user_id: '$user_id',
           },
+          waypoints: { $push: '$waypoint' }
         },
       },
-      {
-        $sort: {'_id.timestamp': 1}
-      },
+      
     ], (err, result) => {
       if (err) { reject(err); }
       const waypoints = result.map(waypoint => {
         return {
-          waypoint: waypoint._id.waypoint,
-          timestamp: waypoint._id.timestamp,
+          //timestamp: waypoint._id.timestamp,
           user_id: waypoint._id.user_id,
           delivery_id: waypoint._id.delivery_id,
+          waypoints: waypoint.waypoints,
         };
       });
       resolve(waypoints);
@@ -38,5 +40,5 @@ const getUserWaypointPath = (waypoints,user) =>
 module.exports = getUserWaypointPath;
 if (!module.parent) {
   require('../../../kip')
-  getUserWaypointPath(db.waypoints, 'U3620AA5T').then(console.log.bind(console)) //waypoints of past 2 weeks
+  getUserWaypointPath(db.waypoints, 'U3620AA5T').then(console.log.bind(console)) 
 }
