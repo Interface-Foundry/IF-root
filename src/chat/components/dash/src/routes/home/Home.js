fa3a6a914387afff9054d7cef8980dd7fb8f7652
Christopher Barry <me@chrisb.xyz>
2017-02-15 16:11:15 -0500

    resizeable charts

--- src/chat/components/dash/src/routes/home/Home.js
+++ src/chat/components/dash/src/routes/home/Home.js
@@ -48,17 +48,19 @@ function Home(props, context) {
                 </DropdownButton>
               </div>
             </span>}>
-              <ResponsiveContainer className="resizable-chart">
-                <AreaChart data={data} margin={{ top: 10, right: 30, left: 0, bottom: 0 }} >
-                  <XAxis dataKey="name" />
-                  <YAxis />
-                  <CartesianGrid stroke="#ccc" />
-                  <Tooltip />
-                  <Area type="monotone" dataKey="uv" stackId="1" stroke="#8884d8" fill="#8884d8" />
-                  <Area type="monotone" dataKey="pv" stackId="1" stroke="#82ca9d" fill="#82ca9d" />
-                  <Area type="monotone" dataKey="amt" stackId="1" stroke="#ffc658" fill="#ffc658" />
-                </AreaChart>
-              </ResponsiveContainer>
+              <div className="resizable">
+                <ResponsiveContainer width="100%" height="100%">
+                  <AreaChart data={data} margin={{ top: 10, right: 30, left: 0, bottom: 0 }} >
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <CartesianGrid stroke="#ccc" />
+                    <Tooltip />
+                    <Area type="monotone" dataKey="uv" stackId="1" stroke="#8884d8" fill="#8884d8" />
+                    <Area type="monotone" dataKey="pv" stackId="1" stroke="#82ca9d" fill="#82ca9d" />
+                    <Area type="monotone" dataKey="amt" stackId="1" stroke="#ffc658" fill="#ffc658" />
+                  </AreaChart>
+                </ResponsiveContainer>
+              </div>
           </Panel>
         </div>
       </div>
