9379c9c252f8ede1945472405b0be60149cb9b8d
pbrandt1 <peter.m.brandt@gmail.com>
2017-02-22 18:49:08 -0500

    cafe marketing campaign

---
+++ src/chat/components/slack/gen_users.js
@@ -0,0 +1,36 @@
+require('../../../kip')
+var fs = require('fs')
+var _ = require('lodash')
+
+
+db.Slackbots.find({
+  'meta.delted': {$ne: true},
+  'meta.dateAdded': {$lt: new Date('2017-01-01')}
+}).exec(function (e, bots) {
+  if (e) {
+    console.error(e)
+    process.exit(1)
+  }
+
+  //
+  // saves a json array of admin id's which we will ping onthe given day
+  //
+  function gen_day(day) {
+    // get the nth admin of every bot
+    const admins = bots
+      .map(b => _.get(b, 'meta.office_assistancts.' + (day - 1)))
+      .filter(Boolean)
+
+    // write the list of nth admins to a json file for use later
+    fs.writeFileSync(`users_day_${day}.json`, JSON.stringify(admins))
+  }
+
+  //
+  // generate four days worth of messages
+  //
+  gen_day(1)
+  gen_day(2)
+  gen_day(3)
+  gen_day(4)
+  process.exit(0)
+})
