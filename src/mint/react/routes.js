981ea7226a02bbf389e92e79746d8de7ee4062a5
Christopher Barry <me@chrisb.xyz>
2017-03-13 17:22:25 -0400

    adding react-redux-router to start using redux

--- src/mint/react/routes.js
+++ src/mint/react/routes.js
@@ -1,23 +1,70 @@
 // react/routes.js
 // handles choosing which route to render
-import React from 'react';
+import React from 'react'
+import ReactDOM from 'react-dom'
+
 import {
-  Router,
+  createStore,
+  combineReducers,
+  applyMiddleware
+} from 'redux'
+import {
+  Provider
+} from 'react-redux'
+
+import createHistory from 'history/createBrowserHistory'
+import {
   Route,
-  IndexRoute
-} from 'react-router';
+  Switch
+} from 'react-router'
 
-import App from './components/App';
-import NotFound from './components/NotFound';
+import {
+  ConnectedRouter,
+  routerReducer,
+  routerMiddleware,
+  push
+} from 'react-router-redux'
 
-const Routes = (props) => (
-  <Router {...props}>
-    <Route path="/" component={ App }>
-      <IndexRoute component={App} />
-      <Route path="/cart/:cart_id" component={ App } />
-    </Route>
-    <Route path="*" component={NotFound} />
-  </Router>
+// import reducers from './reducers' // Or wherever you keep your reducers
+import App from './pages/App';
+import NotFound from './pages/NotFound';
+
+// Create a history of your choosing (we're using a browser history in this case)
+const history = createHistory();
+
+// Build the middleware for intercepting and dispatching navigation actions
+const middleware = routerMiddleware(history);
+
+// Add the reducer to your store on the `router` key
+// Also apply our middleware for navigating
+const store = createStore(
+  combineReducers({
+    // ...reducers,
+    router: routerReducer
+  }),
+  applyMiddleware(middleware)
+);
+
+// const Routes = (props) => (
+//    <Provider store={store}>
+//     <ConnectedRouter {...props}>
+//       <Route path="/cart/:cart_id" component={ App }/>
+//       <Route path="*" component={NotFound} />
+//     </ConnectedRouter>
+//   </Provider>
+// );
+// 
+
+const Routes = () => (
+  <Provider store={store}>
+    { /* ConnectedRouter will use the store from Provider automatically */ }
+    <ConnectedRouter history={history}>
+      <Switch>
+        <Route path="/cart/:cart_id" component={ App }/>
+        <Route path="*" component={NotFound} />
+      </Switch>
+    </ConnectedRouter>
+  </Provider>
 );
 
 export default Routes;
