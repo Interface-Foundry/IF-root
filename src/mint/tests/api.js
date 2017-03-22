bc91ffb4140d25e31e02d634f253ba9302c62ced
pbrandt1 <peter.m.brandt@gmail.com>
2017-03-22 17:44:17 -0400

    adding resource test

--- src/mint/tests/api.js
+++ src/mint/tests/api.js
@@ -5,9 +5,9 @@ const assert = require('assert')
 /**
  * We will use the db to circumvent the whole email checking process
  */
-var db;
-const dbReady = require('../db');
-dbReady.then((models) => { db = models; }).catch(e => console.error(e));
+var db
+const dbReady = require('../db')
+dbReady.then((models) => { db = models }).catch(e => console.error(e))
 
 /**
  * mcTesty is our demo user. This is just some generic info about mcTesty, not their db record or anything real.
@@ -48,6 +48,17 @@ const post = function * (url, data) {
   })
 }
 
+const del = function * (url, data) {
+  url = 'http://localhost:3000' + url
+  var body = yield request({
+    uri: url,
+    method: 'DELETE',
+    jar: true,
+    json: true,
+    body: data
+  })
+}
+
 describe('api', () => {
   before(() => co(function * () {
     // clean up the db
@@ -64,7 +75,7 @@ describe('api', () => {
     assert(me.id === me2.id, 'ID must remain the same for same session')
   }))
 
-  it('should return anonymous session before logging in', () => co(function * () {
+  it('GET /api/session should return anonymous session before logging in', () => co(function * () {
     const session = yield get('/api/session')
     assert(session)
     assert(session.user_accounts instanceof Array)
@@ -74,7 +85,7 @@ describe('api', () => {
     assert(session.createdAt)
   }))
 
-  it('should return Unauthorized for anyone that tries to push an item to a cart without being signed in', () => co(function * () {
+  it('POST /api/carts/:cart_id/items should return Unauthorized for anyone that tries to push an item to a cart without being signed in', () => co(function * () {
     var err
     try {
       yield post('/api/carts/12345/items', {
@@ -91,7 +102,7 @@ describe('api', () => {
     assert.strictEqual(err.body.ok, false)
   }))
 
-  it('should return 404 for a DNE cart', () => co(function * () {
+  it('GET /api/carts/[Bad Id] should return 404 for a DNE cart', () => co(function * () {
     var err
     var res
     try {
@@ -108,8 +119,11 @@ describe('api', () => {
     assert.strictEqual(err.body.ok, false)
   }))
 
-  it('should pretend to sign in for the rest of the testing', () => co(function * () {
+  it('GET /api/session should return the user_account if signed in', () => co(function * () {
+    // not signed in yet, just getting the session id
     const session = yield get('/api/session')
+
+    // pretend to sign in here
     const user = yield db.UserAccounts.create({
       email_address: mcTesty.email
     })
@@ -127,7 +141,7 @@ describe('api', () => {
     assert(session2.user_accounts[0].email_address === mcTesty.email)
   }))
 
-  it('should create a new cart for /newcart and redirect to /cart/:Cart_id', () => co(function * () {
+  it('GET /newcart should create a new cart and redirect to /cart/:Cart_id', () => co(function * () {
     var res = yield get('/newcart', true)
 
     // make sure it's redirect to /cart/123456
@@ -140,5 +154,49 @@ describe('api', () => {
 
     // make sure McTesty is the leader
     assert.equal(cart.leader.email_address, mcTesty.email)
+
+    // lets save this cart id for later
+    mcTesty.cart_id = cart.id
+  }))
+
+  it('GET /api/carts should return all the users carts', () => co(function * () {
+    var carts = yield get('/api/carts')
+    assert(carts instanceof Array, 'should return an array of carts, not ' + typeof carts)
+    assert.equal(carts.length, 1, 'should only be one cart because we have only created one')
+    assert.equal(carts[0].id, mcTesty.cart_id)
+    assert(carts[0].leader)
+    assert.equal(carts[0].leader.email_address, mcTesty.email)
+  }))
+
+  it('POST /api/carts/cart_id/item should let McTesty add an item to the cart, returning the cart', () => co(function * () {
+    var url = 'https://www.amazon.com/HiLetgo-Version-NodeMCU-Internet-Development/dp/B010O1G1ES/ref=sr_1_3?ie=UTF8&qid=1490217410&sr=8-3&keywords=nodemcu'
+    var cart = yield post('/api/cart/' + mcTesty.cart_id + '/item', {
+      url: url
+    })
+
+    assert(cart)
+    assert(cart.items instanceof Array)
+    assert.equal(cart.items.length, 1, 'should only be one item in the cart')
+    assert.equal(cart.items[0].original_url, url)
+
+    // save item id for later
+    mcTesty.item_id = cart.items[0].id
+  }))
+
+  it('GET /api/cart/:cart_id should return all the info for a specific cart', () => co(function * () {
+    var cart = yield get('/api/cart/' + mcTesty.cart_id)
+    assert(cart)
+    assert.equal(cart.leader.email_address, mcTesty.email)
+    assert.equal(cart.items.length, 1, 'should only be one item in the cart')
+  }))
+
+  it('DELETE /api/cart/:cart_id/item should delete a specific item', () => co(function * () {
+    var cart = yield del('/api/cart/' + mcTesty.cart_id + '/item', {
+      id: mcTesty.item_id
+    })
+
+    assert(cart)
+    assert.equal(cart.leader.email_address, mcTesty.email)
+    assert.equal(cart.items.length, 0, 'should not be any items in the cart now')
   }))
 })
