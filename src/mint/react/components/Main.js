43540d4133a9c09be20b3921091891c9ed060594
toMoi5344 <lastres.moya.53@gmail.com>
2017-03-23 12:06:38 -0600

    Onboarding styling

--- src/mint/react/components/Main.js
+++ src/mint/react/components/Main.js
@@ -12,17 +12,36 @@ export default class Cart extends Component {
   render() {
     const { cart_id, accounts, newAccount } = this.props;
     const loggedIn = accounts.length > 0;
+
     return (
       <div>
-        <h2>Cart ID #{cart_id}</h2>
-        <div>Accounts: <ul>{accounts.map((account, i) => <li key={i}>{account.email_address}</li>)}</ul></div>
-        <hr/>
-        {loggedIn
-          ? <CartContainer cart_id={cart_id}/>
-          : ((newAccount || newAccount === undefined)
-            ? <InputContainer cart_id={cart_id} />
-            : <div>Looks like you've been here before. We just sent you an email, use that to log in! </div>)}
-        <hr/>
+        <div className="container">
+            <div className="row">
+                <div className="col-xs-12">
+                    <h1 className="page-header">Cart ID #{cart_id}</h1>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-xs-12">
+                    <h6>
+                        <strong>Accounts:</strong>
+                        {accounts.map((account, i) => <span className="text-primary" key={i}>{account.email_address}</span>)}
+                    </h6>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-xs-12">
+                    {loggedIn
+                      ? <CartContainer cart_id={cart_id}/>
+                      : ((newAccount || newAccount === undefined)
+                        ? <InputContainer cart_id={cart_id} />
+                    : <div className="help-block">Looks like you've been here before. We just sent you an email, use that to log in! </div>)}
+                </div>
+            </div>
+
+            <hr/>
+        </div>
+
       </div>
     );
   }
