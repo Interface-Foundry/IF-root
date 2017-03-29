057d89f39e9614b027b57fd92289025f2916d3c8
Hannah Katznelson <hannah.katznelson@gmail.com>
2017-03-29 15:21:22 -0400

    agenda

--- src/mint/server/index.js
+++ src/mint/server/index.js
@@ -39,6 +39,7 @@ var regularRoutes = require('./routes/regular.js');
 var apiRoutes = require('./routes/api.js');
 
 require('colors');
+require('../agendas');
 
 /**
  * Models loaded from the waterline ORM
