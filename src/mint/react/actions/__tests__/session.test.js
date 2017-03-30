cbe182254228074fdec8757d85bc813ff2637f54
Komang Luce <komangwluce@gmail.com>
2017-03-30 11:19:41 -0400

    added intial async action tests, removed unused action

--- src/mint/react/actions/__tests__/session.test.js
+++ src/mint/react/actions/__tests__/session.test.js
@@ -4,7 +4,7 @@ import thunk from 'redux-thunk'
 
 import { fakeStore } from '../../utils';
 import { signIn, update } from '../session'
-import { REQUEST_UPDATE_SESSION, RECEIVE_UPDATE_SESSION } from '../../constants/ActionTypes'
+import { REQUEST_UPDATE_SESSION, RECEIVE_UPDATE_SESSION, REQUEST_SESSION, RECEIVE_SESSION } from '../../constants/ActionTypes'
 
 const middlewares = [ thunk ]
 const mockStore = configureMockStore(middlewares)
@@ -18,7 +18,7 @@ describe('session actions', () => {
 
   body = { fakeResponse: ['do something'] }
 
-  it('creates RECEIVE_UPDATE_SESSION when signIn has been done', () => {
+  it('creates REQUEST_UPDATE_SESSION, RECEIVE_UPDATE_SESSION when signIn has been done', () => {
     nock('http://localhost:3000/api')
       .get('/identify')
       .query({cart_id: 'fakeId', email: 'fakeEmail'})
@@ -45,5 +45,39 @@ describe('session actions', () => {
       .then(() => { 
         expect(store.getActions()).toEqual(expectedActions)
       })
+      .catch((error) => {
+        // error handler
+      });
+  })
+
+  it('creates REQUEST_SESSION, RECEIVE_SESSION when update has been done', () => {
+    nock('http://localhost:3000/api')
+      .get('/session')
+      .reply(200, { body })
+
+    const expectedActions = [
+      { type: REQUEST_SESSION },
+      { type: RECEIVE_SESSION, body }
+    ]
+
+    const store = mockStore({
+      session: {  
+        newAccount: false,
+        onborded: false,
+        user_accounts: [{id: 1}]
+      },
+      cart: {  
+        cart_id: 'testId',
+        items: [{id: 1}]
+      }
+    });
+
+    return store.dispatch(update())
+      .then(() => { 
+        expect(store.getActions()).toEqual(expectedActions)
+      })
+      .catch((error) => {
+        // error handler
+      });
   })
 })
--- src/mint/react/actions/cart.js
+++ src/mint/react/actions/cart.js
@@ -5,9 +5,8 @@ const receive = (newCart) => ({
   newCart
 });
 
-const request = (cart) => ({
+const request = () => ({
   type: REQUEST_CART,
-  ...cart
 });
 
 const receiveItems = (items) => ({
@@ -19,10 +18,8 @@ const requestItems = () => ({
   type: REQUEST_ITEMS
 });
 
-const requestRemoveItem = (cart, item) => ({
-  type: REQUEST_REMOVE_ITEM_FROM_CART,
-  item,
-  cart
+const requestRemoveItem = () => ({
+  type: REQUEST_REMOVE_ITEM_FROM_CART
 });
 
 const receiveRemoveItem = (cart) => ({
@@ -41,7 +38,7 @@ const receiveAddItem = (item) => ({
 
 export function fetchCart(cart_id) {
   return async function (dispatch) {
-    dispatch(request(cart_id));
+    dispatch(request());
     const response = await fetch(`/api/cart/${cart_id}`, {
       credentials: 'same-origin'
     });
@@ -62,7 +59,7 @@ export function fetchItems(cart_id) {
 
 export function removeItem(cart_id, item) {
   return async dispatch => {
-    dispatch(requestRemoveItem(cart_id, item));
+    dispatch(requestRemoveItem());
     const response = await fetch(`/api/cart/${cart_id}/item`, {
       'method': 'DELETE',
       headers: {
