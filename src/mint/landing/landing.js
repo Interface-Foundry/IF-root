1eba945fd6cd4da108d7a01dc63e4ea896a0e26d
Hannah Katznelson <hannah.katznelson@gmail.com>
2017-04-03 17:38:51 -0400

    koh saves the day

--- src/mint/landing/landing.js
+++ src/mint/landing/landing.js
@@ -1,9 +1,25 @@
 function getHTML (url) {
   var html = `<html>
+    <head>
+      <style>
+        ::-webkit-scrollbar {
+           width: 0px;  /* remove scrollbar space */
+           background: transparent;  /* optional: just make scrollbar invisible */
+        }
+        /* optional: show position indicator in red */
+        ::-webkit-scrollbar-thumb {
+           background: transparent;
+        }
+
+        body {
+          margin: 0;
+        }
+      </style>
+    </head>
     <body>
-      <div style="width:38em;margin:3em 20% 1em 20%;">
-        <iframe scrolling="no" height="1000px" width="100%" frameborder="0" src="${url}"></iframe>`+
-      `</div>
+      <div style='height: 100%; width: 50%; margin: 0 auto; overflow: auto;'>
+        <iframe scrolling='no' height="1000px" width="100%" frameborder="0" src="${url}"></iframe>
+      </div>
     </body>
   </html>`;
   return html;
