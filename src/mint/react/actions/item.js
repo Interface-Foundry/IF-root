719c68bc9efab74800b35445826d24626a982201
Christopher Barry <me@chrisb.xyz>
2017-04-05 15:51:54 -0400

    amazon apis!

--- src/mint/react/actions/item.js
+++ src/mint/react/actions/item.js
@@ -18,8 +18,13 @@ export function fetchItem(item_id) {
   return async function (dispatch) {
     dispatch(request());
 
+    // TODO: undo this hack
+    // This is incredibly bad for when we do search
+    // TODO TODO TODO
+    item_id = item_id.includes('http') ? item_id : `https://www.amazon.com/dp/${item_id}`;
+    console.log(item_id);
     try {
-      const response = await fetch('/api/test/item', {
+      const response = await fetch(`/api/itempreview?q=${item_id}`, {
         credentials: 'same-origin'
       });
 
