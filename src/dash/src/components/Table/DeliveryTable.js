589f10c874340b419883ada00f515611935824f2
Timothy Wong <twong001@citymail.cuny.edu>
2017-04-17 12:35:58 -0400

    added team name field to delivery table

--- src/dash/src/components/Table/DeliveryTable.js
+++ src/dash/src/components/Table/DeliveryTable.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 
+function nameFormatter(data, cell) {
+  return `<p>${cell[data]}</p>`;
+}
+
 const DeliveryTable = ({data}) => {
+	debugger;
   return (
     <BootstrapTable data={data} hover>
       <TableHeaderColumn isKey={true} dataField='time_started'>Created Date</TableHeaderColumn>
       <TableHeaderColumn dataField='team_id'>Team ID</TableHeaderColumn>
+      <TableHeaderColumn dataField='team' dataFormat={ nameFormatter.bind(this, 'team_name') }>Team</TableHeaderColumn>
       <TableHeaderColumn dataField='item_count'>Item Count</TableHeaderColumn>
       <TableHeaderColumn dataField='chosen_restaurant'>Restaurant</TableHeaderColumn>
     </BootstrapTable>
