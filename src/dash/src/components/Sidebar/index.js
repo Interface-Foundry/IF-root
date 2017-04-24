484e81774c604a0a709ff287477435626d3f7368
graham <graham.annett@gmail.com>
2017-04-24 13:35:29 -0700

    remove sidebar, css still exists

--- src/dash/src/components/Sidebar/index.js
+++ src/dash/src/components/Sidebar/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import classNames from 'classnames';
 import history from '../../core/history';
-import SubSidebar from '../SubSidebar';
 
 class Sidebar extends Component {
 
