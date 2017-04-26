d1bbcf165b3ec844d1adaff6e1c2e78871302f65
Komang Luce <komangwluce@gmail.com>
2017-04-26 16:29:35 -0400

    small change #1010

--- src/mint/react/components/Settings/Settings.js
+++ src/mint/react/components/Settings/Settings.js
@@ -1,6 +1,7 @@
 // react/components/Settings/Settings.js
 
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { Icon } from '..';
 
 export default class Settings extends Component {
@@ -11,7 +12,7 @@ export default class Settings extends Component {
       <div className='settings'>
       	<ul>
       		<li>{currentUser.email_address}  &nbsp;<Icon icon='Edit'/></li>
-      		<li><Icon icon='Email'/> &nbsp; Send Feedback</li>
+      		<li><Link to={`/cart/${cart_id}/m/Feedback`}><Icon icon='Email'/> &nbsp; Send Feedback</Link></li>
       	</ul>
       	<h4>Kip Version 1.3 (Mint)</h4>
       </div>
