00804851253f7d240542cbdef65e834f9d174c3c
Hannah Katznelson <hannah.katznelson@gmail.com>
2017-04-28 17:36:14 -0400

    lost commit restored to its warm, loving codebase

--- src/mint/server/cart/amazon_cart.js
+++ src/mint/server/cart/amazon_cart.js
@@ -280,10 +280,12 @@ exports.addAmazonItemToCart = function * (item, cart) {
 
   // check if we need to create anew cart
   if (!cart.amazon_hmac || !cart.amazon_cartid) {
-    return exports.createAmazonCart(item)
+    let amazonCart = exports.createAmazonCart(item)
+    cart.amazon_cartid = amazonCart.CartId
+    cart.amazon_hmac = amazonCart.HMAC
+    yield cart.save()
   }
 
-  var cart
   var quantity = item.quantity
 
   // if the item is already in the cart, then we want to increase the quantity
@@ -298,9 +300,9 @@ exports.addAmazonItemToCart = function * (item, cart) {
       'Item.1.CartItemId': itemAlreadyAdded.CartItemId,
       'Item.1.Quantity': quantity
     };
-    cart = yield opHelper.execute('CartModify', amazonParams);
-    checkError(cart.result.CartModifyResponse.Cart)
-    return cart.result.CartModifyResponse.Cart;
+    var cartModify = yield opHelper.execute('CartModify', amazonParams);
+    checkError(cartModify.result.CartModifyResponse.Cart)
+    return cartModify.result.CartModifyResponse.Cart;
   } else {
     var amazonParams = {
       'AssociateTag': associateTag,
@@ -309,9 +311,9 @@ exports.addAmazonItemToCart = function * (item, cart) {
       'Item.1.ASIN': item.asin,
       'Item.1.Quantity': quantity
     };
-    cart = yield opHelper.execute('CartAdd', amazonParams);
-    checkError(cart.result.CartAddResponse.Cart)
-    return cart.result.CartAddResponse.Cart;
+    var cartAdd = yield opHelper.execute('CartAdd', amazonParams);
+    checkError(cartAdd.result.CartAddResponse.Cart)
+    return cartAdd.result.CartAddResponse.Cart;
   }
 };
 
@@ -360,12 +362,31 @@ exports.syncAmazon = function * (cart) {
     throw new Error('can only sync carts that have amazon items, and items must be populated')
   }
 
-  // check if we need to create a new cart
+  // check if we need to create a new cart. if so, no need to sync, just create
   if (!cart.amazon_cartid || !cart.amazon_hmac) {
-    var amazonCart = yield exports.createAmazonCart(cart.items[0])
-  } else {
-    amazonCart = yield exports.getAmazonCart(cart)
+    console.log('creating new cart with all the items')
+    var cartAddAmazonParams = {
+      'AssociateTag': associateTag,
+      'CartId': cart.amazon_cartid,
+      'HMAC': cart.amazon_hmac
+    };
+    cart.items.map((item, index) => {
+      var key = 'Item.' + (index + 1) + '.'
+      cartAddAmazonParams[key + 'ASIN'] = item.asin
+      cartAddAmazonParams[key + 'Quantity'] = item.quantity
+    })
+
+    var res = yield opHelper.execute('CartCreate', cartAddAmazonParams);
+    checkError(res.result.CartCreateResponse.Cart)
+    returnValue = res.result.CartCreateResponse.Cart
+    cart.amazon_cartid = returnValue.CartId
+    cart.amazon_hmac = returnValue.HMAC
+    yield cart.save()
+    return returnValue
   }
+
+  // amazon cart already existed for this cart, so proceed to sync items
+  var amazonCart = yield exports.getAmazonCart(cart)
 
   // Generate two requests and run them through amazon:
   //  - a CartAdd request for the the items missing
@@ -416,7 +437,6 @@ exports.syncAmazon = function * (cart) {
   // from one of the modification requests below
   var returnValue = amazonCart
   if (missingItems.length > 0) {
-    console.log('Inside Amazon Params', cartAddAmazonParams)
     var res = yield opHelper.execute('CartAdd', cartAddAmazonParams);
     checkError(res.result.CartAddResponse.Cart)
     returnValue = res.result.CartAddResponse.Cart
@@ -425,6 +445,10 @@ exports.syncAmazon = function * (cart) {
     res = yield opHelper.execute('CartModify', cartModifyAmazonParams);
     checkError(res.result.CartModifyResponse.Cart)
     returnValue = res.result.CartModifyResponse.Cart
+  }
+
+  if (!returnValue.PurchaseURL) {
+    returnValue.PurchaseURL = amazonCart.PurchaseURL
   }
 
   return returnValue
