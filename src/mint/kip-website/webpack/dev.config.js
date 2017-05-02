147fb3df129907b6e5b72506e209f2721ddef704
Komang Luce <komangwluce@gmail.com>
2017-05-02 18:27:49 -0400

    another try

--- src/mint/kip-website/webpack/dev.config.js
+++ src/mint/kip-website/webpack/dev.config.js
@@ -44,7 +44,9 @@ module.exports = {
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.DefinePlugin({'process.env': {'NODE_ENV': '"development"'}})
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+    })
   ]
 };
 
--- src/mint/kip-website/webpack/prod.config.js
+++ src/mint/kip-website/webpack/prod.config.js
@@ -47,8 +47,9 @@ module.exports = {
     hot: true
   },
   plugins: [
-    new webpack.DefinePlugin({'process.env': {'NODE_ENV': '"production"'}}),
-    new webpack.optimize.DedupePlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production')
+    })
     new webpack.optimize.UglifyJsPlugin({minimize: true, compress: {warnings: false}})
   ]
 };
