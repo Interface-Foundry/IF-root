9c4209d9fa0695f9f396f7e5cf0711d96800678f
Komang Luce <komangwluce@gmail.com>
2017-05-09 14:19:14 -0400

    fix for website, about to start on emails

--- src/mint/react/components/Item/ProductDescription.js
+++ src/mint/react/components/Item/ProductDescription.js
@@ -39,7 +39,7 @@ export default class ProductDescription extends Component {
 
   componentDidMount() {
     window.addEventListener('resize', this._handleWindowResize);
-    this._handleWindowResize()
+    this._handleWindowResize();
   }
   componentWillUnmount() {
     window.removeEventListener('resize', this._handleWindowResize);
