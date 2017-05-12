413c7b25facbdde1ec51bfb3e8b76a6002455b9b
Hannah Katznelson <hannah.katznelson@gmail.com>
2017-05-12 11:09:47 -0400

    cart types hardcoded

--- src/mint/server/cart/cart_types.js
+++ src/mint/server/cart/cart_types.js
@@ -1,16 +1,34 @@
 const stores = [{
-    "store_img": "flag image",
+    "store_img": "",
     "store_type": "amazon_us",
     "store_name": "Amazon US",
     "store_domain": "amazon.com",
     "store_countries": ["US"]
+  }, {
+    "store_img": "",
+    "store_type": "amazon_uk",
+    "store_name": "Amazon UK",
+    "store_domain": "amazon.co.uk",
+    "store_countries": ["UK"]
+  }, {
+    "store_img": "",
+    "store_type": "amazon_canada",
+    "store_name": "Amazon Canada",
+    "store_domain": "amazon.ca",
+    "store_countries": ["Canada"]
+  }, {
+    "store_img": "",
+    "store_type": "ypo",
+    "store_name": "YPO",
+    "store_domain": "ypo.co.uk",
+    "store_countries": ["UK"]
   }
 ]
 
 const countryCoordinates = {
-  'US': [37.0902, 95.7129], //n w
-  'UK': [],
-  'Canada': []
+  'US': [37.0902, 95.7129],
+  'UK': [55.3781, 3.4360],
+  'Canada': [56.1304, 106.3468]
 }
 
 module.exports = {
