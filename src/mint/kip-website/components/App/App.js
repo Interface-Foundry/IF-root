6a6b08fc9f74ead5f87f9448df25e66afaaa8155
Komang Luce <komangwluce@gmail.com>
2017-05-15 15:43:25 -0400

    changed react-router --> react-router-dom

--- src/mint/kip-website/components/App/App.js
+++ src/mint/kip-website/components/App/App.js
@@ -4,7 +4,7 @@
 import React, { Component } from 'react';
 import { Landing, Legal } from '..';
 
-import { Route } from 'react-router';
+import { Route } from 'react-router-dom';
 
 export default class App extends Component {
 	render() {
