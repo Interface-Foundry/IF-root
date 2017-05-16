e412665e1383d65dde147990167ce53310618ecc
Hannah Katznelson <hannah.katznelson@gmail.com>
2017-05-16 11:22:20 -0400

    create new address route

--- src/mint/server/routes/users-api.js
+++ src/mint/server/routes/users-api.js
@@ -130,7 +130,7 @@ module.exports = function (router) {
     // check if the user is already identified as this email
     var currentUser = req.UserSession.user_account
 
-    // IF they are already logged in as this email, probably in a weird state to be re-identifying
+    // If they are already logged in as this email, probably in a weird state to be re-identifying
     // idk maybe they have multiple tabs open or something, just roll with it
     if (currentUser && currentUser.email_address === email) {
       // make them the glorious cart leader if the cart is leaderless, otherwise a member
@@ -354,11 +354,16 @@ module.exports = function (router) {
    * @apiParam {string} :user_id id of the user to update
    * @apiParam {json} body the properties of the new address we're creating
    *
-   * @apiParamExample Request TODO
-  //  * post /api/user/04b36891-f5ab-492b-859a-8ca3acbf856b {
-  //  *   "venmo_accepted": true,
-  //  *   "venmo_id": "MoMcTesty"
-  //  * }
+   * @apiParamExample Request
+   * post /api/user/04b36891-f5ab-492b-859a-8ca3acbf856b {
+   *   "line_1": '2222 Fredrick Douglass Blvd',
+   *   "line_2": "Apt 2B",
+   *   "city": "New York",
+   *   "state": 'NY',
+   *   "zip": 94306,
+   *   "country": 'USA',
+   *   "user_account": user_id
+   * }
    */
   router.post('/user/:user_id/address', (req, res) => co(function * () {
     // check permissions
@@ -375,21 +380,13 @@ module.exports = function (router) {
       throw new Error('Could not find user ' + req.params.user_id)
     }
 
-    var addr = yield db.addresses.create(req.body);
+    logging.info('req.body:', req.body);
+
+    var addr = yield db.Addresses.create(req.body);
+    addr.user_account = user.id;
+    yield addr.save();
 
     res.send(addr);
-
-    // // Can't update some fields
-    // delete req.body.id
-    // delete req.body.email_address
-    // delete req.body.sessions
-    //
-    // // update the properties that they set
-    // _.merge(user, req.body)
-    //
-    // yield user.save()
-    //
-    // res.send(user)
   }))
 
   /**
