31fde806e7b2fe1e43c66432e7a872e94ddc878c
Hannah Katznelson <hannah.katznelson@gmail.com>
2017-05-31 18:06:29 -0400

    checkout event schema

---
+++ src/mint/db/checkout_events.js
@@ -0,0 +1,20 @@
+var Waterline = require('waterline')
+
+var checkoutEventsCollection = Waterline.Collection.extend({
+  identity: 'checkout_events',
+  connection: 'default',
+  migrate: 'safe',
+  attributes: {
+    /** @type {cart} the cart that's been checked out */
+    cart: Waterline.isA('carts'),
+
+    /** @type {user} the user that checks the cart out */
+    user: Waterline.isA('user_accounts'),
+
+    /** @type {cart} the cart from which the current cart was cloned */
+    original_cart: {
+      type: Waterline.isA('carts'),
+      defaultsTo: this.cart
+    }
+  }
+})
