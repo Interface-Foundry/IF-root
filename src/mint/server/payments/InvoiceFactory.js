aa3ebbd421cdcb44b58b89433dd88ae52c1a705c
graham <graham.annett@gmail.com>
2017-06-01 15:55:32 -0700

    better description of factory stuff

--- src/mint/server/payments/InvoiceFactory.js
+++ src/mint/server/payments/InvoiceFactory.js
@@ -1,20 +1,30 @@
 const MintInvoice = require('./Invoice_Mint.js')
 
-const invoiceHandlers = {
-  [MintInvoice.name]: MintInvoice,
-}
-
 var db
 const dbReady = require('../../db')
 dbReady.then((models) => { db = models; })
 
 
+const invoiceHandlers = {
+  [MintInvoice.name]: MintInvoice,
+}
+
 /**
- * Factory Class for Invoices
+ * Factory Class for Invoices to get/create/delete/etc.
+ * Basically helper functions
  *
  * @class      InvoiceFactory (name)
  */
 class InvoiceFactory {
+
+
+  /**
+   * Gets the Invoice by id
+   *
+   * @class      GetById (name)
+   * @param      {string}   invoiceId  The invoice identifier
+   * @return     {Promise}  The invoice db object into class object
+   */
   static async GetById (invoiceId) {
     const invoice = await db.Invoice.findOne({id: invoiceId})
     if (!invoice) {
@@ -23,6 +33,15 @@ class InvoiceFactory {
     return new invoiceHandlers[invoice.invoice_type](invoice)
   }
 
+
+  /**
+   * create a new invoice of type with data
+   *
+   * @class      Create (name)
+   * @param      {string}           invoiceType  The invoice type
+   * @param      {object}           invoiceData  The invoice data
+   * @return     {invoiceHandlers}  instantiation of the class
+   */
   static Create (invoiceType, invoiceData) {
     return new invoiceHandlers[invoiceType](invoiceData)
   }
