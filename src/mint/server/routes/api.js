a7e690be103127d43e9e445cee5c51b4145f844e
Christopher Barry <me@chrisb.xyz>
2017-06-07 11:39:49 -0400

    seend less text for json files, and call backend less. Someone might want to make file reading more efficient though, idk

--- src/mint/server/routes/api.js
+++ src/mint/server/routes/api.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var co = require('co')
 var _ = require('lodash')
-var fs = require('fs');
+var fs = require('fs')
 var path = require('path');
 var router = express.Router();
 
@@ -88,13 +88,16 @@ router.get('/blog/posts', (req, res) => co(function * () {
  * Used for A/B testing now, could be used for language later
  * @returns a json file with all of the strings for the home page
  */
-router.get('/home/json', (req, res) => co(function* () {
+router.get('/home/json', async(req, res) => {
   // need to check if we've already given them a version, and give the same version
   const siteVersion = req.UserSession.siteVersion ? req.UserSession.siteVersion : _.sample(['A', 'B', 'C']);
   req.UserSession.siteVersion = siteVersion;
-  yield req.UserSession.save();
-  res.json({siteVersion});
-}));
+  await req.UserSession.save();
+  // TODO: if anyone has a more efficient way to do this...
+  const json = JSON.parse(fs.readFileSync(path.join(__dirname, 'site.json'), 'utf8'))[siteVersion];
+  json.siteVersion = siteVersion;
+  res.json(json);
+});
 
 function _formatPostObjects(body) {
   let json = JSON.parse(body);
