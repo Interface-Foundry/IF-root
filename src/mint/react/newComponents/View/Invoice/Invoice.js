ad57f567648436051903dd228027a17db4a19392
graham <graham.annett@gmail.com>
2017-06-14 11:43:25 -0700

    for koh, front end stuff not working really

--- src/mint/react/newComponents/View/Invoice/Invoice.js
+++ src/mint/react/newComponents/View/Invoice/Invoice.js
@@ -5,15 +5,41 @@ import React, { Component } from 'react';
 import moment from 'moment';
 
 
+async function getInvoiceByCart (cartId) {
+  const invoice = await fetch(`api/invoice/cart/${this.props.cart.id}`)
+  return invoice
+}
+
 export default class Invoice extends Component {
+  state = {loading: true}
+  data = {}
+
+  async componentDidMount () {
+    const res = await fetch(`http://localhost:3000/api/invoice/cart/${this.props.cart.id}`, {
+      method: 'GET',
+      credentials: 'same-origin',
+      headers : {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+     })
+    debugger;
+    // for koh, doesnt work
+    // const data = await res.json()
+    // this.data = data
+
+    this.state.loading = false
+  }
 
   render() {
-  	const { cart } = this.props;
-  	
+    const { cart } = this.props;
+
+    if (this.state.loading) {
+      return (<p> getting invoice </p>)
+    }
+
     return (
-      <table className='invoice'>
-        <h1>inside invoice</h1>
-      </table>
+      <p> hey {this.data.invoice_type} </p>
     );
   }
 }
