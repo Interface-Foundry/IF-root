d9dfab7880ef983f1f759c306310a2ec81c48e56
Chris Barry <ChrisW-B@users.noreply.github.com>
2017-06-23 11:57:04 -0400

    sidebar name fix

--- src/mint/react/components/View/Results/Default.js
+++ src/mint/react/components/View/Results/Default.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { displayCost } from '../../../utils';
 import { Icon } from '../../../../react-common/components';
-import LoadingTile from './LoadingTile';
 
 export default class Default extends Component {
   static propTypes = {
@@ -18,7 +17,6 @@ export default class Default extends Component {
 
   render() {
     const { user, cart, item, cartAsins, selectItem, addItem, togglePopup } = this.props;
-    if (!item) return <LoadingTile />;
     return (
       <td>
         <div className={`card ${cartAsins.includes(`${item.asin}-${user.id}`) ? 'incart' : ''}`} onClick={() => !cartAsins.includes(`${item.asin}-${user.id}`) ? selectItem(item.id) : null}>
