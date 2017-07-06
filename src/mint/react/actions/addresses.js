3e445abea8be4920093d3fc994faaff38257724c
graham <graham.annett@gmail.com>
2017-07-05 19:00:29 -0700

    start of address actuins/redux

---
+++ src/mint/react/actions/addresses.js
@@ -0,0 +1,23 @@
+import { get, post, put, del } from './async';
+
+
+export const fetchAddress = user_id => get(
+  `/api/cart/${cart_id}`,
+  'CART',
+  (type, json) => ({
+    type: `${type}_SUCCESS`,
+    response: json,
+    receivedAt: Date.now()
+  })
+);
+
+export const createInvoice = (cart_id, invoice_type, split_type) => post(
+  `/api/invoice/${invoice_type}/${cart_id}`,
+  'CREATE_INVOICE',
+  {'split_type': split_type},
+  (type, json) => ({
+    type: `${type}_SUCCESS`,
+    response: json,
+    receivedAt: Date.now()
+  })
+);
