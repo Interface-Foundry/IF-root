68079895ec2ec2ed9df559ef0806f32df839b59a
alyx <alyx@kipthis.com>
2017-07-12 18:46:43 -0400

    almost done with lotte 🌏

--- src/mint/server/scraper/scrape_convert.js
+++ src/mint/server/scraper/scrape_convert.js
@@ -6,6 +6,8 @@ var request = require('request-promise')
 var fx = require("money")
 const Translate = require('@google-cloud/translate')
 var currency = require('currency-code-map')
+var url_parse = require('url-parse')
+var url = require('url')
 
 //encoding stuff
 var charset = require('charset'),
@@ -84,32 +86,174 @@ var scrapeURL = async function (url) {
 
 	  	//detect char encoding
 	  	var enc = charset(res.headers, html) || jschardet.detect(html).encoding.toLowerCase()
+	   	//setup encoding
+	    var iconv = new Iconv(enc, 'UTF-8//TRANSLIT//IGNORE')
+	    //do convert
+   		convert = iconv.convert(new Buffer(html)).toString('utf8')
 
-	  	//if not utf-8
-	    if(enc !== 'utf8') {
-	    	//setup encoding
-	        var iconv = new Iconv(enc, 'UTF-8//TRANSLIT//IGNORE')
-	        //do convert
-   			convert = iconv.convert(new Buffer(html)).toString('utf8')
-	    }else {
-	    	convert = html
-	    }
 	  }else {
-	  	logging.info('ERROR '+response.statusCode+' IN REQUEST!!!!! ', error)
+	  	logging.error('ERROR '+response.statusCode+' IN REQUEST!!!!! ', error)
 	  }
-
 	})
 	return convert
 }
 
+var tryOptionHtml = async function(domain,parentOption,html){
+
+	var $ = await cheerio.load(html)
+
+	switch(domain){
+		case 'lotte.com':
+
+			//BUILD REAL OPTIONS HERE, COLOR + SIZE combinations....???????
+			//NO NO HAS TO BE COLOR > SIZE
+
+			//START BY ADDING PARENT OPTIONS (WITH AVAILABLE = true)
+
+			var options = []
+
+			console.log('🌏🐝🌏 ',parentOption) 
+
+
+
+			if($('.c_list li').length <= 0){
+				console.log('THIS PARENT OPTION HAS NO SUB OPTIONS!!')
+				return 
+			}
+
+			
+			//get options
+			$('.c_list li').each(function(i, elm) {
+
+				var name = $('.sec01',this).text().trim()
+				var product_id = $('a',this).attr('goodsno') //product id for this option
+
+				//checking available if this option has an a link (can't click option if it's unavail)
+				var available
+				if ($('a',this)){
+					available = true
+				}else {
+					available = false
+				}
+
+				optionQ.push({
+					original_name: {
+						value: name
+					},
+				    product_id: product_id,
+				    parent_id: parentOption.product_id,
+				    available: available
+				})
+			})
+
+			console.log('🌏🌏🌏 ',optionQ) 
+
+		break
+	}
+
+}
+
 //try to get data from html
-var tryHtml = async function (s,$) {
+var tryHtml = async function (s,html) {
+
+	var $ = await cheerio.load(html)
+
+	if(!s){
+		logging.error('nothing in scrape object!')
+		return 
+	}
+
 	switch(s.domain.name){
 
 		case 'lotte.com':
 
+			//get product id
+			var parsed = url.parse(s.original_link, true)
+			if(parsed.query && parsed.query.goods_no){
+				s.parent_id = parsed.query.goods_no
+			}
+			
+			//name, descrip
+			s.original_name.value = $('.group_tit').text().trim()
+			s.original_description.value = $('.md_tip').text().trim()
+
+			//price
+			var p = $('.after_price').text().trim().replace(/[^0-9.]/g, "")
+			s.original_price.value = parseFloat(p)
+
+			//image URL
+		    s.thumbnail_url = $('img','#fePrdImg01').attr('src')
+		    s.main_image_url = $('img','#fePrdImg01').attr('src')
+
+			//html queries to do for options
+			var optionQ = []
+
+			//get options
+			$('.c_list li').each(function(i, elm) {
+
+				var opt_url = $('a',this).attr('loadurl') //url to get sub options for this options
+				var name = $('.sec01',this).text().trim()
+				var price = $('.sec02',this).text().trim().replace(/[^0-9.]/g, "")
+				var product_id = $('a',this).attr('goodsno') //product id for this option
+				var img = $('img',this).attr('src') //option image
+				img = img.replace('_60','_150') //make option images larger (150px instead of 60px)
+
+				if(opt_url){
+					optionQ.push({
+						opt_url: 'http://www.lotte.com'+opt_url,
+						type: 'style',
+						original_name: {
+							value: name
+						},
+						original_price: {
+							value: price
+						},
+					    thumbnail_url:img,
+					    main_image_url:img,
+					    product_id: product_id
+					})
+				}else {
+					console.log('ITEM COMPLETELY SOLD OUT DONT EVEN RETURN IT!!!!')
+				}
+
+				//loadurl: 
+				//http://www.lotte.com/goods/viewGoodsDetailOptInfo.lotte?goods_no=297941318
+				// reutrns nothing, so no sub options!
+
+				//http://www.lotte.com/goods/viewGoodsDetailOptInfo.lotte?goods_no=392012841
+				//returns HTML options, now check these
+
+				//PARALLEL QUERY ALL OPTION URLS!!!
+
+				//STORE goodsno 
+				//
+
+				//goods_no in the option select field
+			})	
+
+			//srape all product option URLs checking for suboptions ~
+
+			var htmlQ = []
+			for (i = 0; i < optionQ.length; i++) { 
+				htmlQ.push(scrapeURL(optionQ[i].opt_url))
+			}
+			var results = await Promise.all(htmlQ)
+
+			var optionResults = []
+			for (i = 0; i < optionQ.length; i++) { 
+				optionResults.push(tryOptionHtml(s.domain.name,optionQ[i],results[i]))
+			}
+
+			var options = await Promise.all(optionResults)
 
 
+			
+
+			console.log('🐝🐝🐝 ',options)
+
+
+
+			// return s
 			break
 
 		case 'store.punyus.jp':
@@ -277,7 +421,7 @@ var tryHtml = async function (s,$) {
 			return s
 		break
 		default:
-			logging.info('error no domain found for store')
+			return logging.error('error no domain found for store')
 	}
 }
 
@@ -401,7 +545,7 @@ var translateText = async function (s){
 	}
 	if(s.options && s.options.length > 0){
 		s.options.forEach(function(o){
-			if(o.original_name){ //we AREN'T translating size options, it breaks translations
+			if(o.original_name){
 				c.push({
 					type:'option',
 					value: o.original_name.value
@@ -447,12 +591,14 @@ var scrape = async function (url, user_country, user_locale, store_country, doma
 		logging.info('USER_COUNTRY, USER_LOCALE', user_country, user_locale)
 		var s = getLocale(url,user_country,user_locale,store_country,domain) //get domain
 		var html = await scrapeURL(url)
-		var $ = cheerio.load(html)
-		s = await tryHtml(s,$)
+		s = await tryHtml(s,html)
 
-		logging.info('got html')
+		if(!s){
+			return logging.error('no s object found!')
+		}
 
 		var rates = await getRates()
+
  		var price = await foreignExchange(s.domain.currency,s.user.currency,s.original_price.value,currencySpread,rates)
  		s = await storeFx(rates,price,s)
 
@@ -491,6 +637,11 @@ var storeFx = async function(rates,price,s){
  */
 function randomInt(exclusiveMax) {
   return Math.floor(Math.random() * Math.floor(exclusiveMax))
+}
+
+//does this string contain a number (like a price in a string)
+function hasNumber(string) {
+  return /\d/.test(string)
 }
 
 /**
--- src/mint/server/scraper/url_tester.js
+++ src/mint/server/scraper/url_tester.js
@@ -2,11 +2,14 @@ const logging = require('../../../logging.js')
 const scrape = require('./scrape_convert')
 
 //var uri = 'http://www.lotte.com/goods/viewGoodsDetail.lotte?goods_no=303964888&infw_disp_no_sct_cd=20&infw_disp_no=5370476&allViewYn=N'
-var uri = 'https://www.muji.net/store/cmdty/detail/4549738522508'
-//var domain = 'www.lotte.com'
-var domain = 'muji.net'
-//var story_country = 'KR'
-var story_country = 'JP'
+var uri = 'http://www.lotte.com/goods/viewGoodsDetail.lotte?goods_no=396484359&infw_disp_no_sct_cd=78&infw_disp_no=5505566&allViewYn=N'
+
+//var uri = 'https://www.muji.net/store/cmdty/detail/4549738522508'
+var domain = 'lotte.com'
+//var domain = 'muji.net'
+var story_country = 'KR'
+//var story_country = 'JP'
+
 var user_locale = 'en'
 var user_country = 'US'
 
