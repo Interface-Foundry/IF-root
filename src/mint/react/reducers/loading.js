9e0ec2636989d4597ace17fbb928d48364e73a56
Chris Barry <me@chrisb.xyz>
2017-07-24 16:31:42 -0400

    fix scrolling on display pages

--- src/mint/react/reducers/loading.js
+++ src/mint/react/reducers/loading.js
@@ -7,10 +7,17 @@ const initialState = {
 
 export default (state = initialState, action) => {
   const loadingState = { ...state.loadingState };
+  let failed = state.failed;
   if (action.type.includes('_LOADING')) loadingState[action.type.replace('_LOADING', '')] = true;
   else if (action.type.includes('_SUCCESS')) delete loadingState[action.type.replace('_SUCCESS', '')];
+  else if (action.type.includes('_FAIL')) {
+    failed = true;
+    delete loadingState[action.type.replace('_FAIL', '')];
+  }
   return {
+    ...state,
     loadingState,
-    loading: Object.values(loadingState).reduce((a, i) => a || i, false)
+    loading: Object.values(loadingState).reduce((a, i) => a || i, false),
+    failed
   };
-}
+};
