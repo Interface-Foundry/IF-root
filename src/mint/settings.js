5cc36925f6ed6fc30d4f0ce1a8e080ae4a03d335
Chris Barry <me@chrisb.xyz>
2017-07-25 17:28:28 -0400

    settings fix

--- src/mint/settings.js
+++ src/mint/settings.js
@@ -6,10 +6,10 @@ const production = process.env.NODE_ENV === 'production'
 
 const settings = {
   PRODUCTION: production,
-  KIP_PAY_ENABLED: flag(process.env.KIP_PAY_ENABLED),
-  STRIPE_KEY: testOrProd('pk_test_8bnLnE2e1Ch7pu87SmQfP8p7', 'pk_live_0LCJqLkmMCFKYDwbPCrhQknH'),
-  PAYPAL_KEY: testOrProd('AW4Qaa3xF5SKI1Ysz6kTkFWq0c7AGBtpUXlJEkkO8SMhMO5Kn', 'AVr0hZHU5vDLj1MVHlVchyeDCOrcmFPCT2pxv3A0zLjntjmiwT4wP'),
-  GA_ENABLED: flag(testOrProd(process.env.GA, true))
+  KIP_PAY_ENABLED: JSON.stringify(flag(process.env.KIP_PAY_ENABLED)),
+  STRIPE_KEY: JSON.stringify(testOrProd('pk_test_8bnLnE2e1Ch7pu87SmQfP8p7', 'pk_live_0LCJqLkmMCFKYDwbPCrhQknH')),
+  PAYPAL_KEY: JSON.stringify(testOrProd('AW4Qaa3xF5SKI1Ysz6kTkFWq0c7AGBtpUXlJEkkO8SMhMO5Kn', 'AVr0hZHU5vDLj1MVHlVchyeDCOrcmFPCT2pxv3A0zLjntjmiwT4wP')),
+  GA_ENABLED: JSON.stringify(flag(testOrProd(process.env.GA, true)))
 }
 
 console.log(settings)
@@ -41,14 +41,14 @@ function flag(string) {
 
   if (typeof string === 'string') {
     switch (string.toLowerCase()) {
-      case 'false':
-      case 'f':
-      case 'no':
-      case 'n':
-      case '0':
-        return false
-      default:
-        return true
+    case 'false':
+    case 'f':
+    case 'no':
+    case 'n':
+    case '0':
+      return false
+    default:
+      return true
     }
   }
 
