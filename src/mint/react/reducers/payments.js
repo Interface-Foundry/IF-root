a331a1e191d83436c83be6c0ecc56e8651939027
Chris Barry <me@chrisb.xyz>
2017-07-28 14:05:17 -0400

    payment reducer fixes

--- src/mint/react/reducers/payments.js
+++ src/mint/react/reducers/payments.js
@@ -8,39 +8,24 @@ const initialState = {
 
 export default function payments(state = initialState, action) {
   switch (action.type) {
-  case 'PAYMENTSOURCES_SUCCESS':
-    return {
-      ...state,
-      paymentSources: action.response
-    };
-  case 'DELETE_PAYMENTSOURCE_LOADING':
-    return {
-      ...state
-    };
   case 'DELETE_PAYMENTSOURCE_SUCCESS':
     return {
       ...state,
       paymentSources: state.paymentSources.filter((paymentSource, i) => paymentSource.id !== action.response)
     };
+  case 'PAYMENTSOURCES_SUCCESS':
   case 'CREATE_PAYMENTSOURCE_SUCCESS':
     return {
       ...state,
-      userPaymentStatus: action.response
+      userPaymentSources: action.response
     };
   case 'INVOICE_SUCCESS':
-    return {
-      ...state,
-      invoice: action.response
-    };
   case 'CREATE_INVOICE_SUCCESS':
-    return {
-      ...state,
-      invoice: action.response
-    };
   case 'INVOICE_BY_CART_SUCCESS':
     return {
       ...state,
-      invoice: action.response
+      invoice: action.response,
+      userPaymentStatus: { paid: false }
     };
   case 'INVOICE_BY_CART_FAIL':
     return {
@@ -50,19 +35,10 @@ export default function payments(state = initialState, action) {
   case 'UPDATE_INVOICE_OPTIONS_SUCCESS':
     return {
       ...state,
-      invoice: action.response.invoice,
-      userPaymentStatus: action.response.userPaymentStatus
+      ...action
     };
   case 'CREATE_PAYMENT_SUCCESS':
-    return {
-      ...state,
-      userPaymentStatus: action.response
-    };
   case 'CREATE_PAYMENT_WITHOUT_SOURCE':
-    return {
-      ...state,
-      userPaymentStatus: action.response
-    };
   case 'FETCH_PAYMENT_STATUS_SUCCESS':
     return {
       ...state,
