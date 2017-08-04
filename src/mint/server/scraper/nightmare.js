a737dd4f6deafbf29c637e711020ad3f1f733f76
Hannah Katznelson <hannah.katznelson@gmail.com>
2017-08-04 18:24:24 -0400

    scraping item basics from taobao, finally

--- src/mint/server/scraper/nightmare.js
+++ src/mint/server/scraper/nightmare.js
@@ -1,7 +1,22 @@
 var co = require('co')
-var Nightmare = require('nightmare')
-var nightmare = Nightmare({show: true})
 var Agent = require('socks5-https-client/lib/Agent')
+var Nightmare = require('nightmare')
+// var proxynightmare = Nightmare({
+//   show: true,
+//   gotoTimeout: 10000,
+//   switches: {
+//     'proxy-server': 'socks5://109.201.154.239:1080',
+//     'ignore-certificate-errors': false
+//   }
+// })
+var nightmare = Nightmare({show: true})
+
+/**
+ * returns a random integer between 0 and the specified exclusive maximum.
+ */
+function randomInt(exclusiveMax) {
+  return Math.floor(Math.random() * Math.floor(exclusiveMax))
+}
 
 /**
  * returns a fake user agent to be used in request headers.
@@ -21,25 +36,66 @@ var fakeUserAgent = function () {
 
 
 // nightmare.goto('https://item.taobao.com/item.htm?spm=a219r.lmn002.14.350.70b3a555i5eHfT&id=554289270484&ns=1&abbucket=13&sku=1627207:117004077#detail')
-module.exports = async function (uri) {
+module.exports = async function (uri, proxy) {
   console.log('scraping.... with *nightmare*')
+  // if (proxy || true) {
+  //   console.log('let\'s use a proxy')
+  //   var n = proxynightmare
+  // }
+  // else var n = nightmare
 
-  return await nightmare
-    .goto(uri, {
-		  'User-Agent': fakeUserAgent(),
-		  'Accept': 'text/html,application/xhtml+xml',
-		  'Accept-Language':'en-US,en;q=0.8',
-		  'Cache-Control':'max-age=0',
-		  'Connection':'keep-alive'
-		})
-    // .wait('#J_Promo')
-    .evaluate(function () {
-      return document.querySelector('body').innerHTML
-    })
-    .end()
-    .then(function (res) {
-      // console.log('result:', res)
-      console.log('got response')
-      return res
-    })
-}
+  try {
+    return await nightmare
+      .goto(uri, {
+  		  // 'UserAgent': fakeUserAgent(),
+  		  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Encoding': 'gzip, deflate, br',
+  		  'Accept-Language':'en-US,en;q=0.8',
+  		  'Cache-Control':'max-age=0',
+  		  'Connection':'keep-alive',
+        'Cookie': 'thw=cn; t=951d4b470afbae0a33d3fa4a81238ce8; cookie2=3c4ab744fc2d0f42a082ae537c12e330; v=0; linezing_session=hEZO8PpaAtGHW1vCfORKttLK_1501773405797TXCi_1; _m_h5_tk=edc429663c52d293bfbddd2169ffb3c2_1501780948742; _m_h5_tk_enc=44055e33675a37f2d7d7bc7da69f751f; miid=1744327738693929296; hng=CN%7Czh-cn%7CCNY; cna=vxfsEX5MHAICAUUMGzI8qqtS; _tb_token_=3dfbeeee8a431; mt=ci%3D-1_0; uc1=cookie14=UoTcDv7dsENytA%3D%3D; isg=Ajw8S7sBSUvWs3327xIh5uH6DdPkGOpZgvXwsBa9SCcK4dxrPkWw77LT7T9i',
+        'dnt': 1,
+        'upgrade-insecure-requests': 1,
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.78 Safari/537.36'
+      })
+      // .authentication('x7229287', 'QbhYhHsKZG')
+      // .goto(uri)
+      // .cookies.clear('_med')
+      // .cookies.clear('hng')
+      // .cookies.set({
+      //   name: 'thw',
+      //   value: 'cn',
+      //   domain: '.taobao.com',
+      //   path: '/'
+      // })
+      // .cookies.set({
+      //   name: 'hng',
+      //   value: 'CN%7Czh-CN%7CCNY%7C156',
+      //   domain: '.taobao.com',
+      //   path: '/',
+      //   session: true
+      // })
+      .goto(uri)
+      // .goto(uri.slice(0, 8) + 'item' + uri.slice(12))
+
+      // .cookies.get()
+      // .then(function(cookies) {
+      //   console.log('cookies', cookies)
+      // })
+      // console.log('about to evaluate the url')
+
+      .evaluate(function () {
+        return document.querySelector('body').innerHTML
+      })
+      .end()
+      .then(function (res) {
+        // console.log('result:', res)
+        console.log('got response', !!res)
+        return res
+      })
+  }
+  catch (err) {
+    console.log('error:', err)
+  }
+
+  }
--- src/mint/server/scraper/stores/taobao.js
+++ src/mint/server/scraper/stores/taobao.js
@@ -1,17 +1,49 @@
 var fx_currency = require('../foreign_exchange')
+var url = require('url')
 
 module.exports = async function (s, $) {
-  s.original_name.value = $('#J_Title').find('h3').text()
 
-  if ($('#J_PromoPrice').find('strong'.text())) {
+  //get product id
+  var parsed = url.parse(s.original_link, true)
+  if(parsed.query && parsed.query.id){
+    s.product_id = parsed.query.id
+    s.parent_id = parsed.query.id
+  }
+
+  //get item name
+  console.log('taobao called')
+  s.original_name.value = $('#J_Title').find('h3').text().trim()
+  console.log('original name', s)
+
+  //get item price
+  if ($('#J_PromoPrice').find('strong').text()) {
+    console.log('found a promo price')
+
     var price = $('#J_PromoPrice').find('strong').text()
     s.original_price.value = price.slice(1)
-    console.log('found a promo price', price)
   }
   else {
+    console.log('did not find a promo price')
     console.log('did not find a price; trying again')
+    console.log($('#J_StrPrice').html())
     var price = $('#J_StrPrice').find('.tb-rmb-num').text()
+    s.original_price.value = price
     console.log('price:', price)
+  }
+
+  //get item description, if there is one
+  if ($('.tb-subtitle')) {
+    s.original_description.value = $('.tb-subtitle').text().trim()
+  }
+
+  //get main image
+  if ($('.tb-main-pic')) {
+    s.main_image_url = $('.tb-main-pic').find('img').attr('src')
+  }
+
+  //get smaller image
+  if ($('#J_UlThumb')) {
+    s.thumbnail_url = $('J_UlThumb').find('li').attr('data-index', '1').find('img').attr('src')
   }
 
   console.log('t h i n g : ', s)
